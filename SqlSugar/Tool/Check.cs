10d4427e0d5660d460547bd289d824b05c667034
sunkaixuan <610262374@qq.com>
2015-07-19 18:47:15 +0800

    OK

--- SqlSugar/Tool/Check.cs
+++ SqlSugar/Tool/Check.cs
@@ -15,11 +15,22 @@ namespace SqlSugar
     /// </summary>
     public class Check
     {
+        /// <summary>
+        /// 使用导致此异常的参数的名称初始化 System.ArgumentNullException 类的新实例。
+        /// </summary>
+        /// <param name="checkObj"></param>
+        /// <param name="message"></param>
         public static void ArgumentNullException(object checkObj, string message)
         {
             if (checkObj == null)
                 throw new ArgumentNullException(message);
         }
+        /// <summary>
+        /// 使用指定的错误消息初始化 System.Exception 类的新实例。
+        /// </summary>
+        /// <param name="isException"></param>
+        /// <param name="message"></param>
+        /// <param name="args"></param>
         public static void Exception(bool isException, string message, params string[] args)
         {
             if (isException)
