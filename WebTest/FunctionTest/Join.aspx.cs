ecc5d41b40e90e2cd70d3ba6150cee1131283db5
sunkaixuan <610262374@qq.com>
2015-07-22 22:50:28 +0800

    OK

--- WebTest/FunctionTest/Join.aspx.cs
+++ WebTest/FunctionTest/Join.aspx.cs
@@ -4,14 +4,19 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
-
+using SqlSugar;
+using Models;
 namespace WebTest.FunctionTest
 {
     public partial class Join : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            using (var db = SugarDao.GetInstance())
+            {
+                var list = db.Sqlable().Form("student", "s").Join("school", "l", "s.sch_id", "l.id", JoinType.INNER).SelectToList<Student>("*");
 
+            }
         }
     }
 }
