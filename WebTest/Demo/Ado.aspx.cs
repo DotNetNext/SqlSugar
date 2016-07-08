ad6b8321d51fdce901c4cdcd27a9c20ce751515e
sunkaixuan <610262374@qq.com>
2016-07-09 00:49:24 +0800

    ok

---
+++ WebTest/Demo/Ado.aspx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using SqlSugar;
+using WebTest.Dao;
+using Models;
+namespace WebTest.Demo
+{
+    /// <summary>
+    /// 原始操作数据库函数
+    /// </summary>
+    public partial class Ado : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            using (SqlSugarClient db = SugarDao.GetInstance())//开启数据库连接
+            {
+               var r1= db.GetDataTable("select * from student");
+               var r2= db.GetSingle<Student>("select * from student");
+               var r3 = db.GetScalar("select  count(1) from student");
+               var r4 = db.GetReader("select  count(1) from student");
+               var r5 = db.GetString("select  top 1 name from student");
+               var r6 = db.ExecuteCommand("select 1");
+            }
+        }
+    }
+}
--- WebTest/Demo/Tran.aspx.cs
+++ WebTest/Demo/Tran.aspx.cs
@@ -4,14 +4,33 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
-
+using SqlSugar;
+using WebTest.Dao;
 namespace WebTest.Demo
 {
+    /// <summary>
+    /// 事务和锁
+    /// </summary>
     public partial class Tran : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            using (SqlSugarClient db = SugarDao.GetInstance())//开启数据库连接
+            {
+                db.IsNoLock = true;//启用无锁查询
+                db.CommandTimeOut = 30000;//设置超时时间
+                try
+                {
+                    db.BeginTran();//开启事务
 
+                    db.CommitTran();//提交事务
+                }
+                catch (Exception)
+                {
+                    db.RollbackTran();//回滚事务
+                    throw;
+                }
+            }
         }
     }
 }
