35c3358b8dc4f3ee1c9c5fede723708a76f3240e
sunkaixuan <610262374@qq.com>
2016-07-08 23:58:28 +0800

    ok

--- WebTest/Demo/CreateModelFile.aspx.cs
+++ WebTest/Demo/CreateModelFile.aspx.cs
@@ -4,13 +4,31 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using WebTest.Dao;
 
 namespace WebTest.Demo
 {
+    /// <summary>
+    /// 创建实体文件
+    /// </summary>
     public partial class CreateModelFile : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            using (var db = SugarDao.GetInstance())
+            {
+
+                //根据当前数据库生成所有表的实体类文件 （参数：SqlSugarClient ，文件目录，命名空间）
+                //db.ClassGenerating.CreateClassFiles(db,Server.MapPath("~/Models"),"Models");
+                //根据表名生成实体类文件
+                //db.ClassGenerating.CreateClassFilesByTableNames(db, Server.MapPath("~/Models"), "Models" , "student","school");
+
+                //根据表名生成class字符串
+                var str = db.ClassGenerating.TableNameToClass(db, "Student");
+
+                //根据SQL语句生成class字符串
+                var str2 = db.ClassGenerating.SqlToClass(db, "select top 1 * from Student", "student");
+            }
 
         }
     }
