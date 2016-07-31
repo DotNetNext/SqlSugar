967fac7e5a23969a655bffec1896636d169cb908
sunkaixuan <610262374@qq.com>
2016-07-31 11:21:21 +0800

    OK

--- WebTest/Demos/Ado.aspx.cs
+++ WebTest/Demos/Ado.aspx.cs
@@ -19,9 +19,10 @@ namespace WebTest.Demo
             using (SqlSugarClient db = SugarDao.GetInstance())//开启数据库连接
             {
                var r1= db.GetDataTable("select * from student");
-               var r2= db.GetSingle<Student>("select * from student");
+               var r2= db.GetSingle<Student>("select top 1 * from student");
                var r3 = db.GetScalar("select  count(1) from student");
                var r4 = db.GetReader("select  count(1) from student");
+               r4.Dispose();
                var r5 = db.GetString("select  top 1 name from student");
                var r6 = db.ExecuteCommand("select 1");
             }
