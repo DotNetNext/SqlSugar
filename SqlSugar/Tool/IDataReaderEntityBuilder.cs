6d65be2783355a73bbfe26b46a99f417c52f5b3a
sunkaixuan <610262374@qq.com>
2016-08-10 01:48:31 +0800

    ok

--- SqlSugar/Tool/IDataReaderEntityBuilder.cs
+++ SqlSugar/Tool/IDataReaderEntityBuilder.cs
@@ -122,7 +122,7 @@ namespace SqlSugar
         /// <param name="pro"></param>
         private static void GeneratorCallMethod(ILGenerator generator, Type type, bool isNullable, PropertyInfo pro, string dbTypeName, string fieldName)
         {
-            List<string> guidThrow = new List<string>() { "int32", "datetime", "decimal", "double", "byte" };//数据库为GUID有错的实体类形
+            List<string> guidThrow = new List<string>() { "int32", "datetime", "decimal", "double", "byte","string" };//数据库为GUID有错的实体类形
             List<string> intThrow = new List<string>() { "datetime", "byte" };//数据库为int有错的实体类形
             List<string> stringThrow = new List<string>() { "int32", "datetime", "decimal", "double", "byte", "guid" };//数据库为vachar有错的实体类形
             List<string> decimalThrow = new List<string>() { "datetime", "byte", "guid" };
