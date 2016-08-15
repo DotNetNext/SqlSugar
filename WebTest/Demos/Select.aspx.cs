f8bda8407dabff750147b2dbf4f5cf8d6adf4b07
sunkaixuan <610262374@qq.com>
2016-08-15 22:39:28 +0800

    OK

--- WebTest/Demos/Select.aspx.cs
+++ WebTest/Demos/Select.aspx.cs
@@ -209,7 +209,7 @@ namespace WebTest.Demo
                 var convert4 = db.Queryable<Student>().Where(c => DateTime.Now > DateTime.Now).ToList();
 
                 //支持字符串Where 让你解决，更复杂的查询
-                var student12 = db.Queryable<Student>().Where(c => "a" == "a").Where("id>100").ToList();
+                var student12 = db.Queryable<Student>().Where(c => "a" == "a").Where("id>@id", new { id=1}).ToList();
                 var student13 = db.Queryable<Student>().Where(c => "a" == "a").Where("id>100 and id in( select 1)").ToList();
 
 
