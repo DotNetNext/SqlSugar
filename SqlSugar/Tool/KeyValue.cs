ed1d3fdb88549733119deee20697f3aa7e9e904c
sunkaixuan <610262374@qq.com>
2016-09-19 03:02:15 +0800

    ok

--- SqlSugar/Tool/KeyValue.cs
+++ SqlSugar/Tool/KeyValue.cs
@@ -5,12 +5,17 @@ using System.Text;
 
 namespace SqlSugar
 {
+    /// <summary>
+    /// 自定义键值类 key is string, value is string
+    /// </summary>
     public class KeyValue
     {
         public string Key { get; set; }
         public string Value { get; set; }
     }
-
+    /// <summary>
+    /// 自定义键值类 key is string, value is object
+    /// </summary>
     public class KeyValueObj
     {
         public string Key { get; set; }
