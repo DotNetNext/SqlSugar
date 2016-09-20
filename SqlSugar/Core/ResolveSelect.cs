51ed0378712f0f0f3aca959c2dd0bb8d85d8bf2e
sunkaixuan <610262374@qq.com>
2016-09-21 03:35:58 +0800

    ok

--- SqlSugar/Core/ResolveSelect.cs
+++ SqlSugar/Core/ResolveSelect.cs
@@ -8,7 +8,7 @@ namespace SqlSugar
 {
 
     /// <summary>
-    /// ** 描述：解析SELECT里的表达示
+    /// ** 描述：解析Queryable.Select函数的参数
     /// ** 创始时间：2016-9-21
     /// ** 修改时间：-
     /// ** 作者：sunkaixuan
@@ -35,6 +35,8 @@ namespace SqlSugar
                 throw new SqlSugarException("Select 解析失败 ", new { selectString = reval.Select });
             }
             reval.Select = reval.Select.Replace("\"", "'");
+            reval.Select = reval.Select.Replace("DateTime.Now", "GETDATE()");
+            reval.Select = ConvertFuns(reval.Select);
         }
         /// <summary>
         /// 单表情况
@@ -51,7 +53,38 @@ namespace SqlSugar
                 throw new SqlSugarException("Select 解析失败 ", new { selectString = reval.Select });
             }
             expStr = expStr.Replace("\"", "'");
+            expStr = expStr.Replace("DateTime.Now", "GETDATE()");
+            expStr = ConvertFuns(expStr);
             reval.Select = expStr;
+        }
+        /// <summary>
+        /// 替换函数
+        /// </summary>
+        /// <param name="selectStr"></param>
+        /// <returns></returns>
+        internal static string ConvertFuns(string selectStr)
+        {
+            var hasFun = selectStr.Contains("(");
+            if (hasFun)
+            {
+                var ms = Regex.Matches(selectStr, @"(?<=\=\s+)([a-z,A-Z]+)?\((.+?)\)");
+                if (ms != null && ms.Count > 0)
+                {
+                    foreach (Match m in ms)
+                    {
+                        string itemStr = m.Groups[0].Value;
+                        string itemValue = m.Groups[2].Value;
+                        string funName = m.Groups[1].Value;
+                        selectStr = selectStr.Replace(itemStr, itemValue);
+                    }
+                }
+                var hasObjToFun = Regex.IsMatch(selectStr, @"\.ObjTo[a-z,A-Z]+?\(\s*\)");
+                if (hasObjToFun)
+                {
+                    selectStr = Regex.Replace(selectStr,@"\.ObjTo[a-z,A-Z]+?\(\s*\)","");
+                }
+            }
+            return selectStr;
         }
     }
 }
