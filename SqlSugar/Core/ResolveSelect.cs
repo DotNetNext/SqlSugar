a2652a85fa8a62dfc2b8b841b73bf97361f6063f
sunkaixuan <610262374@qq.com>
2016-09-21 02:12:20 +0800

    ok

---
+++ SqlSugar/Core/ResolveSelect.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SqlSugar
+{
+
+    public class ResolveSelect
+    {
+
+        internal static string GetResult(string expStr)
+        {
+            return null;
+        }
+    }
+}
