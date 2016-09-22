1e22d8f2f658e535dfea87ba937b12b101603109
sunkaixuan <610262374@qq.com>
2016-09-22 23:09:00 +0800

    ok

--- WebTest/Demos/Delete.aspx.cs
+++ WebTest/Demos/Delete.aspx.cs
@@ -18,10 +18,10 @@ namespace WebTest.Demo
         {
             using (var db = SugarDao.GetInstance())
             {
-                //真删除
+                //删除
                 db.Delete<School, int>(10);//注意主键必需为实体类的第一个属性
                 db.Delete<School>(it => it.id > 100);
-                //批量删除
+                //主键批量删除
                 db.Delete<School, string>(new string[] { "100", "101", "102" });
                 //非主键批量删除
                 db.Delete<School, string>(it => it.name, new string[] { "" });
@@ -29,7 +29,7 @@ namespace WebTest.Demo
 
                 //假删除
                 //db.FalseDelete<school>("is_del", 100);
-                //等同于 update school set is_del=0 where id in(100)
+                //等同于 update school set is_del=1 where id in(100)
                 //db.FalseDelete<school>("is_del", it=>it.id==100);
 
 
