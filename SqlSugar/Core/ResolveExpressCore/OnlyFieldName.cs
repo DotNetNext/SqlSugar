91e3b36701d6a84b068fd2fc4af0bc650cebd80d
sunkaixuan <610262374@qq.com>
2016-09-26 01:48:50 +0800

    ok

--- SqlSugar/Core/ResolveExpressCore/OnlyFieldName.cs
+++ SqlSugar/Core/ResolveExpressCore/OnlyFieldName.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Linq.Expressions;
 
-namespace SqlSugar.Core.ResolveExpressCore
+namespace SqlSugar
 {
     internal partial class ResolveExpress
     {
