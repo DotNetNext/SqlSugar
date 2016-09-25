6cbc1bd74ca2bcf14a013cedd8848931e788a826
sunkaixuan <610262374@qq.com>
2016-09-26 02:14:03 +0800

    ok

---
+++ SqlSugar/Core/ResolveExpressCore/Constant.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SqlSugar
+{
+    internal partial class ResolveExpress
+    {
+        /// <summary>
+        /// 解析bool类型用到的字典
+        /// </summary>
+        public const List<ExpressConstantBoolModel> ConstantBoolDictionary = new List<ExpressConstantBoolModel>()
+        {
+               new ExpressConstantBoolModel(){ Key=Guid.NewGuid(), OldValue="True", Type=SqlSugarTool.StringType},
+               new ExpressConstantBoolModel(){ Key=Guid.NewGuid(), OldValue="False",Type=SqlSugarTool.StringType},
+               new ExpressConstantBoolModel(){ Key=Guid.NewGuid(), OldValue="True",Type=SqlSugarTool.BoolType},
+               new ExpressConstantBoolModel(){ Key=Guid.NewGuid(), OldValue="False",Type=SqlSugarTool.BoolType}
+
+        };
+    }
+}
--- SqlSugar/Core/ResolveExpressCore/Model.cs
+++ SqlSugar/Core/ResolveExpressCore/Model.cs
@@ -16,5 +16,32 @@ namespace SqlSugar
             Key = 1,
             Value = 2
         }
+        /// <summary>
+        /// 表达式常量用来处理bool的实体
+        /// </summary>
+        public class ExpressConstantBoolModel
+        {
+
+            public Guid Key { get; set; }
+
+            public Type Type { get; set; }
+
+            public string OldValue { get; set; }
+
+            public string NewValue
+            {
+                get
+                {
+                    if (Type == SqlSugarTool.BoolType)
+                    {
+                        return Convert.ToBoolean(OldValue) ? "1" : "0";
+                    }
+                    else
+                    {
+                        return OldValue.ToString();
+                    }
+                }
+            }
+        }
     }
 }
