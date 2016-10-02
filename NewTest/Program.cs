88ddb446aea714baa5cbbac372e627c3f5cd4f0d
sunkaixuan <610262374@qq.com>
2016-10-02 21:04:52 +0800

    ok

--- NewTest/Program.cs
+++ NewTest/Program.cs
@@ -58,6 +58,7 @@ namespace NewTest
                 //流水号功能
                 case "serialnumber": demo = new SerialNumber(); break;
                 //多语言支持 没写例子
+                //多库并行计算 没写例子
 
                 /****************************支持**************************************/
                 //公开含数
