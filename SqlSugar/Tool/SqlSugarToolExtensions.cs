d00606475db956fcaa866fa3dda3f778ec01d702
sunkaixuan <610262374@qq.com>
2016-10-05 15:26:36 +0800

    ok

--- SqlSugar/Tool/SqlSugarToolExtensions.cs
+++ SqlSugar/Tool/SqlSugarToolExtensions.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
-using System.Web;
 using System.Data.SqlClient;
 
 namespace SqlSugar
