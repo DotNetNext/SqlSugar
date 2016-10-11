7cd06f7bb8629769ef48ec4b16e0b90474d3f917
sunkaixuan <610262374@qq.com>
2016-10-12 03:09:52 +0800

    ok

---
+++ PkDapper/Demos/Page.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.SqlClient;
+using Dapper;
+using Dapper.Contrib;
+using Dapper.Contrib.Extensions;
+using Models.PkDapper;
+using SqlSugar;
+using SyntacticSugar;
+namespace PkDapper.Demos
+{
+    public class Page : IDemos
+    {
+ 
+        public void Init()
+        {
+            Console.WriteLine("分页");
+
+            var eachCount = 3000;
+
+            /*******************车轮战是性能评估最准确的一种方式***********************/
+            for (int i = 0; i < 10; i++)
+            {
+
+                //dapper
+                Dapper(eachCount);
+
+                //sqlSugar
+                SqlSugar(eachCount);
+            }
+
+        }
+
+        private static void SqlSugar(int eachCount)
+        {
+            GC.Collect();//回收资源
+            System.Threading.Thread.Sleep(2000);//休息2秒
+
+            PerHelper.Execute(eachCount, "SqlSugar", () =>
+            {
+                using (SqlSugarClient conn = new SqlSugarClient(PubConst.connectionString))
+                {
+                    var list = conn.SqlQuery<Test>("select * from(select *,row_number() over(order by id) as r from test  ) t where t.r between @b and @e ", new { b = 1000, e = 1010 });
+                }
+            });
+        }
+
+        private static void Dapper(int eachCount)
+        {
+            GC.Collect();//回收资源
+            System.Threading.Thread.Sleep(2000);//休息2秒
+
+            //正试比拼
+            PerHelper.Execute(eachCount, "Dapper", () =>
+            {
+                using (SqlConnection conn = new SqlConnection(PubConst.connectionString))
+                {
+                    var list = conn.Query<Test>("select * from(select *,row_number() over(order by id) as r from test  ) t where t.r between @b and @e ", new { b = 1000, e = 1010 });
+                }
+            });
+        }
+    }
+}
--- PkDapper/Program.cs
+++ PkDapper/Program.cs
@@ -18,7 +18,7 @@ namespace PkDapper
             WarmUp wu = new WarmUp();//预热处理
 
              //设置执行的DEMO
-            string switchOn = "1";
+            string switchOn = "6";
             IDemos demo = null;
             switch (switchOn)
             {
@@ -37,11 +37,14 @@ namespace PkDapper
                 //比拼批量删除
                 case "5": demo = new DeleteArray(); break;
 
+                //分页
+                case "6": demo = new Page(); break;
+
                 //比拼普通插入
-                case "6": demo = new InsertItem(); break;
+                case "7": demo = new InsertItem(); break;
 
                 //比拼普通更新
-                case "7": demo = new UpdateItem(); break;
+                case "8": demo = new UpdateItem(); break;
                 default: Console.WriteLine("switchOn的值错误，请输入正确的 case"); break;
             }
             demo.Init();
