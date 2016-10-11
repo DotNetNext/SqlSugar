85e052ee9c149167614782ece62ea81be2257683
sunkaixuan <610262374@qq.com>
2016-10-11 17:58:54 +0800

    ok

--- PkDapper/Program.cs
+++ PkDapper/Program.cs
@@ -18,22 +18,28 @@ namespace PkDapper
             WarmUp wu = new WarmUp();//预热处理
 
              //设置执行的DEMO
-            string switchOn = "2";
+            string switchOn = "1";
             IDemos demo = null;
             switch (switchOn)
             {
                 //查询所有
                 case "1": demo = new SelectBigData(); break;
+
                 //查询单条
                 case "2": demo = new SelectSingle(); break;
+
                 //比拼海量数据更新
                 case "3": demo = new UpdateList(); break;
+
                 //比拼海量数据插入
                 case "4": demo = new InsertList(); break;
+
                 //比拼批量删除
                 case "5": demo = new DeleteArray(); break;
+
                 //比拼普通插入
                 case "6": demo = new InsertItem(); break;
+
                 //比拼普通更新
                 case "7": demo = new UpdateItem(); break;
                 default: Console.WriteLine("switchOn的值错误，请输入正确的 case"); break;
