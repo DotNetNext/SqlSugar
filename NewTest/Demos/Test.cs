a7dfb7d8c7d8d73333ec010ff5dfdbaac1864ef2
sunkaixuan <610262374@qq.com>
2016-10-13 05:36:55 +0800

    ok

--- NewTest/Demos/Test.cs
+++ NewTest/Demos/Test.cs
@@ -22,11 +22,23 @@ namespace NewTest.Demos
             {
 
                 //解析拉姆达各种情况组合
+
+                //基本
                 var t1 = db.Queryable<Student>().Where(it => it.id == 1).ToList();
                 var t2 = db.Queryable<Student>().Where(it => it.id == p.id).ToList();
                 var t3 = db.Queryable<Student>().Where(it => it.id == _p.id).ToList();
                 var t4 = db.Queryable<Student>().Where(it => it.id == Getp().id).ToList();
                 var t5 = db.Queryable<Student>().Where(it => it.id == MyCost.p.id).ToList();
+
+
+                //BOOL
+                var t11 = db.Queryable<Student2>("Student").Where(it => it.isOk).ToList();
+                var t21 = db.Queryable<Student2>("Student").Where(it => it.isOk==MyCost.p.isOk).ToList();
+                var t31 = db.Queryable<Student2>("Student").Where(it => !it.isOk).ToList();
+                var t41 = db.Queryable<Student2>("Student").Where(it => it.isOk==true).ToList();
+                var t51 = db.Queryable<Student2>("Student").Where(it =>it.isOk==false).ToList();
+                var t61 = db.Queryable<Student2>("Student").Where(it => it.isOk == _p.isOk).ToList();
+                var t71 = db.Queryable<Student2>("Student").Where(it => it.isOk == !p.isOk).ToList();
 
             }
         }
