5bca52194617de4fb58fdce63f9ced5bf30822b9
sunkaixuan <610262374@qq.com>
2016-10-13 01:40:22 +0800

    ok

--- NewTest/Demos/Select.cs
+++ NewTest/Demos/Select.cs
@@ -41,9 +41,11 @@ namespace NewTest.Demos
             using (var db = SugarDao.GetInstance())
             {
 
+
                 //特殊拉姆达写法测试
+               // var ts0 = db.Queryable<Student>().Where(it =>it.name.Length==1).ToList();
                 var ts1 = db.Queryable<Student2>("Student").Where(it => it.isOk).ToList();
-                var ts2 = db.Queryable<Student2>("Student").Where(it => it.isOk&&!it.isOk).ToList();
+                var ts2 = db.Queryable<Student2>("Student").Where(it => it.isOk&&!it.isOk&&true&&!true).ToList();
                 var ts3= db.Queryable<Student2>("Student").Where(it => it.isOk && it.isOk).ToList();
                 var ts4 = db.Queryable<Student2>("Student").Where(it => !it.isOk).ToList();
 
