80bfc200a33a3ae538feab8191e6155b092755a3
sunkaixuan <610262374@qq.com>
2016-10-14 00:46:02 +0800

    ok

--- SqlSugar/PubModel/PubEnum.cs
+++ SqlSugar/PubModel/PubEnum.cs
@@ -10,8 +10,17 @@ namespace SqlSugar
     /// </summary>
     public enum JoinType
     {
+        /// <summary>
+        /// 等值连接
+        /// </summary>
         INNER = 0,
+        /// <summary>
+        /// 左外连
+        /// </summary>
         LEFT = 1,
+        /// <summary>
+        /// 右外连
+        /// </summary>
         RIGHT = 2
     }
     /// <summary>
@@ -19,7 +28,13 @@ namespace SqlSugar
     /// </summary>
     public enum ApplyType
     {
+        /// <summary>
+        /// 笛卡尔积
+        /// </summary>
         CORSS = 1,
+        /// <summary>
+        /// 外连
+        /// </summary>
         OUTRE = 2
     }
     /// <summary>
@@ -27,7 +42,13 @@ namespace SqlSugar
     /// </summary>
     public enum OrderByType
     {
+        /// <summary>
+        /// 升序
+        /// </summary>
         asc = 0,
+        /// <summary>
+        /// 降序
+        /// </summary>
         desc = 1
     }
     /// <summary>
@@ -35,7 +56,13 @@ namespace SqlSugar
     /// </summary>
     public enum PageModel
     {
+        /// <summary>
+        /// 05分页
+        /// </summary>
         RowNumber = 0,
+        /// <summary>
+        /// 12分页
+        /// </summary>
         Offset = 1
     }
     /// <summary>
