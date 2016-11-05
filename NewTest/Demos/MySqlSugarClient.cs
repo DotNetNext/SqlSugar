a402163ae845e9046ffeb9e0fa6d24aaae0f5ba5
sunkaixuan <610262374@qq.com>
2016-11-05 19:35:50 +0800

    Add Demo

---
+++ NewTest/Demos/MySqlSugarClient.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SqlSugar;
+
+namespace NewTest.Demos
+{
+    /// <summary>
+    /// 重写SqlSugar底层
+    /// </summary>
+    public class MySqlSugarClient:SqlSugarClient
+    {
+        public override System.Data.DataSet GetDataSetAll(string sql, object pars)
+        {
+            return base.GetDataSetAll(sql, pars);
+        }
+
+        public override void BeginTran(System.Data.IsolationLevel iso, string transactionName)
+        {
+            base.BeginTran(iso, transactionName);
+        }
+
+        public override int ExecuteCommand(string sql, object pars)
+        {
+            return base.ExecuteCommand(sql, pars);
+        }
+    }
+}
