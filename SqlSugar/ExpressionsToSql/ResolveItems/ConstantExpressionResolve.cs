cafc76483ab63a72710f3e7b249a668215f1e718
sunkaixuan <610262374@qq.com>
2017-01-08 15:41:54 +0800

    -

--- SqlSugar/ExpressionsToSql/ResolveItems/ConstantExpressionResolve.cs
+++ SqlSugar/ExpressionsToSql/ResolveItems/ConstantExpressionResolve.cs
@@ -5,11 +5,28 @@ using System.Linq.Expressions;
 using System.Text;
 namespace SqlSugar
 {
-    public class ConstantExpressionResolve:BaseResolve
+    public class ConstantExpressionResolve : BaseResolve
     {
         public ConstantExpressionResolve(ExpressionParameter parameter) : base(parameter)
         {
-
+            var expression = base.Expression as MemberExpression;
+            var isLeft = parameter.IsLeft;
+            var isSingle = parameter.Context.IsSingle;
+            string value = string.Empty;
+            if (parameter.BaseParameter.BinaryExpressionInfoList != null)
+            {
+                parameter.BaseParameter.BinaryExpressionInfoList.Add(new KeyValuePair<string, BinaryExpressionInfo>(ExpressionConst.BinaryExpressionInfoListKey, new BinaryExpressionInfo()
+                {
+                    IsLeft = Convert.ToBoolean(IsLeft),
+                    Value = value,
+                    ExpressionType = expression.GetType()
+                }));
+            }
+            if (isLeft == null && base.SqlWhere == null)
+            {
+                base.SqlWhere = new StringBuilder();
+                base.SqlWhere.Append(value);
+            }
         }
     }
 }
--- SqlSugar/ExpressionsToSql/ResolveItems/MemberExpressionResolve.cs
+++ SqlSugar/ExpressionsToSql/ResolveItems/MemberExpressionResolve.cs
@@ -14,12 +14,13 @@ namespace SqlSugar
             var isSingle = parameter.Context.IsSingle;
             string fieldName = string.Empty;
             fieldName = isSingle ? expression.Member.Name : expression.Member.ToString();
-            parameter.BaseParameter.BinaryExpressionInfoList.Add(new KeyValuePair<string, BinaryExpressionInfo>(ExpressionConst.BinaryExpressionInfoListKey, new BinaryExpressionInfo()
-            {
-                IsLeft = Convert.ToBoolean(IsLeft),
-                Value = fieldName,
-                ExpressionType = expression.GetType()
-            }));
+            if (parameter.BaseParameter.BinaryExpressionInfoList != null)
+                parameter.BaseParameter.BinaryExpressionInfoList.Add(new KeyValuePair<string, BinaryExpressionInfo>(ExpressionConst.BinaryExpressionInfoListKey, new BinaryExpressionInfo()
+                {
+                    IsLeft = Convert.ToBoolean(IsLeft),
+                    Value = fieldName,
+                    ExpressionType = expression.GetType()
+                }));
             if (isLeft == null && base.SqlWhere == null)
             {
                 base.SqlWhere = new StringBuilder();
