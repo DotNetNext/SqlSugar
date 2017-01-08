6dff7607a5333f739ff8899ec6f0a890969f01c0
sunkaixuan <610262374@qq.com>
2017-01-08 23:10:48 +0800

    -

--- SqlSugar/ExpressionsToSql/ResolveItems/MemberNewExpressionResolve.cs
+++ SqlSugar/ExpressionsToSql/ResolveItems/MemberNewExpressionResolve.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
-
 namespace SqlSugar
 {
     public class MemberNewExpressionResolve : BaseResolve
