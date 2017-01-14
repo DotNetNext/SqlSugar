a81934027dff392ec27bfe19e3cc821a6084e5bb
sunkaixuan <610262374@qq.com>
2017-01-14 23:19:08 +0800

    -

--- SqlSugar/ExpressionsToSql/Common/ExpressionErrorMessage.cs
+++ SqlSugar/ExpressionsToSql/Common/ExpressionErrorMessage.cs
@@ -11,16 +11,14 @@ namespace SqlSugar
         {
             get
             {
-                return ExpressionConst.GetThrowMessage("拉姆达解析出错：不支持{0}此种运算符查找！",
-                                       "Lambda parsing error: {0} does not support the operator to find!");
+                return ExpressionConst.GetThrowMessage("Lambda parsing error: {0} does not support the operator to find!","拉姆达解析出错：不支持{0}此种运算符查找！");
             }
         }
         internal static string ExpFileldError
         {
             get
             {
-                return ExpressionConst.GetThrowMessage("Expression format error, correct format: it=>it.fieldName",
-                                       "表达示格式错误，正确格式： it=>it.fieldName");
+                return ExpressionConst.GetThrowMessage("Expression format error, correct format: it=>it.fieldName","表达示格式错误，正确格式： it=>it.fieldName");
             }
         }
     }
