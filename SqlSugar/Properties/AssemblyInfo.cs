6eceab3a885508b0d48211e1342116d5cf550e6f
sunkaixuan <610262374@qq.com>
2017-02-26 21:00:48 +0800

    -

--- SqlSugar/Properties/AssemblyInfo.cs
+++ SqlSugar/Properties/AssemblyInfo.cs
@@ -5,14 +5,14 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SqlSugar")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("SqlSugar 4.x")]
+[assembly: AssemblyDescription("author:sunkaixuan,Apache Licence 2.0")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SqlSugar")]
+[assembly: AssemblyProduct("SqlSugar 4.x")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+[assembly: AssemblyCulture("sqlsugar 4.x")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
