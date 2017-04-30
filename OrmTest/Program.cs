c3b3d4e71f1872ec62b7a6e7693487efb44fb1f0
sunkaixuan <610262374@qq.com>
2017-04-30 17:26:45 +0800

    -

--- OrmTest/Program.cs
+++ OrmTest/Program.cs
@@ -19,13 +19,13 @@ namespace OrmTest
         {
             //Unit Test
             int eachCount = 1;
-            new Field(eachCount).Init();
-            new Where(eachCount).Init();
-            new Method(eachCount).Init();
-            new JoinQuery(eachCount).Init();
-            new SingleQuery(eachCount).Init();
-            new SelectQuery(eachCount).Init();
-
+            //new Field(eachCount).Init();
+            //new Where(eachCount).Init();
+            //new Method(eachCount).Init();
+            //new JoinQuery(eachCount).Init();
+            //new SingleQuery(eachCount).Init();
+            //new SelectQuery(eachCount).Init();
+            new MapTable().Init();
 
             //Performance Test
             for (int i = 0; i < 100; i++)
---
+++ OrmTest/UnitTest/RenameMapping/MapTable.cs
@@ -0,0 +1,26 @@
+﻿using OrmTest.Models;
+using SqlSugar;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrmTest.UnitTest
+{
+    public class MapTable : ExpTestBase
+    {
+        public void Init()
+        {
+            using (var db = GetInstance())
+            {
+               var x= db.Queryable<Student>().ToList();
+            }
+        }
+        public SqlSugarClient GetInstance()
+        {
+            SqlSugarClient db = new SqlSugarClient(new SystemTablesConfig() { ConnectionString = Config.ConnectionString, DbType = DbType.SqlServer, IsAutoCloseConnection = true });
+            return db;
+        }
+    }
+}
