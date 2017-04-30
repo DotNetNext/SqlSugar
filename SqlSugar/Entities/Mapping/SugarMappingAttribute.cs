d90e76fca8e85453699f1efe4e8356f1543b4ad3
sunkaixuan <610262374@qq.com>
2017-04-30 17:29:23 +0800

    -

--- SqlSugar/Entities/Mapping/SugarMappingAttribute.cs
+++ SqlSugar/Entities/Mapping/SugarMappingAttribute.cs
@@ -1,8 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Reflection;
+using System;
 
 namespace SqlSugar
 {
