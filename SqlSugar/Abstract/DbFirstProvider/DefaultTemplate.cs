0bd18a370c9d83e317c98931dc4868289c4aeab9
sunkaixuan <610262374@qq.com>
2017-05-28 22:01:36 +0800

    -

--- SqlSugar/Abstract/DbFirstProvider/DefaultTemplate.cs
+++ SqlSugar/Abstract/DbFirstProvider/DefaultTemplate.cs
@@ -7,11 +7,36 @@ namespace SqlSugar.Abstract.DbFirstProvider
 {
     public class DefaultTemplate
     {
-        public static string ClassTemplate { get; set; }
-        public static string ClassDescriptionTemplate { get; set; }
-        public static string PropertyTemplate { get; set; }
-        public static string PropertyDescriptionTemplate { get; set; }
-        public static string ConstructorTemplate { get; set; }
-        public static string NamespaceTemplate { get; set; }
+        public static string ClassTemplate = @"namespace {Namespace}
+{
+    {ClassPropertyDescription}
+    {SugarTable}
+    public class {ClassName}
+    {
+        public {ClassName}(){
+           {Constructor}
+        }
+        {PropertyDescription}
+        {Property}
+    }
+}";
+        public static string ClassDescriptionTemplate = @"    /// <summary>
+    ///{ClassDescription}
+    /// </summary>\r\n";
+
+        public static string PropertyTemplate = @"           {SugarColumn}
+public {PropertyType} {PropertyName} {get;set;}\r\n";
+
+        public static string PropertyDescriptionTemplate = @"/// <summary>
+        /// Desc:{PropertyDescription}
+        /// Default:{DefaultValue}
+        /// Nullable:{IsNullable}
+        /// </summary>\r\n";
+
+        public static string ConstructorTemplate = @"           this.$PropertyName =Convert.To$PropertyType(""{0}"")\r\n";
+
+        public static string NamespaceTemplate = @"using System;
+        using System.Linq;
+        using System.Text;\r\n";
     }
 }
