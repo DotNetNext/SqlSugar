ba12acef8c3b2885d2875387f3737ae8d1cdfd9a
sunkaixuan <610262374@qq.com>
2017-05-28 10:04:53 +0800

    -

--- SqlSugar/Entities/ConnectionConfig.cs
+++ SqlSugar/Entities/ConnectionConfig.cs
@@ -24,15 +24,4 @@ namespace SqlSugar
         /// </summary>
         public InitKeyType InitKeyType = InitKeyType.SystemTable;
     }
-    public enum InitKeyType
-    {
-        /// <summary>
-        /// Init primary key and identity key from the system table 
-        /// </summary>
-        SystemTable = 0,
-        /// <summary>
-        /// Init primary key and identity key from the attribute 
-        /// </summary>
-        Attribute = 1
-    }
 }
---
+++ SqlSugar/Enum/InitKeyType.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SqlSugar
+{
+    public enum InitKeyType
+    {
+        /// <summary>
+        /// Init primary key and identity key from the system table 
+        /// </summary>
+        SystemTable = 0,
+        /// <summary>
+        /// Init primary key and identity key from the attribute 
+        /// </summary>
+        Attribute = 1
+    }
+}
