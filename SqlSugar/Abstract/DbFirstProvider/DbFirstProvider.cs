2a97c87085aa79a35e8796c63a2218478be340bd
sunkaixuan <610262374@qq.com>
2017-05-28 20:56:45 +0800

    -

--- SqlSugar/Abstract/DbFirstProvider/DbFirstProvider.cs
+++ SqlSugar/Abstract/DbFirstProvider/DbFirstProvider.cs
@@ -7,5 +7,69 @@ namespace SqlSugar
     public abstract partial class DbFirstProvider : IDbFirst
     {
         public virtual SqlSugarClient Context { get; set; }
+
+        public List<SchemaInfo> GetSchemaInfoList
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        #region Setting Template
+        public IDbFirst SettingClassDescriptionTemplate(Func<string> func)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IDbFirst SettingClassTemplate(Func<string> func)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IDbFirst SettingConstructorTemplate(Func<string> func)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IDbFirst SettingPropertyDescriptionTemplate(Func<string> func)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IDbFirst SettingPropertyTemplate(Func<string> func)
+        {
+
+           throw new NotImplementedException();
+        }
+        #endregion
+
+        #region Where
+        public IDbFirst Where(DbObjectType dbObjectType)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IDbFirst Where(Func<string, bool> func)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IDbFirst Where(params string[] objectNames)
+        {
+            throw new NotImplementedException();
+        } 
+        #endregion
+
+        public void ToClassStringList()
+        {
+            throw new NotImplementedException();
+        }
+        public void CreateClassFile()
+        {
+            throw new NotImplementedException();
+        }
+
+
     }
 }
--- SqlSugar/Interface/IDbFirst.cs
+++ SqlSugar/Interface/IDbFirst.cs
@@ -7,5 +7,16 @@ namespace SqlSugar
     public partial interface IDbFirst
     {
         SqlSugarClient Context { get; set; }
+        IDbFirst SettingClassTemplate(Func<string> func);
+        IDbFirst SettingClassDescriptionTemplate(Func<string> func);
+        IDbFirst SettingPropertyTemplate(Func<string> func);
+        IDbFirst SettingPropertyDescriptionTemplate(Func<string> func);
+        IDbFirst SettingConstructorTemplate(Func<string> func);
+        IDbFirst Where(params string[] objectNames);
+        IDbFirst Where(Func<string,bool> func);
+        IDbFirst Where(DbObjectType dbObjectType);
+        List<SchemaInfo> GetSchemaInfoList { get; }
+        void CreateClassFile();
+        void ToClassStringList();
     }
 }
