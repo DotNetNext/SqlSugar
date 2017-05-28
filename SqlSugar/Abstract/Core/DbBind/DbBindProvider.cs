2f3a6c7604e7e0efa1a5755b9dc7fcf8b27d3736
sunkaixuan <610262374@qq.com>
2017-05-28 19:00:34 +0800

    -

--- SqlSugar/Abstract/Core/DbBind/DbBindProvider.cs
+++ SqlSugar/Abstract/Core/DbBind/DbBindProvider.cs
@@ -9,7 +9,38 @@ namespace SqlSugar
 {
     public abstract partial class DbBindProvider : DbBindAccessory, IDbBind
     {
-        public virtual SqlSugarClient Context { get; set; }
+        #region Properties
+        public virtual SqlSugarClient Context { get; set; } 
+        #endregion
+
+        #region Public methods
+        public virtual List<T> DataReaderToList<T>(Type type, IDataReader dataReader, string fields)
+        {
+            using (dataReader)
+            {
+                if (type.Name.Contains("KeyValuePair"))
+                {
+                    return GetKeyValueList<T>(type, dataReader);
+                }
+                else if (type.IsValueType || type == PubConst.StringType)
+                {
+                    return GetValueTypeList<T>(type, dataReader);
+                }
+                else if (type.IsArray)
+                {
+                    return GetArrayList<T>(type, dataReader);
+                }
+                else
+                {
+                    return GetEntityList<T>(type, Context, dataReader, fields);
+                }
+            }
+        }
+        public abstract string ChangeDBTypeToCSharpType(string typeName);
+
+        #endregion
+
+        #region Throw rule
         public virtual List<string> GuidThrow
         {
             get
@@ -59,28 +90,6 @@ namespace SqlSugar
                 return new List<string>() { "datetime", "guid" };
             }
         }
-        public virtual List<T> DataReaderToList<T>(Type type, IDataReader dataReader,string fields)
-        {
-            using (dataReader)
-            {
-                if (type.Name.Contains("KeyValuePair"))
-                {
-                    return GetKeyValueList<T>(type, dataReader);
-                }
-                else if (type.IsValueType || type == PubConst.StringType)
-                {
-                    return GetValueTypeList<T>(type, dataReader);
-                }
-                else if (type.IsArray)
-                {
-                    return GetArrayList<T>(type, dataReader);
-                }
-                else
-                {
-                    return GetEntityList<T>(type, Context, dataReader, fields);
-                }
-            }
-        }
-        public abstract string ChangeDBTypeToCSharpType(string typeName);
+        #endregion
     }
 }
