da24a13df7ed9431b438b161263a6f22beddd28b
sunkaixuan <610262374@qq.com>
2017-05-31 10:45:43 +0800

    -

--- SqlSugar/Entities/EntityInfo.cs
+++ SqlSugar/Entities/EntityInfo.cs
@@ -9,8 +9,9 @@ namespace SqlSugar
 {
     public class EntityInfo
     {
+        private string _DbTableName;
         public string EntityName { get; set; }
-        public string DbTableName { get; set; }
+        public string DbTableName { get { return _DbTableName == null ? EntityName : _DbTableName;  } set { _DbTableName = value; } }
         public Type Type { get; set; }
         public List<EntityColumnInfo> Columns { get; set; }
     }
