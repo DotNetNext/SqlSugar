fcf85ec3c8df86ece433587d586f4c54e885629c
sunkaixuan <610262374@qq.com>
2017-06-23 00:31:53 +0800

    Update Core

--- Src/Asp.NetCore/SqlServerTest/src/SqlSugar/OnlyCore/CoreMethod.cs
+++ Src/Asp.NetCore/SqlServerTest/src/SqlSugar/OnlyCore/CoreMethod.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using System.Threading.Tasks;
-using Microsoft.Extensions.DependencyModel;
 
 namespace SqlSugar
 {
@@ -11,9 +10,7 @@ namespace SqlSugar
     {
         public static Assembly LoadAssembly(string name)
         {
-            var deps = DependencyContext.Default;
-            var assemblyies = deps.CompileLibraries.Where(it => it.Name == name).ToList();
-            return assemblyies.Any() ? Assembly.Load(new AssemblyName(assemblyies.First().Name)) : null;
+            return  Assembly.Load(new AssemblyName(PubConst.AssemblyName)) ;
         }
     }
 }
