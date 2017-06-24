87204a6f6cbb04addba179a10dea48e4bbd0b3f0
sunkaixuan <610262374@qq.com>
2017-06-25 06:48:41 +0800

    Code First Update

--- Src/Asp.Net/SqlSugar/Abstract/CodeFirstProvider/CodeFirstProvider.cs
+++ Src/Asp.Net/SqlSugar/Abstract/CodeFirstProvider/CodeFirstProvider.cs
@@ -78,7 +78,7 @@ namespace SqlSugar
             List<DbColumnInfo> columns = new List<DbColumnInfo>();
             if (entityInfo.Columns.IsValuable())
             {
-                foreach (var item in entityInfo.Columns)
+                foreach (var item in entityInfo.Columns.Where(it=>it.IsIgnore==false))
                 {
                     DbColumnInfo dbColumnInfo = EntityColumnToDbColumn(entityInfo, tableName, item);
                     columns.Add(dbColumnInfo);
