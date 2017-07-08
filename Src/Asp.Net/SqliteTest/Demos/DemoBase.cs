a50972b8dbcf60738c2515c23405f273271451f6
sunkaixuan <610262374@qq.com>
2017-07-09 00:34:56 +0800

    Update Sqlite

---
+++ Src/Asp.Net/SqliteTest/Demos/DemoBase.cs
@@ -0,0 +1,23 @@
+﻿using SqlSugar;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OrmTest.Demo
+{
+    public class DemoBase
+    {
+        public  static SqlSugarClient GetInstance()
+        {
+            SqlSugarClient db = new SqlSugarClient(new ConnectionConfig() { ConnectionString = Config.ConnectionString, DbType = DbType.Sqlite, IsAutoCloseConnection = true });
+            db.Ado.IsEnableLogEvent = true;
+            db.Ado.LogEventStarting = (sql, pars) =>
+            {
+                Console.WriteLine(sql + "\r\n" + db.RewritableMethods.SerializeObject(pars));
+                Console.WriteLine();
+            };
+            return db;
+        }
+    }
+}
