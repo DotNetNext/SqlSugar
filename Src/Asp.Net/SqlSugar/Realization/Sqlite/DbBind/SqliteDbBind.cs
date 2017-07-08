3f9c51aebeded6926e1eff76bc9f07d423532b7f
sunkaixuan <610262374@qq.com>
2017-07-08 18:42:03 +0800

    Update Sqlite

--- Src/Asp.Net/SqlSugar/Realization/Sqlite/DbBind/SqliteDbBind.cs
+++ Src/Asp.Net/SqlSugar/Realization/Sqlite/DbBind/SqliteDbBind.cs
@@ -31,24 +31,29 @@ namespace SqlSugar
                 {
 
                     new KeyValuePair<string, CSharpDataType>("int",CSharpDataType.@int),
-                    new KeyValuePair<string, CSharpDataType>("mediumint",CSharpDataType.@int),
                     new KeyValuePair<string, CSharpDataType>("integer",CSharpDataType.@int),
+                    new KeyValuePair<string, CSharpDataType>("int32",CSharpDataType.@int),
+                    new KeyValuePair<string, CSharpDataType>("integer32",CSharpDataType.@int),
 
                     new KeyValuePair<string, CSharpDataType>("varchar",CSharpDataType.@string),
                     new KeyValuePair<string, CSharpDataType>("text",CSharpDataType.@string),
                     new KeyValuePair<string, CSharpDataType>("char",CSharpDataType.@string),
-                    new KeyValuePair<string, CSharpDataType>("enum",CSharpDataType.@string),
 
                     new KeyValuePair<string, CSharpDataType>("tinyint",CSharpDataType.@byte),
+                    new KeyValuePair<string, CSharpDataType>("UNSIGNEDINTEGER8",CSharpDataType.@byte),
                     new KeyValuePair<string, CSharpDataType>("smallint",CSharpDataType.@short),
+                    new KeyValuePair<string, CSharpDataType>("int16",CSharpDataType.@short),
                     new KeyValuePair<string, CSharpDataType>("bigint",CSharpDataType.@long),
+                    new KeyValuePair<string, CSharpDataType>("int64",CSharpDataType.@long),
+                    new KeyValuePair<string, CSharpDataType>("integer64",CSharpDataType.@long),
                     new KeyValuePair<string, CSharpDataType>("bit",CSharpDataType.@bool),
+                    new KeyValuePair<string, CSharpDataType>("bool",CSharpDataType.@bool),
+                    new KeyValuePair<string, CSharpDataType>("boolean",CSharpDataType.@bool),
                     new KeyValuePair<string, CSharpDataType>("real",CSharpDataType.@double),
                     new KeyValuePair<string, CSharpDataType>("double",CSharpDataType.@double),
                     new KeyValuePair<string, CSharpDataType>("float",CSharpDataType.@float),
                     new KeyValuePair<string, CSharpDataType>("decimal",CSharpDataType.@decimal),
                     new KeyValuePair<string, CSharpDataType>("numeric",CSharpDataType.@decimal),
-                    new KeyValuePair<string, CSharpDataType>("year",CSharpDataType.@int),
 
                     new KeyValuePair<string, CSharpDataType>("datetime",CSharpDataType.DateTime),
                     new KeyValuePair<string, CSharpDataType>("timestamp",CSharpDataType.DateTime),
@@ -56,15 +61,12 @@ namespace SqlSugar
                     new KeyValuePair<string, CSharpDataType>("time",CSharpDataType.DateTime),
 
                     new KeyValuePair<string, CSharpDataType>("blob",CSharpDataType.byteArray),
-                    new KeyValuePair<string, CSharpDataType>("tinyblob",CSharpDataType.byteArray),
-                    new KeyValuePair<string, CSharpDataType>("varbinary",CSharpDataType.byteArray),
+                    new KeyValuePair<string, CSharpDataType>("raw",CSharpDataType.byteArray),
+                    new KeyValuePair<string, CSharpDataType>("oleobject",CSharpDataType.byteArray),
                     new KeyValuePair<string, CSharpDataType>("binary",CSharpDataType.byteArray),
-                    new KeyValuePair<string, CSharpDataType>("multipoint",CSharpDataType.byteArray),
-                    new KeyValuePair<string, CSharpDataType>("geometry",CSharpDataType.byteArray),
-                    new KeyValuePair<string, CSharpDataType>("multilinestring",CSharpDataType.byteArray),
-                    new KeyValuePair<string, CSharpDataType>("polygon",CSharpDataType.byteArray),
 
                     new KeyValuePair<string, CSharpDataType>("varchar",CSharpDataType.Guid),
+                    new KeyValuePair<string, CSharpDataType>("guid",CSharpDataType.Guid)
                 };
             }
         }
