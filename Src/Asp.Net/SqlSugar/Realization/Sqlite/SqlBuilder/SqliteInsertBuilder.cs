2a900481fd0467f64083f8e5cb65baceef245d4c
sunkaixuan <610262374@qq.com>
2017-07-10 01:01:31 +0800

    Upadte Sqlite

--- Src/Asp.Net/SqlSugar/Realization/Sqlite/SqlBuilder/SqliteInsertBuilder.cs
+++ Src/Asp.Net/SqlSugar/Realization/Sqlite/SqlBuilder/SqliteInsertBuilder.cs
@@ -1,4 +1,6 @@
-﻿namespace SqlSugar
+﻿using System;
+
+namespace SqlSugar
 {
     public class SqliteInsertBuilder : InsertBuilder
     {
@@ -20,6 +22,47 @@
      VALUES
            ({2}) ;";
 
+                }
+            }
+        }
+
+        public override string SqlTemplateBatch
+        {
+            get
+            {
+                return "INSERT INTO {0} ({1})";
+            }
+        }
+
+        public override object FormatValue(object value)
+        {
+            if (value == null)
+            {
+                return "NULL";
+            }
+            else
+            {
+                var type = value.GetType();
+                if (type == PubConst.DateType)
+                {
+                    var date = value.ObjToDate();
+                    if (date < Convert.ToDateTime("1900-1-1"))
+                    {
+                        date = Convert.ToDateTime("1900-1-1");
+                    }
+                    return "'" + date.ToString("yyyy-MM-dd HH:mm:ss.fff") + "'";
+                }
+                else if (type == PubConst.BoolType)
+                {
+                    return value.ObjToBool() ? "1" : "0";
+                }
+                else if (type == PubConst.StringType || type == PubConst.ObjType)
+                {
+                    return "'" + value.ToString().ToSqlFilter() + "'";
+                }
+                else
+                {
+                    return   "'"+value.ToString() + "'";
                 }
             }
         }
