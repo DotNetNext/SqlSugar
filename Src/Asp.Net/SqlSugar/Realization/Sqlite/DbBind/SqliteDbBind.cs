f56ae15db2d2674192ef131dae24171ede62629d
sunkaixuan <610262374@qq.com>
2017-07-25 14:02:02 +0800

    4.2.2.2

--- Src/Asp.Net/SqlSugar/Realization/Sqlite/DbBind/SqliteDbBind.cs
+++ Src/Asp.Net/SqlSugar/Realization/Sqlite/DbBind/SqliteDbBind.cs
@@ -36,6 +36,9 @@ namespace SqlSugar
                     new KeyValuePair<string, CSharpDataType>("integer32",CSharpDataType.@int),
 
                     new KeyValuePair<string, CSharpDataType>("varchar",CSharpDataType.@string),
+                    new KeyValuePair<string, CSharpDataType>("varchar2",CSharpDataType.@string),
+                    new KeyValuePair<string, CSharpDataType>("nvarchar",CSharpDataType.@string),
+                    new KeyValuePair<string, CSharpDataType>("nvarchar2",CSharpDataType.@string),
                     new KeyValuePair<string, CSharpDataType>("text",CSharpDataType.@string),
                     new KeyValuePair<string, CSharpDataType>("char",CSharpDataType.@string),
                     new KeyValuePair<string, CSharpDataType>("num",CSharpDataType.@string),
