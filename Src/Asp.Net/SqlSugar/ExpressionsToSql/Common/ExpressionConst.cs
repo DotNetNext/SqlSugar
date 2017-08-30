bae50b0b272840dc8335b98ffcb496b203bcf38d
sunkaixuan <610262374@qq.com>
2017-08-30 15:40:38 +0800

    -

--- Src/Asp.Net/SqlSugar/ExpressionsToSql/Common/ExpressionConst.cs
+++ Src/Asp.Net/SqlSugar/ExpressionsToSql/Common/ExpressionConst.cs
@@ -7,16 +7,13 @@ namespace SqlSugar
 {
     internal class ExpressionConst
     {
-        public const string BinaryFormatString = " ( {0} {1} {2} ) ";
+        public const string Format4 = ")";
+        public const string Format3 = "(";
         public const string Format0 = "{0}";
         public const string Format1 = "$__$";
-        public const string Format2 = "o__o";
-        public const string Format3 = "(";
-        public const string Format4 = ")";
-        public const string SqlFuncFullName = "SqlSugar.SqlFunc"; 
-        public const string MethodConst = "MethodConst";
         public const string Const = "Const";
-        public readonly static Type MemberExpressionType = typeof(MemberExpression);
-        public readonly static Type ConstantExpressionType = typeof(ConstantExpression);
+        public const string MethodConst = "MethodConst";
+        public const string SqlFuncFullName = "SqlSugar.SqlFunc";
+        public const string BinaryFormatString = " ( {0} {1} {2} ) ";
     }
 }
