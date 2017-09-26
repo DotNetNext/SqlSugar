20082cf581742f3eb4a9bce11a089303c40fb6f8
sunkaixuan <610262374@qq.com>
2017-09-26 17:17:41 +0800

    -

---
+++ Src/Asp.Net/SqlSugar/Realization/Oracle/Updateable/OracleUpdateable.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SqlSugar
+{
+    public class OracleUpdateable<T>:UpdateableProvider<T> where T:class,new()
+    {
+
+    }
+}
