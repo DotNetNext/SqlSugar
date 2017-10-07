c11b31e053a753c9c7e83f96be2c295ba9cb8448
sunkaixuan <610262374@qq.com>
2017-10-07 11:39:35 +0800

    -

--- Src/Asp.Net/SqlSugar/Realization/Oracle/DbFirst/OracleDbFirst.cs
+++ Src/Asp.Net/SqlSugar/Realization/Oracle/DbFirst/OracleDbFirst.cs
@@ -7,8 +7,6 @@ namespace SqlSugar
 {
     public class OracleDbFirst : DbFirstProvider
     {
-        public OracleDbFirst() {
-            throw new Exception("Oracle该功能还未测试");
-        }
+
     }
 }
