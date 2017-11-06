e309fade001b4ae2ab4e085bd7f3e5e9b778ae67
sunkaixuan <610262374@qq.com>
2017-11-06 11:41:44 +0800

    -

---
+++ Src/Asp.NetCore2/SqlSeverTest/NugetTest/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using SqlSugar;
+namespace NugetTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var db = new SqlSugarClient(new ConnectionConfig() {
+                 ConnectionString=Config.ConnectionString,
+                 IsAutoCloseConnection=true
+            });
+            var list = db.Ado.GetInt("select 1");
+            Console.WriteLine("Hello World!");
+        }
+        public class Config
+        {
+            public static string ConnectionString = "Database=SqlSugar4xTest;Data Source=127.0.0.1;User Id=root;Password=root;pooling=false;CharSet=utf8;port=3306";
+        }
+    }
+}
