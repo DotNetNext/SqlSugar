5aaf29ee939d5ea0d870974ca029258bac34e655
sunkaixuan <610262374@qq.com>
2017-11-30 14:57:22 +0800

    -

--- Src/Asp.Net/SqlSugar/Entities/ModelContext.cs
+++ Src/Asp.Net/SqlSugar/Entities/ModelContext.cs
@@ -13,6 +13,7 @@ namespace SqlSugar
         public SqlSugarClient Context { get; set; }
         public ISugarQueryable<T> CreateMapping<T>() where T : class, new()
         {
+            Check.ArgumentNullException(Context, "Please use Sqlugar.ModelContext");
             using (Context)
             {
                 return Context.Queryable<T>();
