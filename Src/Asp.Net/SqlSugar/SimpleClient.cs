cbc48816d4c5904d11c63e86c1d0d80f9020805f
sunkaixuan <610262374@qq.com>
2018-01-17 13:00:41 +0800

    Update SimpleClient

--- Src/Asp.Net/SqlSugar/SimpleClient.cs
+++ Src/Asp.Net/SqlSugar/SimpleClient.cs
@@ -73,4 +73,71 @@ namespace SqlSugar
             return this.Context.Deleteable<T>().In(ids).ExecuteCommand() > 0;
         }
     }
+    public partial class SimpleClient<T> where T : class, new()
+    {
+        protected SqlSugarClient Context { get; set; }
+        public SqlSugarClient FullClient { get { return this.Context; } }
+
+        private SimpleClient()
+        {
+
+        }
+        public SimpleClient(SqlSugarClient context)
+        {
+            this.Context = context;
+        }
+
+        public T GetById(dynamic id)
+        {
+            return Context.Queryable<T>().InSingle(id);
+        }
+        public List<T> GetList()  
+        {
+            return Context.Queryable<T>().ToList();
+        }
+        public List<T> GetList(Expression<Func<T, bool>> whereExpression)
+        {
+            return Context.Queryable<T>().Where(whereExpression).ToList();
+        }
+        public bool Insert(T insertObj) 
+        {
+            return this.Context.Insertable(insertObj).ExecuteCommand() > 0;
+        }
+        public int InsertReturnIdentity(T insertObj) 
+        {
+            return this.Context.Insertable(insertObj).ExecuteReturnIdentity();
+        }
+        public bool InsertRange(T[] insertObjs) 
+        {
+            return this.Context.Insertable(insertObjs).ExecuteCommand() > 0;
+        }
+        public bool InsertRange(List<T>[] insertObjs)
+        {
+            return this.Context.Insertable(insertObjs).ExecuteCommand() > 0;
+        }
+        public bool Update(T updateObj) 
+        {
+            return this.Context.Updateable(updateObj).ExecuteCommand() > 0;
+        }
+        public bool Update(Expression<Func<T, T>> columns, Expression<Func<T, bool>> whereExpression) 
+        {
+            return this.Context.Updateable<T>().UpdateColumns(columns).Where(whereExpression).ExecuteCommand() > 0;
+        }
+        public bool Delete(T deleteObj)
+        {
+            return this.Context.Deleteable<T>().Where(deleteObj).ExecuteCommand() > 0;
+        }
+        public bool Delete(Expression<Func<T, bool>> whereExpression) 
+        {
+            return this.Context.Deleteable<T>().Where(whereExpression).ExecuteCommand() > 0;
+        }
+        public bool DeleteById(dynamic id) 
+        {
+            return this.Context.Deleteable<T>().In(id).ExecuteCommand() > 0;
+        }
+        public bool DeleteByIds(dynamic[] ids)
+        {
+            return this.Context.Deleteable<T>().In(ids).ExecuteCommand() > 0;
+        }
+    }
 }
