37767e5b4ab20fbf7e344005c16e21973718ad99
sunkaixuan <610262374@qq.com>
2018-03-20 17:34:58 +0800

    Update Demo

--- Src/Asp.NetCore2/SqlSeverTest/NugetTest/Program.cs
+++ Src/Asp.NetCore2/SqlSeverTest/NugetTest/Program.cs
@@ -8,7 +8,8 @@ namespace NugetTest
         {
             var db = new SqlSugarClient(new ConnectionConfig() {
                  ConnectionString=Config.ConnectionString,
-                 IsAutoCloseConnection=true
+                 IsAutoCloseConnection=true,
+                 DbType=DbType.Oracle
             });
             var list = db.Ado.GetInt("select 1");
             Console.WriteLine("Hello World!");
