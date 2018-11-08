1d88e2b55b2fc606990ce6c14aa3a1419bd33fc9
610262374@qq.com <610262374@qq.com>
2018-11-08 14:31:36 +0800

    -

--- Src/Asp.Net/SqlSugar/CacheScheme/CacheKeyBuider.cs
+++ Src/Asp.Net/SqlSugar/CacheScheme/CacheKeyBuider.cs
@@ -28,6 +28,7 @@ namespace SqlSugar
             result.IdentificationList.Add(queryBuilder.Take.ObjToString());
             result.IdentificationList.Add(queryBuilder.Skip.ObjToString());
             result.IdentificationList.Add(queryBuilder.IsCount.ObjToString());
+            result.IdentificationList.Add(queryBuilder.GetSelectValue.ObjToString().Length.ObjToString());
             if (queryBuilder.Parameters.HasValue())
             {
                 foreach (var item in queryBuilder.Parameters)
