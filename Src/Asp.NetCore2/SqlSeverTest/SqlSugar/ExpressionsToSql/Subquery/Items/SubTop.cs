8443581bac59856f3f17034bc326355d369c08ee
sunkaixuan <610262374@qq.com>
2018-12-03 00:35:00 +0800

    Update Core

--- Src/Asp.NetCore2/SqlSeverTest/SqlSugar/ExpressionsToSql/Subquery/Items/SubTop.cs
+++ Src/Asp.NetCore2/SqlSeverTest/SqlSugar/ExpressionsToSql/Subquery/Items/SubTop.cs
@@ -39,8 +39,7 @@ namespace SqlSugar
                 {
                     return 150;
                 }
-                else if (this.Context is OracleExpressionContext)
-                {
+                else if (this.Context is OracleExpressionContext) {
 
                     return 401;
                 }
@@ -60,11 +59,7 @@ namespace SqlSugar
             }
             else if (this.Context is OracleExpressionContext)
             {
-                return (HasWhere ? "AND" : "WHERE") + " ROWNUM=1";
-            }
-            else if (this.Context is PostgreSQLExpressionContext)
-            {
-                return "limit 1";
+                return (HasWhere?"AND":"WHERE")+ " ROWNUM=1";
             }
             else
             {
