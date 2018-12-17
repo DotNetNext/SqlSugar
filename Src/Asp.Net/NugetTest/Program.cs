dd7bd5f9a73990944d8eb35fb44a486757cb1c59
610262374@qq.com <610262374@qq.com>
2018-12-17 19:13:46 +0800

    Update Nuget

--- Src/Asp.Net/NugetTest/Program.cs
+++ Src/Asp.Net/NugetTest/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SqlSugar;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,6 +10,15 @@ namespace NugetTest
     {
         static void Main(string[] args)
         {
+            var db = new SqlSugarClient(new ConnectionConfig()
+            {
+                ConnectionString = "server=.;uid=sa;pwd=@jhl85661501;database=SqlSugar4XTest",
+                IsAutoCloseConnection = true,
+                DbType = DbType.SqlServer
+            });
+            InstanceFactory.RemoveCache();
+            var list = db.Ado.GetInt("select 1");
+            Console.WriteLine("Hello World!");
         }
     }
 }
