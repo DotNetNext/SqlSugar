b5693a70585420241578e06449ba7b0ca08db42b
610262374@qq.com <610262374@qq.com>
2018-12-17 19:04:55 +0800

    Update Nuget

---
+++ Src/Asp.Net/NugetTest/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NugetTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
