0dd774b4b2bee49826a2bd79aa5003c24df727ad
sunkaixuan <610262374@qq.com>
2019-01-21 12:37:23 +0800

    Update InSingle bug

--- Src/Asp.Net/SqlSugar/Abstract/QueryableProvider/QueryableProvider.cs
+++ Src/Asp.Net/SqlSugar/Abstract/QueryableProvider/QueryableProvider.cs
@@ -271,7 +271,7 @@ namespace SqlSugar
                 Where(SqlBuilder.SqlFalse);
                 return this;
             }
-            if (pkValues.Length == 1 && pkValues.First().GetType().FullName.IsCollectionsList()|| pkValues.First() is IEnumerable)
+            if (pkValues.Length == 1 && pkValues.First().GetType().FullName.IsCollectionsList()|| (pkValues.First() is IEnumerable&&pkValues.First().GetType()!=UtilConstants.StringType))
             {
                 var newValues = new List<object>();
                 foreach (var item in pkValues.First() as IEnumerable)
