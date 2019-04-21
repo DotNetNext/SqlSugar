be02d016199263fc94dbbb6c280f4dda36df820a
sunkaixuan <610262374@qq.com>
2019-04-21 22:23:24 +0800

    Update Demo

--- Src/Asp.NetCore2/SqlSeverTest/SqlSeverTest/Config.cs
+++ Src/Asp.NetCore2/SqlSeverTest/SqlSeverTest/Config.cs
@@ -8,8 +8,8 @@ namespace OrmTest
 {
     public class Config
     {
-        public static string ConnectionString = "server=.;uid=sa;pwd=haosql;database=SqlSugar4XTest";
-        public static string ConnectionString2 = "server=.;uid=sa;pwd=haosql;database=SQLSUGAR4XTEST";
-        public static string ConnectionString3 = "server=.;uid=sa;pwd=haosql;database=sqlsugar4xtest";
+        public static string ConnectionString = "server=.;uid=sa;pwd=@jhl85661501;database=SqlSugar4XTest";
+        public static string ConnectionString2 = "server=.;uid=sa;pwd=@jhl85661501;database=SQLSUGAR4XTEST";
+        public static string ConnectionString3 = "server=.;uid=sa;pwd=@jhl85661501;database=sqlsugar4xtest";
     }
 }
