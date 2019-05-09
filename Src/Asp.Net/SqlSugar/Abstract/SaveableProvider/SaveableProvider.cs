566e75fe6770f6ff8e45402fdec2719e1ea9f716
sunkaixuan <610262374@qq.com>
2019-05-09 20:36:57 +0800

    Add Saveable async methods

--- Src/Asp.Net/SqlSugar/Abstract/SaveableProvider/SaveableProvider.cs
+++ Src/Asp.Net/SqlSugar/Abstract/SaveableProvider/SaveableProvider.cs
@@ -80,6 +80,7 @@ namespace SqlSugar
             }
         }
 
+        #region Core
         public int ExecuteCommand()
         {
             LoadInsertable();
@@ -118,6 +119,23 @@ namespace SqlSugar
                 updateable.ExecuteCommand();
             return saveObjects;
         }
+        #endregion
+        #region Core Async
+        public Task<int> ExecuteCommandAsync()
+        {
+            return Task.FromResult(ExecuteCommand());
+        }
+
+        public Task<T> ExecuteReturnEntityAsync()
+        {
+            return Task.FromResult(ExecuteReturnEntity());
+        }
+
+        public Task<List<T>> ExecuteReturnListAsync()
+        {
+            return Task.FromResult(ExecuteReturnList());
+        }
+        #endregion
         public ISaveable<T> InsertColumns(Expression<Func<T, object>> columns)
         {
             LoadInsertable();
