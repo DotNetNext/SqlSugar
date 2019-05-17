8f09003d78d04f5381042b97c72f06afe6d8b2ca
sunkaixuan <610262374@qq.com>
2019-05-17 21:09:04 +0800

    Update CreateDatabase

--- Src/Asp.Net/SqlSugar/Realization/MySql/DbMaintenance/MySqlDbMaintenance.cs
+++ Src/Asp.Net/SqlSugar/Realization/MySql/DbMaintenance/MySqlDbMaintenance.cs
@@ -12,7 +12,7 @@ namespace SqlSugar
         {
             get
             {
-                return "SELECT NAME FROM MASTER.DBO.SYSDATABASES ORDER BY NAME";
+                return "SHOW DATABASES";
             }
         }
         protected override string GetColumnInfosByTableNameSql
@@ -248,6 +248,35 @@ namespace SqlSugar
         #endregion
 
         #region Methods
+        /// <summary>
+        ///by current connection string
+        /// </summary>
+        /// <param name="databaseDirectory"></param>
+        /// <returns></returns>
+        public override bool CreateDatabase(string databaseName, string databaseDirectory = null)
+        {
+            if (databaseDirectory != null)
+            {
+                if (!FileHelper.IsExistDirectory(databaseDirectory))
+                {
+                    FileHelper.CreateDirectory(databaseDirectory);
+                }
+            }
+            var oldDatabaseName = this.Context.Ado.Connection.Database;
+            var connection = this.Context.CurrentConnectionConfig.ConnectionString;
+            connection = connection.Replace(oldDatabaseName, "sys");
+            var newDb = new SqlSugarClient(new ConnectionConfig()
+            {
+                DbType = this.Context.CurrentConnectionConfig.DbType,
+                IsAutoCloseConnection = true,
+                ConnectionString = connection
+            });
+            if (!GetDataBaseList(newDb).Any(it => it.Equals(databaseName, StringComparison.CurrentCultureIgnoreCase)))
+            {
+                newDb.Ado.ExecuteCommand(string.Format(CreateDataBaseSql, databaseName, databaseDirectory));
+            }
+            return true;
+        }
         public override bool AddTableRemark(string tableName, string description)
         {
             string sql = string.Format(this.AddTableRemarkSql, this.SqlBuilder.GetTranslationTableName(tableName), description);
