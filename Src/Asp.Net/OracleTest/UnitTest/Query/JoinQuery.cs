10d5537a086c59114be14d91593327b3a9e5088a
sunkaixuan <610262374@qq.com>
2019-05-17 16:58:40 +0800

    Update Demo

--- Src/Asp.Net/OracleTest/UnitTest/Query/JoinQuery.cs
+++ Src/Asp.Net/OracleTest/UnitTest/Query/JoinQuery.cs
@@ -129,8 +129,7 @@ namespace OrmTest.UnitTest
         public new SqlSugarClient GetInstance()
         {
             SqlSugarClient db = new SqlSugarClient(new ConnectionConfig() { ConnectionString = Config.ConnectionString, DbType = DbType.SqlServer });
-            db.Ado.IsEnableLogEvent = true;
-            db.Ado.LogEventStarting = (sql, pars) =>
+            db.Aop.OnLogExecuting = (sql, pars) =>
             {
                 Console.WriteLine(sql + " " + pars);
             };
