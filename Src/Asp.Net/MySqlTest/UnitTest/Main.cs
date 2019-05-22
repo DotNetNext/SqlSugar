1733f3be4ed94ea74b70f9270da1e0f708852d5a
sunkaixuan <610262374@qq.com>
2019-05-22 20:42:46 +0800

    Update Demo

---
+++ Src/Asp.Net/MySqlTest/UnitTest/Main.cs
@@ -0,0 +1,38 @@
+﻿using SqlSugar;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OrmTest
+{
+    public partial class NewUnitTest
+    {
+       public static  SqlSugarClient Db=> new SqlSugarClient(new ConnectionConfig()
+        {
+            DbType = DbType.MySql,
+            ConnectionString = Config.ConnectionString,
+            InitKeyType = InitKeyType.Attribute,
+            IsAutoCloseConnection = true,
+            AopEvents = new AopEvents
+            {
+                OnLogExecuting = (sql, p) =>
+                {
+                    Console.WriteLine(sql);
+                    Console.WriteLine(string.Join(",", p?.Select(it => it.ParameterName + ":" + it.Value)));
+                }
+            }
+        });
+        public static void Init()
+        {
+            Updateable();
+            Json();
+            Ado();
+            Queryable();
+            QueryableAsync();
+            Thread();
+            Thread2();
+            Thread3();
+        }
+    }
+}
