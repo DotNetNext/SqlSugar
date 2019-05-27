73dc46458e22d4a2247f7e955b9be3db0c5d076a
sunkaixuan <610262374@qq.com>
2019-05-27 17:07:16 +0800

    Update Demo

--- Src/Asp.Net/OracleTest/Program.cs
+++ Src/Asp.Net/OracleTest/Program.cs
@@ -1,15 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
-using System.Linq.Expressions;
-using SqlSugar;
-using OrmTest.Models;
-using System.Data.SqlClient;
+﻿using OrmTest.PerformanceTesting;
 using OrmTest.UnitTest;
-using OrmTest.PerformanceTesting;
+using System;
 
 namespace OrmTest
 {
@@ -17,21 +8,15 @@ namespace OrmTest
     {
         static void Main(string[] args)
         {
+            OldTestMain.Init();
+ 
+            //Unit test
+            NewUnitTest.Init();
 
-            /***Unit Test***/
-            new Select(1).Init();
-            new Field(1).Init();
-
-            /***Demo***/
-            OrmTest.Demo.Query.Init();
-            OrmTest.Demo.Insert.Init();
-            //OrmTest.Demo.Delete.Init();
-            OrmTest.Demo.Update.Init();
-            OrmTest.Demo.DbFirst.Init();
-            OrmTest.Demo.JoinSql.Init();
-            OrmTest.Demo.Filter.Init();
-            OrmTest.Demo.ComplexModel.Init();
-            OrmTest.Demo.CodeFirst.Init();
+            Console.WriteLine("all successfully.");
+            Console.ReadKey();
         }
+
+ 
     }
 }
---
+++ Src/Asp.Net/OracleTest/UnitTest/UQueryable.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrmTest
+{
+    public partial class NewUnitTest
+    {
+        public static void Queryable() {
+
+            var pageindex = 1;
+            var pagesize = 10;
+            var total = 0;
+            var totalPage = 0;
+            var list=Db.Queryable<Order>().ToPageList(pageindex, pagesize, ref total, ref totalPage);
+
+            //Db.CodeFirst.InitTables(typeof(CarType));
+            //Db.Updateable<CarType>()
+            //      .SetColumns(it => new CarType { State = SqlSugar.SqlFunc.IIF(it.State == true, false, true) }).Where(it => true)
+            //   .ExecuteCommand();
+
+            //Db.CodeFirst.InitTables(typeof(TestTree));
+            //Db.DbMaintenance.TruncateTable<TestTree>();
+            //Db.Ado.ExecuteCommand("insert testtree values(hierarchyid::GetRoot(),geography :: STGeomFromText ('POINT(55.9271035250276 -3.29431266523898)',4326),'name')");
+            //var list2 = Db.Queryable<TestTree>().ToList();
+
+            Db.CodeFirst.InitTables<GuidTable>();
+            Db.Queryable<GuidTable>().Where(it => it.Id.HasValue).ToList();
+        }
+
+
+        public class GuidTable
+        {
+            public Guid? Id { get; set; }
+        }
+    }
+}
