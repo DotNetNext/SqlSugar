603b080b86c3d194bc3ca6c3cc7bbad288676d49
andyz168 <810802300@qq.com>
2019-05-27 10:35:07 +0800

    Update Config.cs

--- Src/Asp.NetCore2/SqlSeverTest/DB2Test/Config.cs
+++ Src/Asp.NetCore2/SqlSeverTest/DB2Test/Config.cs
@@ -8,6 +8,6 @@ namespace OrmTest
 {
     public class Config
     {
-        public static string ConnectionString = "Server=139.159.228.222:50000;Database=TSMESDB;User ID=db2admin;Password=!QAZxsw2;";
+        public static string ConnectionString = "Server=myAddress:myPortNumber;Database=myDataBase;UID=myUsername;PWD=myPassword;";
     }
 }
