eca49860545a566e1716498b63d76eabb4efaca2
sunkaixuan <610262374@qq.com>
2019-06-02 16:38:16 +0800

    Update demo

--- Src/Asp.Net/SqlServerTest/Demo/DemoB_Aop.cs
+++ Src/Asp.Net/SqlServerTest/Demo/DemoB_Aop.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SqlSugar;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,7 +11,26 @@ namespace OrmTest
     {
         public static void Init()
         {
+            Console.WriteLine("");
+            Console.WriteLine("#### Aop Start ####");
 
+            SqlSugarClient db = new SqlSugarClient(new ConnectionConfig()
+            {
+                DbType = DbType.SqlServer,
+                ConnectionString = Config.ConnectionString,
+                InitKeyType = InitKeyType.Attribute,
+                IsAutoCloseConnection = true,
+                AopEvents = new AopEvents
+                {
+                    OnLogExecuting = (sql, p) =>
+                    {
+                        Console.WriteLine(sql);
+                        Console.WriteLine(string.Join(",", p?.Select(it => it.ParameterName + ":" + it.Value)));
+                    }
+                }
+            });
+
+            Console.WriteLine("#### Aop End ####");
         }
     }
 }
