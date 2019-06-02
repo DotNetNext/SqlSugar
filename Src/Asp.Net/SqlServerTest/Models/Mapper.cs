2a644d438ee6bda66fe8c841f7764e7d01561573
sunkaixuan <610262374@qq.com>
2019-06-02 10:31:00 +0800

    Update demo

---
+++ Src/Asp.Net/SqlServerTest/Models/Mapper.cs
@@ -0,0 +1,30 @@
+﻿using SqlSugar;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrmTest.Models
+{
+    [SugarTable("OrderDetail")]
+    public class OrderItemInfo
+    {
+        [SugarColumn(IsPrimaryKey = true, IsIdentity = true)]
+        public int ItemId { get; set; }
+        public string ItemCode { get; set; }
+        public decimal Pirce { get; set; }
+        public int OrderId { get; set; }
+        [SugarColumn(IsIgnore = true)]
+        public Order Order { get; set; }
+    }
+    [SugarTable("Order")]
+    public class OrderInfo
+    {
+        [SugarColumn(IsPrimaryKey = true, IsIdentity = true)]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        [SugarColumn(IsIgnore = true)]
+        public List<OrderItem> Items { get; set; }
+    }
+}
