a75836d2d9e73e0469abaa99770c873aed50acb7
sunkaixuan <610262374@qq.com>
2019-06-02 10:24:00 +0800

    Update demo

--- Src/Asp.Net/SqlServerTest/Program.cs
+++ Src/Asp.Net/SqlServerTest/Program.cs
@@ -6,10 +6,14 @@ namespace OrmTest
 {
     class Program
     {
+        /// <summary>
+        /// Set up config.cs file and start directly F5
+        /// 设置Config.cs文件直接F5启动例子
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
-            //OldTestMain.Init();
-
+ 
             //Demo
             Demo0_SqlSugarClient.Init();
             Demo1_Queryable.Init();
