987b74a734e4406d786f31c8df1116716d4dad4b
sunkaixuan <610262374@qq.com>
2019-06-04 21:11:04 +0800

    Update demo

---
+++ Src/Asp.NetCore2/SqlSeverTest/SqliteTest/Config.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrmTest
+{
+    /// <summary>
+    /// Setting up the database name does not require you to create the database
+    /// 设置好数据库名不需要你去手动建库
+    /// </summary>
+    public class Config
+    {
+        public static string GetCurrentProjectPath
+        {
+
+            get
+            {
+                return Environment.CurrentDirectory.Replace(@"\bin\Debug", "");
+            }
+        }
+        public static string ConnectionString = @"DataSource="+ GetCurrentProjectPath + @"\DataBase\SqlSugar4xTest.sqlite";
+        public static string ConnectionString2 = @"DataSource=" + GetCurrentProjectPath + @"\DataBase\SqlSugar4xTest2.sqlite";
+        public static string ConnectionString3 = @"DataSource=" + GetCurrentProjectPath + @"\DataBase\SqlSugar4xTest3.sqlite";
+    }
+}
--- Src/Asp.NetCore2/SqlSeverTest/SqliteTest/Demo/Demo5_SqlQueryable.cs
+++ Src/Asp.NetCore2/SqlSeverTest/SqliteTest/Demo/Demo5_SqlQueryable.cs
@@ -1,4 +1,4 @@
-﻿using OrmTest.Models;
+﻿ 
 using SqlSugar;
 using System;
 using System.Collections.Generic;
