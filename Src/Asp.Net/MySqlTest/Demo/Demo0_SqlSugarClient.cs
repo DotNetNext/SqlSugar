a97843b3b66f66b13870921130f4d8cdc347a772
sunkaixuan <610262374@qq.com>
2019-06-04 13:22:37 +0800

    Update demo

--- Src/Asp.Net/MySqlTest/Demo/Demo0_SqlSugarClient.cs
+++ Src/Asp.Net/MySqlTest/Demo/Demo0_SqlSugarClient.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.Data.Linq.Mapping;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
