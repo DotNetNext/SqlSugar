6bf1cf084550b956098ac91053865d585d55c150
610262374@qq.com <sunkaixuan>
2019-06-19 17:51:34 +0800

    PgSql Bug

--- Src/Asp.Net/SqlSugar/Realization/PostgreSQL/SqlBuilder/PostgreSQLBuilder.cs
+++ Src/Asp.Net/SqlSugar/Realization/PostgreSQL/SqlBuilder/PostgreSQLBuilder.cs
@@ -73,7 +73,7 @@ namespace SqlSugar
             }
             else if (name.Contains("("))
             {
-                return name.ToLower();
+                return name;
             }
             else if (name.Contains(SqlTranslationLeft) && name.Contains(SqlTranslationRight))
             {
