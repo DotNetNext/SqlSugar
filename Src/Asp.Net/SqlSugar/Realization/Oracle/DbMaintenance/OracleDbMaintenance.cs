d1610bc6fd851115b635f0220d751c04abd18459
neil <neil.lau@qq.com>
2020-02-24 13:03:53 +0800

    fixes

--- Src/Asp.Net/SqlSugar/Realization/Oracle/DbMaintenance/OracleDbMaintenance.cs
+++ Src/Asp.Net/SqlSugar/Realization/Oracle/DbMaintenance/OracleDbMaintenance.cs
@@ -271,9 +271,9 @@ namespace SqlSugar
             }
             return base.AddColumn(tableName,columnInfo);
         }
-        public override bool CreateIndex(string tableName, string[] columnNames)
+        public override bool CreateIndex(string tableName, string[] columnNames, bool isUnique=false)
         {
-            string sql = string.Format(CreateIndexSql, tableName, string.Join(",", columnNames), string.Join("_", columnNames.Select(it=>(it+"abc").Substring(0,3))));
+            string sql = string.Format(CreateIndexSql, tableName, string.Join(",", columnNames), string.Join("_", columnNames.Select(it=>(it+"abc").Substring(0,3))), isUnique ? "unique" : "");
             this.Context.Ado.ExecuteCommand(sql);
             return true;
         }
