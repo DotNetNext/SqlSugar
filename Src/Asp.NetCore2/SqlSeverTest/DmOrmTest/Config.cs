93487a4283e4c9d1c4bc56e70c120f34dfc84c8f
skx <610262374@qq.com>
2020-11-02 15:27:30 +0800

    Update demo

--- Src/Asp.NetCore2/SqlSeverTest/DmOrmTest/Config.cs
+++ Src/Asp.NetCore2/SqlSeverTest/DmOrmTest/Config.cs
@@ -7,8 +7,7 @@ using System.Threading.Tasks;
 namespace OrmTest
 {
     /// <summary>
-    /// Setting up the database name does not require you to create the database
-    /// 设置好数据库名不需要你去手动建库
+    /// 配置连接字符串后，需要手动建库,表可以自动创建
     /// </summary>
     public class Config
     {
