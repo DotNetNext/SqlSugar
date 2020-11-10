8b99025d0dad17106bcc10b98df3e8e8c5904308
skx <610262374@qq.com>
2020-11-10 22:34:01 +0800

    Update CodeFirst

--- Src/Asp.Net/SqlSugar/Abstract/CodeFirstProvider/CodeFirstProvider.cs
+++ Src/Asp.Net/SqlSugar/Abstract/CodeFirstProvider/CodeFirstProvider.cs
@@ -109,6 +109,9 @@ namespace SqlSugar
                 }
             }
             var tableName = GetTableName(entityInfo);
+            this.Context.MappingTables.Add(entityInfo.EntityName,tableName);
+            entityInfo.DbTableName = tableName;
+            entityInfo.Columns.ForEach(it => { it.DbTableName = tableName; });
             var isAny = this.Context.DbMaintenance.IsAnyTable(tableName);
             if (isAny)
                 ExistLogic(entityInfo);
