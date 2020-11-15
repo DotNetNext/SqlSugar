ccfd52d223b17eb6f4c30cc69f4c3abad6105e1a
skx <610262374@qq.com>
2020-11-15 12:56:24 +0800

    code optimization

--- Src/Asp.Net/SqlSugar/ExpressionsToSql/ResolveItems/BaseResolve.cs
+++ Src/Asp.Net/SqlSugar/ExpressionsToSql/ResolveItems/BaseResolve.cs
@@ -442,22 +442,7 @@ namespace SqlSugar
                     }
                     else
                     {
-                        var propertyName = property.Name;
-                        var dbColumnName = propertyName;
-                        var mappingInfo = this.Context.MappingColumns.FirstOrDefault(it => it.EntityName == item.Type.Name && it.PropertyName.Equals(propertyName, StringComparison.CurrentCultureIgnoreCase));
-                        if (mappingInfo.HasValue())
-                        {
-                            dbColumnName = mappingInfo.DbColumnName;
-                        }
-                        asName = this.Context.GetTranslationText(item.Type.Name + "." + propertyName);
-                        if (Context.IsJoin)
-                        {
-                            this.Context.Result.Append(Context.GetAsString(asName, dbColumnName, shortName.ObjToString()));
-                        }
-                        else
-                        {
-                            this.Context.Result.Append(Context.GetAsString(asName, dbColumnName));
-                        }
+                        asName = GetAsName(item, shortName, property);
                     }
                 }
             }
@@ -494,6 +479,29 @@ namespace SqlSugar
             {
                 Check.ThrowNotSupportedException(item.GetType().Name);
             }
+        }
+
+        private string GetAsName(Expression item, object shortName, PropertyInfo property)
+        {
+            string asName;
+            var propertyName = property.Name;
+            var dbColumnName = propertyName;
+            var mappingInfo = this.Context.MappingColumns.FirstOrDefault(it => it.EntityName == item.Type.Name && it.PropertyName.Equals(propertyName, StringComparison.CurrentCultureIgnoreCase));
+            if (mappingInfo.HasValue())
+            {
+                dbColumnName = mappingInfo.DbColumnName;
+            }
+            asName = this.Context.GetTranslationText(item.Type.Name + "." + propertyName);
+            if (Context.IsJoin)
+            {
+                this.Context.Result.Append(Context.GetAsString(asName, dbColumnName, shortName.ObjToString()));
+            }
+            else
+            {
+                this.Context.Result.Append(Context.GetAsString(asName, dbColumnName));
+            }
+
+            return asName;
         }
 
         private static bool IsBoolValue(Expression item)
