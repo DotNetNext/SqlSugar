e88d199f10ebeb535dbc3a2a58e89f3886619839
skx <610262374@qq.com>
2020-12-02 16:37:37 +0800

    -

--- Src/Asp.Net/SqlSugar/OnlyNet/DependencyManagement.cs
+++ Src/Asp.Net/SqlSugar/OnlyNet/DependencyManagement.cs
@@ -46,7 +46,7 @@ namespace SqlSugar
                 {
                     var message = ErrorMessage.GetThrowMessage(
                      "You need to refer to MySql.Data.dll",
-                     " MySql.Data.dll  未安装或者版本冲突，按下面步骤操作即可 1、从Nuget卸载所有项目的MySql.Data.dll和SqlSugar，检查引用中是否还存在,存在直接删掉引用，然后Nuget重新安装即可 ，注意.NET 4.52版本以下用户只能用MySql.Data 6.9.12 版本 记住 6.9.12 ， .NET 4.52及以上用最新的");
+                     " MySql.Data.dll  未安装或者版本冲突，按下面步骤操作即可 1、从Nuget卸载所有项目的MySql.Data.dll和SqlSugar，检查引用中是否还存在,存在直接删掉引用，然后Nuget重新安装即可 ，注意.NET 4.52版本以下用户只能用MySql.Data 6.9.12 版本 记住 6.9.12 或者6.9之前的版本 ， .NET 4.52及以上用最新的");
                     throw new Exception(message);
                 }
             }
