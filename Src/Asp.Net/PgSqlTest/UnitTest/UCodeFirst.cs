2dcd4ca48163e6b7ff2586b7dd038cb99f37d572
skx <610262374@qq.com>
2021-01-29 20:36:52 +0800

    PgSql CodeFirst  alter column bug

--- Src/Asp.Net/PgSqlTest/UnitTest/UCodeFirst.cs
+++ Src/Asp.Net/PgSqlTest/UnitTest/UCodeFirst.cs
@@ -13,7 +13,24 @@ namespace OrmTest
             if (Db.DbMaintenance.IsAnyTable("UnitCodeTest1", false))
                 Db.DbMaintenance.DropTable("UnitCodeTest1");
             Db.CodeFirst.InitTables<UnitCodeTest1>();
+            Db.CodeFirst.InitTables<UnitCodeTest22>();
+            Db.Insertable(new UnitCodeTest22()).ExecuteCommand();
+            Db.DbMaintenance.TruncateTable<UnitCodeTest22>();
+            Db.CodeFirst.InitTables<UnitCodeTest3>();
+            Db.Insertable(new UnitCodeTest22() {  id=1}).ExecuteCommand();
         }
+ 
+        public class UnitCodeTest22 {
+             [SqlSugar.SugarColumn(IsNullable =true)]
+             public decimal? id { get; set; }
+        }
+        [SqlSugar.SugarTable("UnitCodeTest22")]
+        public class UnitCodeTest3
+        {
+            [SqlSugar.SugarColumn(IsNullable = false)]
+            public int id { get; set; }
+        }
+
         public class UnitCodeTest1
         {
             [SqlSugar.SugarColumn(IndexGroupNameList = new string[] { "group1" })]
