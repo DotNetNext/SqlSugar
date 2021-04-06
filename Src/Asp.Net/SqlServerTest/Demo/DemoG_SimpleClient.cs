bb3a254bdef258ba3c6f26cb9ec13ac88a5d7366
SUNKAIXUAN <610262374@qq.com>
2021-04-06 16:41:08 +0800

    Update demo

--- Src/Asp.Net/SqlServerTest/Demo/DemoG_SimpleClient.cs
+++ Src/Asp.Net/SqlServerTest/Demo/DemoG_SimpleClient.cs
@@ -14,23 +14,44 @@ namespace OrmTest
             Console.WriteLine("");
             Console.WriteLine("#### SimpleClient Start ####");
 
-            SqlSugarClient db = new SqlSugarClient(new ConnectionConfig()
-            {
-                DbType = DbType.SqlServer,
-                ConnectionString = Config.ConnectionString,
-                InitKeyType = InitKeyType.Attribute,
-                IsAutoCloseConnection = true,
-                AopEvents = new AopEvents
-                {
-                    OnLogExecuting = (sql, p) =>
-                    {
-                        Console.WriteLine(sql);
-                        Console.WriteLine(string.Join(",", p?.Select(it => it.ParameterName + ":" + it.Value)));
-                    }
-                }
-            });
-
+            var order = new OrderDal();
+            order.GetList();
+            order.GetById(1);
+            order.MyTest();
             Console.WriteLine("#### SimpleClient End ####");
+        }
+        public class OrderDal:Repository<Order>
+        {
+            public void MyTest() { 
+            
+            }
+        }
+        public class Repository<T> : SimpleClient<T> where T : class, new()
+        {
+            public Repository(ISqlSugarClient context = null) : base(context)//注意这里要有默认值等于null
+            {
+                if (context == null)
+                {
+                    base.Context = new SqlSugarClient(new ConnectionConfig()
+                    {
+                        DbType = SqlSugar.DbType.SqlServer,
+                        InitKeyType = InitKeyType.Attribute,
+                        IsAutoCloseConnection = true,
+                        ConnectionString = Config.ConnectionString
+                    });
+                }
+            }
+
+            /// <summary>
+            /// 扩展方法，自带方法不能满足的时候可以添加新方法
+            /// </summary>
+            /// <returns></returns>
+            public List<T> CommQuery(string json)
+            {
+                //base.Context.Queryable<T>().ToList();可以拿到SqlSugarClient 做复杂操作
+                return null;
+            }
+
         }
     }
 }
