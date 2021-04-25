233b7558f80d0db16601f9defcf8b372252f8b2c
sunkaixuna <610262374@qq.com>
2021-04-25 15:51:57 +0800

    Update ReplaceSqlParameter

--- Src/Asp.Net/SqlServerTest/Demo/Demo6_Queue.cs
+++ Src/Asp.Net/SqlServerTest/Demo/Demo6_Queue.cs
@@ -44,6 +44,7 @@ namespace OrmTest
             db.Queryable<Order>().AddQueue();
             db.Queryable<Order>().AddQueue();
             db.AddQueue("select * from [Order] where id=@id", new { id = 10000 });
+            db.AddQueue("select id=@id+1", new { id = 10000 });
             var result2 = db.SaveQueues<Order, Order, Order>();
 
             Console.WriteLine("#### Queue End ####");
