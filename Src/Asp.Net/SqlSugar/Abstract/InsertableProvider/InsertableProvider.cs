3f5ce11d18985c18742fa9bfd7fa72bb86495896
sunkaixuna <610262374@qq.com>
2021-05-15 22:43:57 +0800

    Insertable  count=0  AddQueue BUG

--- Src/Asp.Net/SqlSugar/Abstract/InsertableProvider/InsertableProvider.cs
+++ Src/Asp.Net/SqlSugar/Abstract/InsertableProvider/InsertableProvider.cs
@@ -35,8 +35,11 @@ namespace SqlSugar
         #region Core
         public void AddQueue()
         {
-            var sqlObj = this.ToSql();
-            this.Context.Queues.Add(sqlObj.Key, sqlObj.Value);
+            if (this.InsertObjs!=null&&this.InsertObjs.Length > 0&& this.InsertObjs[0]!=null)
+            {
+                var sqlObj = this.ToSql();
+                this.Context.Queues.Add(sqlObj.Key, sqlObj.Value);
+            }
         }
         public virtual int ExecuteCommand()
         {
