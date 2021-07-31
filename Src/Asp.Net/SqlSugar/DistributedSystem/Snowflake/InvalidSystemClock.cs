e2fd7bb04fe60b6d5251c1f4ee7e605eaf7d734d
sunkaixuna <610262374@qq.com>
2021-07-31 20:19:19 +0800

    Add Snowflake

---
+++ Src/Asp.Net/SqlSugar/DistributedSystem/Snowflake/InvalidSystemClock.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace SqlSugar.DistributedSystem.Snowflake
+{
+    public class InvalidSystemClock : Exception
+    {      
+        public InvalidSystemClock(string message) : base(message) { }
+    }
+}
---
+++ Src/Asp.Net/SqlSugar/DistributedSystem/Snowflake/SnowFlakeSingle.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SqlSugar.DistributedSystem.Snowflake
+{
+	public sealed class SnowFlakeSingle 
+	{
+		public static readonly SnowFlakeSingle instance = new SnowFlakeSingle();
+		private SnowFlakeSingle()
+		{
+			worker = new Snowflake.IdWorker(1, 1);
+		}
+		static SnowFlakeSingle() { }
+		public static SnowFlakeSingle Instance
+		{
+			get { return instance; }
+		}
+		private Snowflake.IdWorker worker;
+		public long getID()
+		{
+			return worker.NextId();
+		}
+	}
+}
