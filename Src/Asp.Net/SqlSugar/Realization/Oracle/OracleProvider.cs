250b1b2be33404a18b1928e65f2894b40e9d4829
sunkaixuna <610262374@qq.com>
2021-08-03 21:26:35 +0800

    Remove Code

--- Src/Asp.Net/SqlSugar/Realization/Oracle/OracleProvider.cs
+++ Src/Asp.Net/SqlSugar/Realization/Oracle/OracleProvider.cs
@@ -3,7 +3,6 @@ using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Data.Common;
-using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
--- Src/Asp.Net/SqlSugar/Realization/Sqlite/SqliteProvider.cs
+++ Src/Asp.Net/SqlSugar/Realization/Sqlite/SqliteProvider.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Data;
 using System.Data.Common;
-using System.Data.SqlClient;
 using System.Data.SQLite;
 using System.Linq;
 using System.Text;
