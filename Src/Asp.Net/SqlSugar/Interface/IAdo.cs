1fff3b8262c8ccebf7b371f269c7b5b904554a97
sunkaixuna <610262374@qq.com>
2021-08-30 00:25:39 +0800

    Update IAdo

--- Src/Asp.Net/SqlSugar/Interface/IAdo.cs
+++ Src/Asp.Net/SqlSugar/Interface/IAdo.cs
@@ -174,8 +174,8 @@ namespace SqlSugar
 
         DbResult<bool> UseTran(Action action, Action<Exception> errorCallBack = null);
         DbResult<T> UseTran<T>(Func<T> action, Action<Exception> errorCallBack = null);
-        Task<DbResult<bool>> UseTranAsync(Action action, Action<Exception> errorCallBack = null);
-        Task<DbResult<T>> UseTranAsync<T>(Func<T> action, Action<Exception> errorCallBack = null);
+        Task<DbResult<bool>> UseTranAsync(Func<Task> action, Action<Exception> errorCallBack = null);
+        Task<DbResult<T>> UseTranAsync<T>(Func<Task<T>> action, Action<Exception> errorCallBack = null);
         IAdo UseStoredProcedure();
 
 
