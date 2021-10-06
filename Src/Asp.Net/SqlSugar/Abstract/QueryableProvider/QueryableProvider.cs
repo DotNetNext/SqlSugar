6d34ad1607f9ab8a11ef88682d666ccefa1c2573
sunkaixuna <610262374@qq.com>
2021-10-06 15:01:29 +0800

    Modifying error Description

--- Src/Asp.Net/SqlSugar/Abstract/QueryableProvider/QueryableProvider.cs
+++ Src/Asp.Net/SqlSugar/Abstract/QueryableProvider/QueryableProvider.cs
@@ -1281,7 +1281,7 @@ namespace SqlSugar
             }
             else if (result.Count == 2)
             {
-                Check.Exception(true, ".Single()  result must not exceed one . You can use.First()");
+                Check.Exception(true, ErrorMessage.GetThrowMessage(".Single()  result must not exceed one . You can use.First()", "使用single查询结果集不能大于1，适合主键查询，如果大于1你可以使用Queryable.First"));
                 return default(T);
             }
             else
