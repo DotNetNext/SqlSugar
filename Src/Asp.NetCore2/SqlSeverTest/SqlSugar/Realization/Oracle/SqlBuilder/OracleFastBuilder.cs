471beeecb1cc9cd4516fc6981145a84253042b03
sunkaixuna <610262374@qq.com>
2021-11-21 00:09:09 +0800

    Update Core

---
+++ Src/Asp.NetCore2/SqlSeverTest/SqlSugar/Realization/Oracle/SqlBuilder/OracleFastBuilder.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SqlSugar 
+{
+    public class OracleFastBuilder : FastBuilder, IFastBuilder
+    {
+        public Task<int> ExecuteBulkCopyAsync(DataTable dt)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
