85ca336cc81fcb3ec11c46381d0417c0a761e3b1
sunkaixuna <610262374@qq.com>
2021-11-29 20:50:31 +0800

    Remove nvarchar to guid check

--- Src/Asp.Net/SqlServerTest/UnitTest/UInsert2.cs
+++ Src/Asp.Net/SqlServerTest/UnitTest/UInsert2.cs
@@ -16,8 +16,18 @@ namespace OrmTest
             db.Insertable(new UnitInsertMethod() { Name = "2" }).CallEntityMethod(it => it.Create("admin")).ExecuteCommand();
             db.Updateable(new UnitInsertMethod() {Id=1, Name = "1" }).CallEntityMethod(it => it.Create()).ExecuteCommand();
             db.Updateable(new UnitInsertMethod() { Name = "1" }).CallEntityMethod(it => it.Create("admint")).ExecuteCommand();
+            db.CodeFirst.InitTables<Unitsdafa111>();
+            db.Insertable(new Unitsdafa111()).ExecuteCommand();
+            db.Insertable(new Unitsdafa111() {Id=Guid.NewGuid(),Id2=Guid.NewGuid() }).ExecuteCommand();
+            var list=db.Queryable<Unitsdafa111>().ToList();
         }
-
+        public class Unitsdafa111
+        {
+            [SqlSugar.SugarColumn(IsNullable =true,ColumnDataType ="nvarchar(50)")]
+            public Guid Id { get; set; }
+            [SqlSugar.SugarColumn(IsNullable = true, ColumnDataType = "nvarchar(50)")]
+            public Guid? Id2 { get; set; }
+        }
         public class UnitInsertMethod
         {
             [SqlSugar.SugarColumn(IsPrimaryKey = true, IsIdentity = true)]
