5008b02117cb72741d4cf9a25e3598939d9a48fe
sunkaixuna <610262374@qq.com>
2021-12-16 02:04:41 +0800

    Code optimization

--- Src/Asp.Net/SqlSugar/SqlSugarScope.cs
+++ Src/Asp.Net/SqlSugar/SqlSugarScope.cs
@@ -9,10 +9,8 @@ using System.Threading.Tasks;
 
 namespace SqlSugar
 {
-    public class SqlSugarScope: ISqlSugarClient, ITenant
+    public partial class SqlSugarScope: ISqlSugarClient, ITenant
     {
-        private List<ConnectionConfig> _configs;
-        private Action<SqlSugarClient> _configAction;
         private SqlSugarScope()
         {
 
@@ -645,83 +643,6 @@ namespace SqlSugar
         public IFastest<T> Fastest<T>() where T : class, new()
         {
             return ScopedContext.Fastest<T>();
-        }
-
-
-        private SqlSugarClient GetContext()
-        {
-            SqlSugarClient result = null;
-            var key = _configs.GetHashCode().ToString();
-            StackTrace st = new StackTrace(true);
-            var methods = st.GetFrames();
-            var isAsync = UtilMethods.IsAnyAsyncMethod(methods);
-            if (isAsync)
-            {
-                result = GetAsyncContext(key);
-            }
-            else
-            {
-                result = GetThreadContext(key);
-            }
-            return result;
-        }
-
-        private SqlSugarClient GetAsyncContext(string key)
-        {
-            SqlSugarClient result = CallContextAsync<SqlSugarClient>.GetData(key);
-            if (result == null)
-            {
-                List<ConnectionConfig> configList = GetCopyConfigs();
-                CallContextAsync<SqlSugarClient>.SetData(key, new SqlSugarClient(configList));
-                result = CallContextAsync<SqlSugarClient>.GetData(key);
-                if (this._configAction != null)
-                {
-                    this._configAction(result);
-                }
-            }
-
-            return result;
-        }
-
-        private SqlSugarClient GetThreadContext(string key)
-        {
-            SqlSugarClient result = CallContextThread<SqlSugarClient>.GetData(key);
-            if (result == null)
-            {
-                List<ConnectionConfig> configList = GetCopyConfigs();
-                CallContextThread<SqlSugarClient>.SetData(key, new SqlSugarClient(configList));
-                result = CallContextThread<SqlSugarClient>.GetData(key);
-                if (this._configAction != null)
-                {
-                    this._configAction(result);
-                }
-            }
-            return result;
-        }
-
-        private List<ConnectionConfig> GetCopyConfigs()
-        {
-            return _configs.Select(it => new ConnectionConfig()
-            {
-                AopEvents = it.AopEvents,
-                ConfigId = it.ConfigId,
-                ConfigureExternalServices = it.ConfigureExternalServices,
-                ConnectionString = it.ConnectionString,
-                DbType = it.DbType,
-                IndexSuffix = it.IndexSuffix,
-                InitKeyType = it.InitKeyType,
-                IsAutoCloseConnection = it.IsAutoCloseConnection,
-                LanguageType = it.LanguageType,
-                MoreSettings = it.MoreSettings == null ? null : new ConnMoreSettings()
-                {
-                    DefaultCacheDurationInSeconds = it.MoreSettings.DefaultCacheDurationInSeconds,
-                    DisableNvarchar = it.MoreSettings.DisableNvarchar,
-                    PgSqlIsAutoToLower = it.MoreSettings.PgSqlIsAutoToLower,
-                    IsAutoRemoveDataCache = it.MoreSettings.IsAutoRemoveDataCache,
-                    IsWithNoLockQuery = it.MoreSettings.IsWithNoLockQuery
-                },
-                SlaveConnectionConfigs = it.SlaveConnectionConfigs
-            }).ToList();
         }
     }
 }
