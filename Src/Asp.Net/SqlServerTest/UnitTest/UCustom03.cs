df7d355c11d529e1d00beba5e9f6d9070e33eac4
sunkaixuna <610262374@qq.com>
2021-12-26 01:47:45 +0800

    Update unit test

--- Src/Asp.Net/SqlServerTest/UnitTest/UCustom03.cs
+++ Src/Asp.Net/SqlServerTest/UnitTest/UCustom03.cs
@@ -10,6 +10,9 @@ namespace OrmTest
     {
         public static void Init()
         {
+            Demo8();
+            Demo7();
+            Demo6();
             Demo5();
             Demo4();
             Demo3();
@@ -170,6 +173,137 @@ namespace OrmTest
             var json2 = db.Context.Utilities.SerializeObject(conditionalModels);
 
             if (json != json2) 
+            {
+                throw new Exception("unit error");
+            }
+        }
+
+        private static void Demo6()
+        {
+            var db = NewUnitTest.Db;
+            List<ConditionalTree> conModels = new List<ConditionalTree>();
+            conModels.Add(new ConditionalTree()
+            {
+                ConditionalList = new List<KeyValuePair<WhereType, IConditionalModel>>()//  (id=1 or id=2 and id=1)
+            {
+                //new  KeyValuePair<WhereType, ConditionalModel>( WhereType.And ,new ConditionalModel() { FieldName = "id", ConditionalType = ConditionalType.Equal, FieldValue = "1" }),
+                new  KeyValuePair<WhereType, IConditionalModel> (WhereType.Null,new ConditionalModel() { FieldName = "id", ConditionalType = ConditionalType.Equal, FieldValue = "2" }),
+                new  KeyValuePair<WhereType, IConditionalModel> (WhereType.And,new ConditionalModel() { FieldName = "name", ConditionalType = ConditionalType.Equal, FieldValue = "2" }),
+                new  KeyValuePair<WhereType, IConditionalModel> (
+                  WhereType.And, new ConditionalTree(){
+                      ConditionalList=new List<KeyValuePair<WhereType, IConditionalModel>>()
+                  {
+                    new KeyValuePair<WhereType, IConditionalModel>(WhereType.Null,new ConditionalModel(){
+                         FieldName="price", ConditionalType=ConditionalType.Equal, FieldValue="1"
+                    }),
+                    new KeyValuePair<WhereType, IConditionalModel>(WhereType.And,new ConditionalModel(){
+                          FieldName = "CustomId", ConditionalType = ConditionalType.Equal, FieldValue = "1"
+                    })
+
+                  }
+                })
+            }
+            });
+            var json = db.Context.Utilities.SerializeObject(conModels);
+
+            var conditionalModels = db.Context.Utilities.JsonToConditionalModels(json);
+            var list6 = db.Queryable<Order>().Where(it=>it.Id==1).Where(conditionalModels).ToList();
+
+            var json2 = db.Context.Utilities.SerializeObject(conditionalModels);
+
+            if (json != json2)
+            {
+                throw new Exception("unit error");
+            }
+        }
+
+        private static void Demo7()
+        {
+            var db = NewUnitTest.Db;
+            List<IConditionalModel> conModels = new List<IConditionalModel>();
+            conModels.Add(new ConditionalTree()
+            {
+                ConditionalList = new List<KeyValuePair<WhereType, IConditionalModel>>()//  (id=1 or id=2 and id=1)
+                {
+                //new  KeyValuePair<WhereType, ConditionalModel>( WhereType.And ,new ConditionalModel() { FieldName = "id", ConditionalType = ConditionalType.Equal, FieldValue = "1" }),
+                  new  KeyValuePair<WhereType, IConditionalModel> (WhereType.Null,new ConditionalModel() { FieldName = "name", ConditionalType = ConditionalType.Equal, FieldValue = "name" }),
+                
+                  new  KeyValuePair<WhereType, IConditionalModel> ( WhereType.And, new ConditionalTree(){
+                  ConditionalList=new List<KeyValuePair<WhereType, IConditionalModel>>(){
+                    new KeyValuePair<WhereType, IConditionalModel>(WhereType.Null,new ConditionalModel(){
+                         FieldName="customid", ConditionalType=ConditionalType.Equal, FieldValue="1"
+                    }),
+                    new KeyValuePair<WhereType, IConditionalModel>(WhereType.Or,new ConditionalModel(){
+                          FieldName = "id", ConditionalType = ConditionalType.Equal, FieldValue = "1"
+                    }),
+                    new KeyValuePair<WhereType, IConditionalModel>(WhereType.Or,new ConditionalTree(){
+                          ConditionalList=new List<KeyValuePair<WhereType, IConditionalModel>>(){
+                              new KeyValuePair<WhereType, IConditionalModel>(WhereType.Null,new ConditionalModel(){
+                         FieldName="customid", ConditionalType=ConditionalType.Equal, FieldValue="1"
+                           }
+                              
+                              ),
+                              
+                      }
+                    })
+
+
+                  }
+                }),
+                new  KeyValuePair<WhereType, IConditionalModel> (WhereType.And,new ConditionalModel() { FieldName = "id", ConditionalType = ConditionalType.Equal, FieldValue = "2" }),
+            }
+            });
+            var json = db.Context.Utilities.SerializeObject(conModels);
+            var conditionalModels = db.Context.Utilities.JsonToConditionalModels(json);
+            var list6 = db.Queryable<Order>().Where(conditionalModels).ToList();
+            var json2 = db.Context.Utilities.SerializeObject(conditionalModels);
+            if (json != json2)
+            {
+                throw new Exception("unit error");
+            }
+        }
+
+        private static void Demo8()
+        {
+            var db = NewUnitTest.Db;
+            List<IConditionalModel> conModels = new List<IConditionalModel>();
+            conModels.Add(new ConditionalTree()
+            {
+                ConditionalList = new List<KeyValuePair<WhereType, IConditionalModel>>()//  (id=1 or id=2 and id=1)
+                {
+                //new  KeyValuePair<WhereType, ConditionalModel>( WhereType.And ,new ConditionalModel() { FieldName = "id", ConditionalType = ConditionalType.Equal, FieldValue = "1" }),
+                  new  KeyValuePair<WhereType, IConditionalModel> (WhereType.Null,new ConditionalModel() { FieldName = "name", ConditionalType = ConditionalType.Equal, FieldValue = "name" }),
+
+                  new  KeyValuePair<WhereType, IConditionalModel> ( WhereType.And, new ConditionalTree(){
+                  ConditionalList=new List<KeyValuePair<WhereType, IConditionalModel>>(){
+                    new KeyValuePair<WhereType, IConditionalModel>(WhereType.Null,new ConditionalModel(){
+                         FieldName="customid", ConditionalType=ConditionalType.Equal, FieldValue="1"
+                    }),
+                    new KeyValuePair<WhereType, IConditionalModel>(WhereType.And,new ConditionalModel(){
+                          FieldName = "id", ConditionalType = ConditionalType.Equal, FieldValue = "1"
+                    }),
+                    new KeyValuePair<WhereType, IConditionalModel>(WhereType.Or,new ConditionalTree(){
+                          ConditionalList=new List<KeyValuePair<WhereType, IConditionalModel>>(){
+                              new KeyValuePair<WhereType, IConditionalModel>(WhereType.Null,new ConditionalModel(){
+                         FieldName="customid", ConditionalType=ConditionalType.Equal, FieldValue="1"
+                           }
+
+                              ),
+
+                      }
+                    })
+
+
+                  }
+                }),
+                new  KeyValuePair<WhereType, IConditionalModel> (WhereType.Or,new ConditionalModel() { FieldName = "id", ConditionalType = ConditionalType.Equal, FieldValue = "2" }),
+            }
+            });
+            var json = db.Context.Utilities.SerializeObject(conModels);
+            var conditionalModels = db.Context.Utilities.JsonToConditionalModels(json);
+            var list6 = db.Queryable<Order>().Where(conditionalModels).ToList();
+            var json2 = db.Context.Utilities.SerializeObject(conditionalModels);
+            if (json != json2)
             {
                 throw new Exception("unit error");
             }
