68c5f60d3e437e79c9532b331ceeab20d258fa92
sunkaixuna <610262374@qq.com>
2021-12-29 21:25:58 +0800

    Update enum

--- Src/Asp.Net/SqlSugar/Realization/Dm/SqlBuilder/DmUpdateBuilder.cs
+++ Src/Asp.Net/SqlSugar/Realization/Dm/SqlBuilder/DmUpdateBuilder.cs
@@ -60,7 +60,14 @@ namespace SqlSugar
                 }
                 else if (type.IsEnum())
                 {
-                    return Convert.ToInt64(value);
+                    if (this.Context.CurrentConnectionConfig.MoreSettings?.TableEnumIsString == true)
+                    {
+                        return value.ToSqlValue();
+                    }
+                    else
+                    {
+                        return Convert.ToInt64(value);
+                    }
                 }
                 else if (type == UtilConstants.ByteArrayType)
                 {
