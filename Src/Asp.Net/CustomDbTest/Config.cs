da6866e3c56af2f1d3813164d5c4319959406a02
sunkaixuan <610262374@qq.com>
2022-02-26 19:42:09 +0800

    AddCustom Db Demo

---
+++ Src/Asp.Net/CustomDbTest/Config.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrmTest
+{
+    /// <summary>
+    /// Setting up the database name does not require you to create the database
+    /// 只需要设置IP和账户密码 ，自动建库
+    /// </summary>
+    public class Config
+    {
+        public static string GetCurrentProjectPath
+        {
+
+            get
+            {
+                return Environment.CurrentDirectory.Replace(@"\bin\Debug", "");
+            }
+        }
+        /// <summary>
+        /// Account have permission to create database
+        /// 用有建库权限的数据库账号
+        /// </summary>
+        public static string ConnectionString =  "Provider=Microsoft.ACE.OleDB.15.0;Data Source="+GetCurrentProjectPath+"\\test.accdb";
+        /// <summary>
+        /// Account have permission to create database
+        /// 用有建库权限的数据库账号
+        /// </summary>
+        public static string ConnectionString2 = ConnectionString;
+        /// <summary>
+        /// Account have permission to create database
+        /// 用有建库权限的数据库账号
+        /// </summary>
+        public static string ConnectionString3 = ConnectionString;
+    }
+}
