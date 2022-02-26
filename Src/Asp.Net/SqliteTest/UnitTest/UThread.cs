59aa5bcf2f2b3906eba37d1a681c64565574955c
sunkaixuan <610262374@qq.com>
2022-02-26 22:36:49 +0800

    Update unit

--- Src/Asp.Net/SqliteTest/UnitTest/UThread.cs
+++ Src/Asp.Net/SqliteTest/UnitTest/UThread.cs
@@ -40,7 +40,7 @@ namespace OrmTest
                 }
             }
         });
-        public static SqlSugarClient singleDb =  new SqlSugarClient(new ConnectionConfig()
+        public static SqlSugarScope singleDb =  new SqlSugarScope(new ConnectionConfig()
         {
             DbType = DbType.Sqlite,
             ConnectionString = Config.ConnectionString,
