e362480d6108aedffd55edede0b0bfc83e4f11d5
sunkaixuan <610262374@qq.com>
2022-03-09 13:07:07 +0800

    Update split table bug

--- Src/Asp.NetCore2/SqlSeverTest/SqlSugar/Utilities/UtilMethods.cs
+++ Src/Asp.NetCore2/SqlSeverTest/SqlSugar/Utilities/UtilMethods.cs
@@ -505,5 +505,40 @@ namespace SqlSugar
                 return item.FieldValue;
             }
         }
+
+        /// <summary>
+        /// Get Week Last Day Sun
+        /// </summary>
+        /// <param name="datetime"></param>
+        /// <returns></returns>
+        public static DateTime GetWeekLastDaySun(DateTime datetime)
+        {
+            //星期天为最后一天
+            int weeknow = Convert.ToInt32(datetime.DayOfWeek);
+            weeknow = (weeknow == 0 ? 7 : weeknow);
+            int daydiff = (7 - weeknow);
+
+            //本周最后一天
+            string LastDay = datetime.AddDays(daydiff).ToString("yyyy-MM-dd");
+            return Convert.ToDateTime(LastDay);
+        }
+        /// <summary>
+        /// Get Week First Day Mon
+        /// </summary>
+        /// <param name="datetime"></param>
+        /// <returns></returns>
+        public static DateTime GetWeekFirstDayMon(DateTime datetime)
+        {
+            //星期一为第一天
+            int weeknow = Convert.ToInt32(datetime.DayOfWeek);
+
+            //因为是以星期一为第一天，所以要判断weeknow等于0时，要向前推6天。
+            weeknow = (weeknow == 0 ? (7 - 1) : (weeknow - 1));
+            int daydiff = (-1) * weeknow;
+
+            //本周第一天
+            string FirstDay = datetime.AddDays(daydiff).ToString("yyyy-MM-dd");
+            return Convert.ToDateTime(FirstDay);
+        }
     }
 }
