f89a4972779ba1f3195fc08e2b376d61e0efd08c
sunkaixuan <610262374@qq.com>
2022-03-27 00:36:27 +0800

    Update oracle

--- Src/Asp.Net/SqlSugar/Realization/Oracle/SqlBuilder/OracleUpdateBuilder.cs
+++ Src/Asp.Net/SqlSugar/Realization/Oracle/SqlBuilder/OracleUpdateBuilder.cs
@@ -57,7 +57,7 @@ namespace SqlSugar
                     {
                         date = Convert.ToDateTime("1900-1-1");
                     }
-                    return "to_date('" + date.ToString("yyyy-MM-dd HH:mm:ss") + "', 'YYYY-MM-DD HH24:MI:SS') ";
+                    return "to_timestamp('" + date.ToString("yyyy-MM-dd HH:mm:ss.ffffff") + "', 'YYYY-MM-DD HH24:MI:SS.FF') ";
                 }
                 else if (type.IsEnum())
                 {
@@ -83,7 +83,7 @@ namespace SqlSugar
                 else if (type == UtilConstants.DateTimeOffsetType)
                 {
                     var date = UtilMethods.ConvertFromDateTimeOffset((DateTimeOffset)value);
-                    return "to_date('" + date.ToString("yyyy-MM-dd HH:mm:ss") + "', 'YYYY-MM-DD HH24:MI:SS') ";
+                    return "to_timestamp('" + date.ToString("yyyy-MM-dd HH:mm:ss.ffffff") + "', 'YYYY-MM-DD HH24:MI:SS.FF') ";
                 }
                 else if (type == UtilConstants.StringType || type == UtilConstants.ObjType)
                 {
