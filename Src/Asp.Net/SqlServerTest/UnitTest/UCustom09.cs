16cdf5ee70c87c6e1d1ac342ba8947f1bdbf771e
sunkaixuan <610262374@qq.com>
2022-04-08 14:29:34 +0800

    Update unit test

--- Src/Asp.Net/SqlServerTest/UnitTest/UCustom09.cs
+++ Src/Asp.Net/SqlServerTest/UnitTest/UCustom09.cs
@@ -16,6 +16,17 @@ namespace OrmTest
             var list=db.Queryable<Order>()
                .GroupBy(x => new { CreateTime = x.CreateTime.ToString("yyyy-MM"),x.Name })
                 .Select(x => new { CreateTime =  x.CreateTime.ToString("yyyy-MM") }).ToList();
+            db.CodeFirst.InitTables<Unitadfaint21>();
+            db.Insertable(new Unitadfaint21() { id = 1 , id2=22 }).ExecuteCommand();
+            var list2 = db.Queryable<Unitadfaint21>().ToList();
+        }
+
+        public class Unitadfaint21
+        {
+            [SugarColumn(ColumnDataType ="int")]
+            public long? id { get; set; }
+            [SugarColumn(ColumnDataType = "int")]
+            public long id2 { get; set; }
         }
         public class Unitasfa1sadfa
         {
