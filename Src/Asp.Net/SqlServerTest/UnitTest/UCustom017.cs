c4ea537902527c97967d8e63e7497d8525516a8c
sunkaixuan <610262374@qq.com>
2022-04-15 23:52:19 +0800

    Update exp to sql

---
+++ Src/Asp.Net/SqlServerTest/UnitTest/UCustom017.cs
@@ -0,0 +1,24 @@
+﻿using OrmTest.UnitTest.Models;
+using SqlSugar;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrmTest
+{
+    public class UCustom017
+    {
+
+        public static void Init()
+        {
+            var db = NewUnitTest.Db;
+            db.Queryable<Order>()
+                .Where(it => it.Id==SqlFunc.Subqueryable<Order>().Where(z=>z.Id==it.Id).GroupBy(z=>z.Id).Select(x=>x.Id) )
+                .ToList();
+
+        }
+
+    }
+}
