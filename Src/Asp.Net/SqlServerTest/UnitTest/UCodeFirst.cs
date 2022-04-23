1803c609e0f125344d10aae707a2fd448ba5a499
sunkaixuan <610262374@qq.com>
2022-04-23 15:45:08 +0800

    Update unit test

--- Src/Asp.Net/SqlServerTest/UnitTest/UCodeFirst.cs
+++ Src/Asp.Net/SqlServerTest/UnitTest/UCodeFirst.cs
@@ -51,6 +51,31 @@ namespace OrmTest
             if (Db.DbMaintenance.IsAnyTable("User", false))
                 Db.DbMaintenance.DropTable("User");
             db.CodeFirst.InitTables<User>();
+            Db.CodeFirst.InitTables<UnitCodeFirstpks2>();
+            if (Db.DbMaintenance.IsAnyTable("UnitCodeFirst131", false))
+                Db.DbMaintenance.DropTable("UnitCodeFirst131");
+            Db.CodeFirst.InitTables<UnitCodeFirst131>();
+            Db.Insertable(new UnitCodeFirst131() { Id = 1 }).ExecuteCommand();
+            Db.CodeFirst.InitTables<UNITCODEFIRST131>();
+            Db.CodeFirst.InitTables<UNITCOdEFIRST131>();
+        }
+        public class UnitCodeFirst131
+        {
+            public int Id { get; set; }
+        }
+        public class UNITCODEFIRST131
+        {
+            public int Id { get; set; }
+            [SqlSugar.SugarColumn(DefaultValue = "a")]
+            public string Name { get; set; }
+        }
+        public class UNITCOdEFIRST131
+        {
+            public int Id { get; set; }
+            [SqlSugar.SugarColumn(DefaultValue = "a")]
+            public string Name { get; set; }
+            [SqlSugar.SugarColumn(DefaultValue = "getdate()")]
+            public DateTime dt { get; set; }
         }
         public class User 
         {
