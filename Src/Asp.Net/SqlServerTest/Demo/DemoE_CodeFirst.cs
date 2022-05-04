d14f0aed2faa20e03bfe4924b6160fda8dc4dd3e
sunkaixuan <610262374@qq.com>
2022-05-04 11:34:08 +0800

    Update demo

--- Src/Asp.Net/SqlServerTest/Demo/DemoE_CodeFirst.cs
+++ Src/Asp.Net/SqlServerTest/Demo/DemoE_CodeFirst.cs
@@ -19,14 +19,23 @@ namespace OrmTest
                 InitKeyType = InitKeyType.Attribute,
                 IsAutoCloseConnection = true
             });
-            db.DbMaintenance.CreateDatabase(); 
+            db.Aop.OnLogExecuted = (s, p) =>
+            {
+                Console.WriteLine(s);
+            };
+            db.DbMaintenance.CreateDatabase();
             db.CodeFirst.InitTables(typeof(CodeFirstTable1));//Create CodeFirstTable1 
-            db.Insertable(new CodeFirstTable1() { Name = "a", Text="a" }).ExecuteCommand();
+            db.DbMaintenance.TruncateTable<CodeFirstTable1>();
+            db.Insertable(new CodeFirstTable1() { Name = "a", Text="a",CreateTime=DateTime.Now }).ExecuteCommand();
+            db.Insertable(new CodeFirstTable1() { Name = "a", Text = "a", CreateTime = DateTime.Now.AddDays(1) }).ExecuteCommand();
             var list = db.Queryable<CodeFirstTable1>().ToList();
             Console.WriteLine("#### CodeFirst end ####");
         }
     }
 
+    [SugarIndex("index_codetable1_name",nameof(CodeFirstTable1.Name),OrderByType.Asc)]
+    [SugarIndex("index_codetable1_nameid", nameof(CodeFirstTable1.Name), OrderByType.Asc,nameof(CodeFirstTable1.Id),OrderByType.Desc)]
+    [SugarIndex("unique_codetable1_CreateTime", nameof(CodeFirstTable1.CreateTime), OrderByType.Desc,true)]
     public class CodeFirstTable1
     {
         [SugarColumn(IsIdentity = true, IsPrimaryKey = true)]
