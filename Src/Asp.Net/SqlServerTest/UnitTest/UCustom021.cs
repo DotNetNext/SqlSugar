f97e828cb2cdfea9d3190de606eb2762091f2a8d
sunkaixuan <610262374@qq.com>
2022-05-21 11:12:36 +0800

    Add unit test

--- Src/Asp.Net/SqlServerTest/UnitTest/UCustom021.cs
+++ Src/Asp.Net/SqlServerTest/UnitTest/UCustom021.cs
@@ -24,11 +24,39 @@ namespace OrmTest
             var result2 = db.Queryable<UnitStudent111>()
              .Includes(e => e.Exams.Where(s => s.Time >DateTime.Now).ToList()).ToList();
 
-
+            Join<MyJoin>();
 
         }
+
+        public static void Join<T>() where T : IUserLink
+        {
+            var sql= NewUnitTest.Db
+                .Queryable<UserDO, T>((u, a) => new JoinQueryInfos(JoinType.Inner, u.Id == a.UserId))
+                .Where((u, a) => u.Id == 1)
+                .Select((u, a) => a).ToSql();
+            Check.Exception("SELECT a.* FROM [UserDO] u Inner JOIN [MyJoin] a ON ( [u].[Id] =[a].[UserId])   WHERE ( [u].[Id] = @Id0 )"!= sql.Key,"unit error");
+        }
+    }
+    public class MyJoin : IUserLink 
+    {
+        public int UserId { get; set; }
+    }
+    public interface IUserLink
+    {
+         int UserId { get; set; }
     }
 
+    public class UserDO
+    {
+        public int Id { get; set; }
+    }
+
+    public class AuthDO : IUserLink
+    {
+        public int UserId { get; set; }
+
+        public int ObjId { get; set; }
+    }
     public class UnitStudent111
     {
         [SugarColumn(IsIdentity = true, IsPrimaryKey = true)]
