01dc48ceb3ceb4db9c4c7ab5ca577573f655ca5a
sunkaixuan <610262374@qq.com>
2022-06-08 20:55:05 +0800

    Update  .net core version

--- Src/Asp.NetCore2/SqlSugar/Abstract/QueryableProvider/NavigatManager.cs
+++ Src/Asp.NetCore2/SqlSugar/Abstract/QueryableProvider/NavigatManager.cs
@@ -453,6 +453,7 @@ namespace SqlSugar
         {
             var navEntity = navObjectNameColumnInfo.PropertyInfo.PropertyType;
             var navEntityInfo = this.Context.EntityMaintenance.GetEntityInfo(navEntity);
+            this.Context.InitMappingInfo(navEntity);
             var sqlObj = GetWhereSql(navObjectNameColumnInfo.Navigat.Name);
             Check.ExceptionEasy(sqlObj.MappingExpressions.IsNullOrEmpty(), $"{expression} error，dynamic need MappingField ,Demo: Includes(it => it.Books.MappingField(z=>z.studenId,()=>it.StudentId).ToList())", $"{expression}解析出错， 自定义映射需要 MappingField ,例子: Includes(it => it.Books.MappingField(z=>z.studenId,()=>it.StudentId).ToList())");
             if (list.Any() && navObjectNamePropety.GetValue(list.First()) == null)
