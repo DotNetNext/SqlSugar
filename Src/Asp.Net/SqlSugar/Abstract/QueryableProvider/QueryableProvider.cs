02f57c0988ebac926478e81aa23b2c64cffe321a
sunkaixuan <610262374@qq.com>
2022-06-17 12:59:24 +0800

    Update   auto nolcok

--- Src/Asp.Net/SqlSugar/Abstract/QueryableProvider/QueryableProvider.cs
+++ Src/Asp.Net/SqlSugar/Abstract/QueryableProvider/QueryableProvider.cs
@@ -2422,7 +2422,7 @@ namespace SqlSugar
                 if (this.QueryBuilder.AsTables != null && this.QueryBuilder.AsTables.Count==1) 
                 {
                     var tableinfo = this.QueryBuilder.AsTables.First();
-                    if (this.Context.CurrentConnectionConfig?.MoreSettings?.IsWithNoLockQuery == true)
+                    if (this.Context.CurrentConnectionConfig?.MoreSettings?.IsWithNoLockQuery == true&& this.QueryBuilder.AsTables.First().Value.ObjToString().Contains(SqlWith.NoLock) ==false)
                     {
                         this.QueryBuilder.AsTables[tableinfo.Key] = " (SELECT * FROM " + this.QueryBuilder.AsTables.First().Value + $" {SqlWith.NoLock} )";
                     }
