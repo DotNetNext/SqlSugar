793cc9e396087da544c564a1c45ad638f3e00ae6
sunkaixuan <610262374@qq.com>
2022-06-24 14:54:24 +0800

    Add unit  test

--- Src/Asp.Net/SqlServerTest/UnitTest/UCustom017.cs
+++ Src/Asp.Net/SqlServerTest/UnitTest/UCustom017.cs
@@ -17,7 +17,9 @@ namespace OrmTest
             db.Queryable<Order>()
                 .Where(it => it.Id==SqlFunc.Subqueryable<Order>().Where(z=>z.Id==it.Id).GroupBy(z=>z.Id).Select(x=>x.Id) )
                 .ToList();
-
+            db.Queryable<Order>()
+             .Where(it => it.Id != SqlFunc.Subqueryable<Order>().Where(z => z.Id == it.Id).GroupBy(z => z.Id).Select(x => x.Id))
+             .ToList();
         }
 
     }
