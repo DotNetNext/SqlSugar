89ef7aeb883d5b971e040b1e76138deff81fdac2
sunkaixuan <610262374@qq.com>
2022-06-24 14:44:34 +0800

    Add unit test

---
+++ Src/Asp.Net/SqlServerTest/UnitTest/UCustom024.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrmTest
+{
+    public class UCustom024
+    {
+        public static void Init() 
+        {
+            var db = NewUnitTest.Db;
+            var data=db.EntityMaintenance.GetEntityInfoNoCache(typeof(UnitNewP));
+            db.CodeFirst.InitTables<UnitNewP>();
+            db.Insertable(new UnitNewP() { Id = Guid.NewGuid() }).ExecuteCommand();
+            var list=db.Queryable<UnitNewP>().Take(2).ToList();
+            db.Updateable(list).ExecuteCommand();
+            db.Deleteable(list).ExecuteCommand();
+        }
+       
+    }
+
+    public class UnitNewP: UnitBaseP
+    {
+        [SqlSugar.SugarColumn(IsPrimaryKey =true)]
+        public new Guid Id { get; set; }
+        [SqlSugar.SugarColumn(IsNullable =true)]
+        public string Name { get; set; }    
+    }
+    public class UnitBaseP 
+    {
+        public int Id { get; set; }
+    }
+}
