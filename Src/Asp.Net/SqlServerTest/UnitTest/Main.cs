e0b250a9ba188b7619140a1ca77827b520759c4f
sunkaixuan <610262374@qq.com>
2022-08-03 10:44:33 +0800

    Add unit test

--- Src/Asp.Net/SqlServerTest/UnitTest/Main.cs
+++ Src/Asp.Net/SqlServerTest/UnitTest/Main.cs
@@ -31,6 +31,7 @@ namespace OrmTest
         }
         public static void Init()
         {
+            UnitInsertNav3.Init();
             UnitInsertNav2.Init();
             UnitInsertNav.Init();
             UnitInsertNav.Init();
