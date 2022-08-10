c015a3933c9223fa8d435afbdf6f8b8a9819a39d
sunkaixuan <610262374@qq.com>
2022-08-10 15:22:03 +0800

    Add  Queryable.AddJoinInfo(+1)

---
+++ Src/Asp.Net/SqlSugar/Entities/JoinInfoParameter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SqlSugar 
+{
+    public class JoinInfoParameter
+    {
+       public string TableName { get; set; }
+       public string ShortName{ get; set; }
+       public IFuncModel Models { get; set; }
+       public JoinType Type { get; set; } 
+    }
+}
