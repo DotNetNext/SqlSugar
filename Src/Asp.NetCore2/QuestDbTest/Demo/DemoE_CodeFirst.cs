16e04b7a7117d5dccd96bed7fb86d04a27f7a5b4
sunkaixuan <610262374@qq.com>
2022-08-14 07:02:24 +0800

    Update demo

--- Src/Asp.NetCore2/QuestDbTest/Demo/DemoE_CodeFirst.cs
+++ Src/Asp.NetCore2/QuestDbTest/Demo/DemoE_CodeFirst.cs
@@ -25,8 +25,42 @@ namespace OrmTest
             var list = db.Queryable<CodeFirstTable1>().ToList();
             db.CodeFirst.InitTables<IndexClass>();
             db.CodeFirst.InitTables<SplitTableEntity>();
+            TestBool(db);
+            TestGuid(db);
             Console.WriteLine("#### CodeFirst end ####");
         }
+        private static void TestGuid(SqlSugarClient db)
+        {
+            db.CodeFirst.InitTables<GuidTest>();
+            db.DbMaintenance.TruncateTable("BoolTest");
+            var Id = 1;
+            db.Insertable<GuidTest>(new GuidTest() { A = Guid.Empty, Id = Id }).ExecuteCommand();
+            Console.Write(db.Queryable<GuidTest>().First().A);
+            db.Updateable<GuidTest>(new GuidTest() { A = Guid.NewGuid(), Id = Id }).ExecuteCommand();
+            Console.Write(db.Queryable<GuidTest>().First().A);
+        }
+        private static void TestBool(SqlSugarClient db)
+        {
+            db.CodeFirst.InitTables<BoolTest2>();
+            db.DbMaintenance.TruncateTable("BoolTest");
+            var Id = 1;
+            db.Insertable<BoolTest2>(new BoolTest2() { A = true, Id = Id }).ExecuteCommand();
+            Console.Write(db.Queryable<BoolTest2>().First().A);
+            db.Updateable<BoolTest2>(new BoolTest2() { A = false, Id = Id }).ExecuteCommand();
+            Console.Write(db.Queryable<BoolTest2>().First().A);
+        }
+    }
+    public class GuidTest
+    {
+        [SugarColumn(IsPrimaryKey = true)]
+        public long Id { get; set; }
+        public Guid A { get; set; }
+    }
+    public class BoolTest2
+    {
+        [SugarColumn(IsPrimaryKey = true)]
+        public long Id { get; set; }
+        public bool A { get; set; }
     }
 
     [SugarIndex(null, nameof(IndexClass.Name), OrderByType.Asc)]
