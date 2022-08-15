92a5dc49ca907f2d44f029a7f915c523ea63bcd6
sunkaixuan <610262374@qq.com>
2022-08-15 12:24:26 +0800

    Add Demo

---
+++ Src/Asp.NetCore2/GbaseTest/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace GbaseTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           var odbc= new System.Data.Odbc.OdbcConnection(@"Driver={GBase ODBC 8.3 DRIVER};Server=localhost;port=19088;Database=;Protocol=onsoctcp;Uid=gbasedbt;Pwd=GBase123;");
+            odbc.Open();
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
