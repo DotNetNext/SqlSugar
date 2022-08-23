57f36a37a80e30982fe9eda5a987ac72c9f94157
sunkaixuan <610262374@qq.com>
2022-08-23 20:10:17 +0800

    Upadte  db.Queryable<T>().TranLock

--- Src/Asp.Net/SqlSugar/Interface/IQueryable.cs
+++ Src/Asp.Net/SqlSugar/Interface/IQueryable.cs
@@ -58,7 +58,7 @@ namespace SqlSugar
         ISugarQueryable<T> WhereColumns(List<Dictionary<string, object>> columns);
         ISugarQueryable<T> WhereColumns(Dictionary<string, object> columns, bool ignoreDefaultValue);
         ISugarQueryable<T> WhereColumns(Dictionary<string, object> columns);
-        ISugarQueryable<T> TranLock(DbLockType LockType = DbLockType.Wait);
+        ISugarQueryable<T> TranLock(DbLockType? LockType = DbLockType.Wait);
         ISugarQueryable<T> Where(Expression<Func<T, bool>> expression);
         ISugarQueryable<T> Where(string whereString, object parameters = null);
         ISugarQueryable<T> Where(List<IConditionalModel> conditionalModels);
