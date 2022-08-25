64ac90e8bed5b52017bbdd43d7cbe71aeb72265c
sunkaixuan <610262374@qq.com>
2022-08-25 12:49:27 +0800

    Add unit test

--- Src/Asp.Net/MySqlTest/UnitTest/UCustom07.cs
+++ Src/Asp.Net/MySqlTest/UnitTest/UCustom07.cs
@@ -15,12 +15,23 @@ namespace OrmTest
             if (x.Key.Contains("//") || x.Key.Contains("\\")) { throw new Exception("unit error"); }
             var x2 = db.Deleteable(new Date2() { dateTime = DateTime.Now, name = "a" }).ToSql();
             if (x2.Key.Contains("//") || x.Key.Contains("\\")) { throw new Exception("unit error"); }
-            db.CodeFirst.InitTables<Date2, Date1>();
+            db.CodeFirst.InitTables<Date2, Date1, Date3>();
             db.Deleteable(new Date1() { dateTime = DateTime.Now, name = "a" }).ExecuteCommand();
             db.Deleteable(new Date2() { dateTime = DateTime.Now, name = "a" }).ExecuteCommand();
+            db.Insertable(new Date3() { name1 = "a" }).ExecuteCommand();
+            db.Updateable(new Date3() { name1 = "a" }).ExecuteCommand();
             db.DbMaintenance.DropTable("Date1");
             db.DbMaintenance.DropTable("Date2");
+            db.DbMaintenance.DropTable("Date3");
         }
+    }
+
+    public class Date3
+    {
+        [SqlSugar.SugarColumn(IsPrimaryKey = true, ColumnName = "date Time")]
+        public DateTime dateTime { get; set; }
+        [SqlSugar.SugarColumn(  ColumnName = "name 1")]
+        public string name1 { get; set; }
     }
     public class Date2
     {
