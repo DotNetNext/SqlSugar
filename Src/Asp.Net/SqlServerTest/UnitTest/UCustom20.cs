1e1a8e0f1431ddf7274bc0c11f6cd259f1f184b1
sunkaixuan <610262374@qq.com>
2022-08-31 13:22:39 +0800

    Add unit test

---
+++ Src/Asp.Net/SqlServerTest/UnitTest/UCustom20.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrmTest 
+{
+    public  class UCustom20
+    {
+        public static void Init() 
+        {
+            var db = NewUnitTest.Db;
+            var list = db.Queryable<Order>()
+            .Take(2)
+             .Select(i => new TestDTO
+             {
+                 SubOne = new TestSubDTO { NameOne = i.Name, NameTwo = i.Name },
+                 SubTwo = new TestSubDTO { NameOne = i.Name, NameTwo = i.Name }
+             })
+             .ToList();
+        }
+
+        public class TestDTO
+        {
+            public TestSubDTO SubOne { get; set; }
+
+            public TestSubDTO SubTwo { get; set; }
+        }
+
+        public class TestSubDTO
+        {
+            public string NameOne { get; set; }
+
+            public string NameTwo { get; set; }
+        }
+    }
+}
