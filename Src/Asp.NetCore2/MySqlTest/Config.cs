d73495edc71473cd97d19505e994119db68a5aa4
sunkaixuan <610262374@qq.com>
2022-09-26 21:34:54 +0800

    Update  mysql demo

--- Src/Asp.NetCore2/MySqlTest/Config.cs
+++ Src/Asp.NetCore2/MySqlTest/Config.cs
@@ -16,16 +16,16 @@ namespace OrmTest
         /// Account have permission to create database
         /// 用有建库权限的数据库账号
         /// </summary>
-        public static string ConnectionString = "server=localhost;Database=SqlSugar4xTest;Uid=root;Pwd=haosql; AllowLoadLocalInfile=true";
+        public static string ConnectionString = "server=localhost;Database=SqlSugar4xTest;Uid=root;Pwd=123456; AllowLoadLocalInfile=true";
         /// <summary>
         /// Account have permission to create database
         /// 用有建库权限的数据库账号
         /// </summary>
-        public static string ConnectionString2 = "server=localhost;Database=SqlSugar4xTest2;Uid=root;Pwd=haosql";
+        public static string ConnectionString2 = "server=localhost;Database=SqlSugar4xTest;Uid=root;Pwd=123456; AllowLoadLocalInfile=true";
         /// <summary>
         /// Account have permission to create database
         /// 用有建库权限的数据库账号
         /// </summary>
-        public static string ConnectionString3 = "server=localhost;Database=SqlSugar4xTest3;Uid=root;Pwd=haosql";
+        public static string ConnectionString3 = "server=localhost;Database=SqlSugar4xTest;Uid=root;Pwd=123456; AllowLoadLocalInfile=true";
     }
 }
