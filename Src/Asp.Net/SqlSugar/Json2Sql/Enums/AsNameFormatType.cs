be6bf7714ca2737c9d2a5108a54dcc44a228983a
sunkaixuan <610262374@qq.com>
2022-09-27 22:52:16 +0800

    Update  Json 2 sql

---
+++ Src/Asp.Net/SqlSugar/Json2Sql/Enums/AsNameFormatType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SqlSugar 
+{
+    public enum AsNameFormatType
+    {
+        Default = 0,
+        NoConvert = 1
+    }
+}
