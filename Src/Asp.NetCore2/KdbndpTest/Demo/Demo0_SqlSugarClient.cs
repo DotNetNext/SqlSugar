eb318384d495b33b8ec29545589a56c6038d88ad
sunkaixuan <610262374@qq.com>
2022-10-12 20:25:48 +0800

    Update demo

--- Src/Asp.NetCore2/KdbndpTest/Demo/Demo0_SqlSugarClient.cs
+++ Src/Asp.NetCore2/KdbndpTest/Demo/Demo0_SqlSugarClient.cs
@@ -66,7 +66,7 @@ namespace OrmTest
                     }
                 }
             });
-            db.Open();
+           // db.Open();
             //If no exist create datebase 
             db.DbMaintenance.CreateDatabase();
 
