eca729e877d5d54ebb31885e3ac875bf648c2b8f
sunkaixuan <610262374@qq.com>
2022-10-22 18:30:57 +0800

    Code optimization

--- Src/Asp.Net/SqlSugar/Abstract/QueryableProvider/QueryableProvider.cs
+++ Src/Asp.Net/SqlSugar/Abstract/QueryableProvider/QueryableProvider.cs
@@ -15,42 +15,7 @@ namespace SqlSugar
     #region T1
     public partial class QueryableProvider<T> : QueryableAccessory, ISugarQueryable<T>
     {
-        public SqlSugarProvider Context { get; set; }
-        public IAdo Db { get { return Context.Ado; } }
-        public IDbBind Bind { get { return this.Db.DbBind; } }
-        public ISqlBuilder SqlBuilder { get; set; }
-        public MappingTableList OldMappingTableList { get; set; }
-        public MappingTableList QueryableMappingTableList { get; set; }
-        public List<Action<T>> MapperAction { get; set; }
-        public Action<T, MapperCache<T>> MapperActionWithCache { get; set; }
-        public List<Action<List<T>>> Mappers { get; set; }
-        public bool IsCache { get; set; }
-        public int CacheTime { get; set; }
-        public string CacheKey { get; set; }
-        public bool IsAs { get; set; }
-        public QueryBuilder QueryBuilder
-        {
-            get
-            {
-                return this.SqlBuilder.QueryBuilder;
-            }
-            set
-            {
-                this.SqlBuilder.QueryBuilder = value;
-            }
-        }
-        public EntityInfo EntityInfo
-        {
-            get
-            {
-                return this.Context.EntityMaintenance.GetEntityInfo<T>();
-            }
-        }
-        //public ISugarQueryable<T> CrossQueryWithAttr() 
-        //{
-        //    this.QueryBuilder.IsCrossQueryWithAttr = true;
-        //    return this;
-        //}
+       
         public ISugarQueryable<T> CrossQuery<Type>(string configId)
         {
             return this.CrossQuery(typeof(Type),configId);
