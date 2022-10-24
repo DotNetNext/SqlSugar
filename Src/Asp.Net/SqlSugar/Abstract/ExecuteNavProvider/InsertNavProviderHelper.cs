57dd897dabebaf73db8206acdd952f1ac5dda349
sunkaixuan <610262374@qq.com>
2022-10-24 12:50:09 +0800

    Update  db.InsertNav

--- Src/Asp.Net/SqlSugar/Abstract/ExecuteNavProvider/InsertNavProviderHelper.cs
+++ Src/Asp.Net/SqlSugar/Abstract/ExecuteNavProvider/InsertNavProviderHelper.cs
@@ -81,6 +81,11 @@ namespace SqlSugar
             }
             return pkColumn;
         }
+        private EntityColumnInfo GetPkColumnByNav2(EntityInfo entity, EntityColumnInfo nav)
+        {
+            var pkColumn = entity.Columns.FirstOrDefault(it => it.IsPrimarykey == true);
+            return pkColumn;
+        }
         private EntityColumnInfo GetFKColumnByNav(EntityInfo entity, EntityColumnInfo nav)
         {
             var fkColumn = entity.Columns.FirstOrDefault(it => it.PropertyName == nav.Navigat.Name);
