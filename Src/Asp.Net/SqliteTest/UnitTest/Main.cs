08e4fad7ee0159f5a13fb53432225c79de17f68e
sunkaixuan <610262374@qq.com>
2022-11-14 01:31:57 +0800

    Add unit test

--- Src/Asp.Net/SqliteTest/UnitTest/Main.cs
+++ Src/Asp.Net/SqliteTest/UnitTest/Main.cs
@@ -31,6 +31,7 @@ namespace OrmTest
         }
         public static void Init()
         {
+            UnitByteArray.Init();
             Unit01.Init();
             UnitNavInsertadfa1.Init();
             UnitNavInsertIssue.Init();
