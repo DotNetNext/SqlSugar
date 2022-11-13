2dbb7a4f2a95688f13aba7a4051a09c88aba456f
sunkaixuan <610262374@qq.com>
2022-11-14 01:29:13 +0800

    Add unit test

--- Src/Asp.Net/SqlServerTest/UnitTest/Main.cs
+++ Src/Asp.Net/SqlServerTest/UnitTest/Main.cs
@@ -31,6 +31,7 @@ namespace OrmTest
         }
         public static void Init()
         {
+            UnitByteArray.Init();
             UnitInsertNavOneToOne.Init();
             CrossDatabase01.Init();
             CrossDatabase02.Init();
