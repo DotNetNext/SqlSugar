c895edfc98704b3337686758d983d2d1724ce026
sunkaixuan <610262374@qq.com>
2022-11-16 23:36:31 +0800

    Synchronization code

--- Src/Asp.NetCore2/SqlSugar/Abstract/FilterProvider/FilterProvider.cs
+++ Src/Asp.NetCore2/SqlSugar/Abstract/FilterProvider/FilterProvider.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
+using System.Runtime.InteropServices;
 using System.Text;
+using static SqlSugar.QueryFilterProvider;
 
 namespace SqlSugar
 {
@@ -9,6 +12,7 @@ namespace SqlSugar
     {
         internal SqlSugarProvider Context { get; set; }
         private List<SqlFilterItem> _Filters { get; set; }
+        private List<SqlFilterItem> _BackUpFilters { get; set; }
 
         public IFilter Add(SqlFilterItem filter)
         {
@@ -45,6 +49,52 @@ namespace SqlSugar
         public void Clear()
         {
             _Filters = new List<SqlFilterItem>();
+        }
+        public void ClearAndBackup()
+        {
+            _BackUpFilters = _Filters;
+            _Filters = new List<SqlFilterItem>();
+        }
+
+        public void Restore() 
+        {
+            _Filters = _BackUpFilters;
+            if (_Filters == null) 
+            {
+                _Filters = new List<SqlFilterItem>();
+            }
+        }
+
+        public void AddTableFilter<T>(Expression<Func<T,bool>> expression, FilterJoinPosition filterJoinType = FilterJoinPosition.On) where T : class,new()
+        {
+            var isOn = filterJoinType == FilterJoinPosition.On;
+            var tableFilter = new TableFilterItem<T>(expression, isOn);
+            this.Add(tableFilter);
+        }
+        public void AddTableFilterIF<T>(bool isAppendFilter,Expression<Func<T, bool>> expression, FilterJoinPosition filterJoinType = FilterJoinPosition.On) where T : class, new()
+        {
+            if (isAppendFilter) 
+            {
+                AddTableFilter(expression, filterJoinType);
+            }
+        }
+        public void AddTableFilter(Type type,Expression expression, FilterJoinPosition filterJoinType = FilterJoinPosition.On)
+        {
+            var isOn = filterJoinType == FilterJoinPosition.On;
+            this.Add(new TableFilterItem<object>(type, expression, isOn));
+        }
+
+        public void AddTableFilterIF(bool isAppendFilter, Type type, Expression expression, FilterJoinPosition posType = FilterJoinPosition.On)
+        {
+            if (isAppendFilter)
+            {
+                AddTableFilter(type, expression, posType);
+            }
+        }
+        public enum FilterJoinPosition
+        {
+            On=0,
+            Where=1
         }
     }
 }
