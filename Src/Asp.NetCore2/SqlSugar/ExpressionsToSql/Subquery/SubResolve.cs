b0d01f53b64fca9dff510b8545b05d763b990ff6
sunkaixuan <610262374@qq.com>
2022-11-21 22:37:34 +0800

    Update exp to sql

--- Src/Asp.NetCore2/SqlSugar/ExpressionsToSql/Subquery/SubResolve.cs
+++ Src/Asp.NetCore2/SqlSugar/ExpressionsToSql/Subquery/SubResolve.cs
@@ -90,7 +90,7 @@ namespace SqlSugar
                         {
                             var meExp = argus[0] as LambdaExpression;
                             var selfParameterName = meExp.Parameters.First().Name;
-                            context.SingleTableNameSubqueryShortName = (((meExp.Body as BinaryExpression).Left as MemberExpression).Expression as ParameterExpression).Name;
+                            context.SingleTableNameSubqueryShortName = (((meExp.Body as BinaryExpression).Left as MemberExpression)?.Expression as ParameterExpression)?.Name;
                             if (context.SingleTableNameSubqueryShortName == selfParameterName)
                             {
                                 context.SingleTableNameSubqueryShortName = (((meExp.Body as BinaryExpression).Right as MemberExpression).Expression as ParameterExpression).Name;
