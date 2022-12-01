ee63bada9bd9701b0a664635827649b3fcff9d4c
sunkaixuan <610262374@qq.com>
2022-12-01 23:22:05 +0800

    Add unit test

---
+++ Src/Asp.Net/SqlServerTest/UnitTest/UnitSubToList.cs
@@ -0,0 +1,21 @@
+﻿using SqlSugar;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace OrmTest 
+{
+    public class UnitSubToList
+    {
+        public static void Init() 
+        {
+            var db = NewUnitTest.Db;
+            var test47 = db.Queryable<Order>().Select(it => new
+            {
+                disCount = SqlFunc.Subqueryable<Order>().Where(s=>s.Id==it.Id).ToList()
+            })
+            .ToList();
+        }
+    }
+}
