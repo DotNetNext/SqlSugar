d395093265bf1920d5ff1f53ed280356ac67fb2a
sunkaixuan <610262374@qq.com>
2022-12-04 00:13:41 +0800

    Add unit test

--- Src/Asp.NetCore2/PgSqlTest/UnitTest/Main.cs
+++ Src/Asp.NetCore2/PgSqlTest/UnitTest/Main.cs
@@ -31,6 +31,7 @@ namespace OrmTest
         }
         public static void Init()
         {
+            UnitSubToList.Init();
             Unit001.Init();
             Bulk();
             CodeFirst();
