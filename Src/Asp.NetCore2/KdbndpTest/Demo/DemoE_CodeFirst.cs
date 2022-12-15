fc835d2ac5c4856ad84cbd06e3f21f4154e45ebb
sunkaixuan <610262374@qq.com>
2022-12-15 18:25:29 +0800

    Add demo

--- Src/Asp.NetCore2/KdbndpTest/Demo/DemoE_CodeFirst.cs
+++ Src/Asp.NetCore2/KdbndpTest/Demo/DemoE_CodeFirst.cs
@@ -23,10 +23,32 @@ namespace OrmTest
             db.CodeFirst.InitTables(typeof(CodeFirstTable1));//Create CodeFirstTable1 
             db.Insertable(new CodeFirstTable1() { Name = "a", Text="a" }).ExecuteCommand();
             var list = db.Queryable<CodeFirstTable1>().ToList();
+            db.Aop.OnLogExecuting=(sql,p)=>Console.WriteLine(sql);
+            db.CurrentConnectionConfig.MoreSettings = new ConnMoreSettings()
+            {
+                IsAutoToUpper = false
+            };
+            db.CodeFirst.InitTables<CodeFirstNoUpper4>();
+            db.Insertable(new CodeFirstNoUpper4() { Id = Guid.NewGuid() + "", Name = "a" }).ExecuteCommand();
+            var list2 = db.Queryable<CodeFirstNoUpper4>().Where(it => it.Id != null).ToList();
+            db.Updateable(list2).ExecuteCommand();
+            db.Deleteable(list2).ExecuteCommand();
+            db.Updateable(list2.First()).ExecuteCommand();
+            db.Deleteable<CodeFirstNoUpper4>().Where(it => it.Id != null).ExecuteCommand();
+            db.Updateable<CodeFirstNoUpper4>().SetColumns(it => it.Name == "a").Where(it => it.Id != null).ExecuteCommand();
+            db.Updateable<CodeFirstNoUpper4>().SetColumns(it => new CodeFirstNoUpper4()
+            {
+                Name = "a"
+            }).Where(it => it.Id != null).ExecuteCommand();
             Console.WriteLine("#### CodeFirst end ####");
         }
     }
-
+    public class CodeFirstNoUpper4
+    {
+        [SugarColumn(IsPrimaryKey = true)]
+        public string Id { get; set; }
+        public string Name { get; set; }
+    }
     public class CodeFirstTable1
     {
         [SugarColumn(IsIdentity = true, IsPrimaryKey = true)]
