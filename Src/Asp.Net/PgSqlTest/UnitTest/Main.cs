9379477f8f54b8e6efe53a7842f4aa84be016940
sunkaixuan <610262374@qq.com>
2022-12-17 12:34:45 +0800

    Update pgsql

--- Src/Asp.Net/PgSqlTest/UnitTest/Main.cs
+++ Src/Asp.Net/PgSqlTest/UnitTest/Main.cs
@@ -31,6 +31,7 @@ namespace OrmTest
         }
         public static void Init()
         {
+            UInsert3.Init();
             UnitSubToList.Init();
             UnitByteArray.Init();
             Unit001.Init();
