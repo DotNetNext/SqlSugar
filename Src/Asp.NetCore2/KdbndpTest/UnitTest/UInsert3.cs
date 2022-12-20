0889e0d6f7eba956c9a81de7371a3210bc5171dc
sunkaixuan <610262374@qq.com>
2022-12-20 17:30:44 +0800

    Update Unit test

--- Src/Asp.NetCore2/KdbndpTest/UnitTest/UInsert3.cs
+++ Src/Asp.NetCore2/KdbndpTest/UnitTest/UInsert3.cs
@@ -11,7 +11,7 @@ namespace OrmTest
 {
     internal class UInsert3
     {
-        public static void Init() 
+        public static void Init()
         {
             var db = NewUnitTest.Db;
             db.Insertable(new Order() { Name = "a" }).ExecuteCommand();
@@ -22,8 +22,46 @@ namespace OrmTest
             }).ExecuteCommand();
 
             db.Insertable(new ORDER() { Name = "a" }).ExecuteCommand();
-        }
+            db.Updateable(new Order()
+            {
+                CustomId = 1,
+                CreateTime = DateTime.Now,
+                Id = 1,
+                Price = 1,
+                Name = "a"
+            }).ExecuteCommand();
+            db.Updateable(new List<Order>(){ new Order()
+            {
+                CustomId = 1,
+                CreateTime = DateTime.Now,
+                Id = 1,
+                Price = 1,
+                Name = "a"
+            },
+            new Order()
+            {
+                CustomId = 1,
+                CreateTime = DateTime.Now,
+                Id = 1,
+                Price = 1,
+                Name = "a"
+            } }).ExecuteCommand();
 
+
+            db.Updateable<Order>().SetColumns(it => new Order()
+            {
+                CustomId = 1,
+                Price = 1,
+                Name = "a"
+            }, true).Where(it => it.Id == 1).ExecuteCommand();
+
+            db.Updateable<ORDER>().SetColumns(it => new ORDER()
+            {
+                CustomId = 1,
+                Price = 1,
+                Name = "a"
+            }, true).Where(it => it.Id == 1).ExecuteCommand();
+        }
         public class Order
         {
             [SugarColumn(IsPrimaryKey = true, IsIdentity = true)]
@@ -33,7 +71,7 @@ namespace OrmTest
             /// </summary>
             public string Name { get; set; }
             public decimal Price { get; set; }
-            [SugarColumn(InsertServerTime =true)]
+            [SugarColumn(InsertServerTime =true,UpdateServerTime =true)]
             public DateTime CreateTime { get; set; }
             [SugarColumn(IsNullable = true)]
             public int CustomId { get; set; } 
@@ -48,7 +86,7 @@ namespace OrmTest
             /// </summary>
             public string Name { get; set; }
             public decimal Price { get; set; }
-            [SugarColumn(InsertSql = "'2020-1-1'")]
+            [SugarColumn(InsertSql = "'2020-1-1'",UpdateSql = "'2022-1-1'")]
             public DateTime CreateTime { get; set; }
             [SugarColumn(IsNullable = true)]
             public int CustomId { get; set; }
