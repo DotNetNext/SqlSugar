460747807d880f8390112aa8d31b019e9330496b
sunkaixuan <610262374@qq.com>
2022-12-21 15:22:59 +0800

    Add unit test

--- Src/Asp.Net/MySqlTest/UnitTest/Main.cs
+++ Src/Asp.Net/MySqlTest/UnitTest/Main.cs
@@ -31,6 +31,7 @@ namespace OrmTest
         }
         public static void Init()
         {
+            UnitSubToList001.Init();
             UInsert3.Init();
             UnitSubToList.Init();
             UCustom20.Init();
