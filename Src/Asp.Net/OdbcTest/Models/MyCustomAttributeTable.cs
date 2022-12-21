c83aabafe932fafc32ae701bd307c73906fadb88
sunkaixuan <610262374@qq.com>
2022-12-21 17:14:29 +0800

    Add odbc test

---
+++ Src/Asp.Net/OdbcTest/Models/MyCustomAttributeTable.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+ 
+using System.Linq;
+using System.Text;
+
+namespace OrmTest
+{
+    
+    //[SugarTable("CustomAttributeTable")]
+    public class MyCustomAttributeTable
+    {
+
+      
+        //[SugarColumn(IsPrimaryKey =true)]
+        public string Id { get; set; }
+        public string Name { get; set; }
+    }
+}
