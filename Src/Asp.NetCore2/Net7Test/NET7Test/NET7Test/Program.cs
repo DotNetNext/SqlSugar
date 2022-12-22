10df47cdefb1d3087512279fe9d487f5cfff41c5
sunkaixuan <610262374@qq.com>
2022-12-22 17:29:44 +0800

    Add .NET7  Test

---
+++ Src/Asp.NetCore2/Net7Test/NET7Test/NET7Test/Program.cs
@@ -0,0 +1,65 @@
+﻿using SqlSugar;
+
+ServerTest();
+
+Console.Read();
+static void MyTest()
+{
+    var sqlugar = new SqlSugarClient(new ConnectionConfig()
+    {
+        DbType = DbType.SqlServer,
+        ConnectionString = "SERVER=.;uid=sa;pwd=sasa;database=SqlSugar4Text4"
+    },
+    it =>
+    {
+        it.Aop.OnLogExecuting = (s, p) => Console.WriteLine(s, p);
+    });
+    sqlugar.DbMaintenance.CreateDatabase();
+    sqlugar.CodeFirst.InitTables<UnitDate01231>();
+    sqlugar.Insertable(new UnitDate01231()
+    {
+        dateOnly = DateOnly.FromDateTime(DateTime.Now),
+        timeOnly = TimeOnly.FromDateTime(DateTime.Now),
+    }).ExecuteCommand();
+
+
+
+    var list = sqlugar.Queryable<UnitDate01231>().OrderByDescending(it => it.dateOnly).ToList();
+
+    var d1 = new UnitDate01231().dateOnly;
+    var d2 = new UnitDate01231().timeOnly;
+}
+static void ServerTest()
+{
+    var sqlugar = new SqlSugarClient(new ConnectionConfig()
+    {
+        DbType = DbType.SqlServer,
+        ConnectionString = "SERVER=.;uid=sa;pwd=sasa;database=SqlSugar4Text4"
+    },
+    it =>
+    {
+        it.Aop.OnLogExecuting = (s, p) => Console.WriteLine(s, p);
+    });
+    sqlugar.DbMaintenance.CreateDatabase();
+    sqlugar.CodeFirst.InitTables<UnitDate01231>();
+    sqlugar.Insertable(new UnitDate01231()
+    {
+        dateOnly = DateOnly.FromDateTime(DateTime.Now),
+        timeOnly = TimeOnly.FromDateTime(DateTime.Now),
+    }).ExecuteCommand();
+
+
+
+    var list = sqlugar.Queryable<UnitDate01231>().OrderByDescending(it => it.dateOnly).ToList();
+
+    var d1 = new UnitDate01231().dateOnly;
+    var d2 = new UnitDate01231().timeOnly;
+}
+
+public class UnitDate01231
+{
+    [SugarColumn(ColumnDataType = "time")]
+    public TimeOnly timeOnly { get; set; }
+    [SugarColumn(ColumnDataType = "datetime")]
+    public DateOnly dateOnly { get; set; }
+}
