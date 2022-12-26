42f1995e5875fd75aa66f194f8d531ad827dfaf2
sunkaixuan <610262374@qq.com>
2022-12-26 11:22:44 +0800

    Update db.InsertableByObject

--- Src/Asp.NetCore2/SqlSugar/Abstract/InsertableProvider/InsertMethodInfo.cs
+++ Src/Asp.NetCore2/SqlSugar/Abstract/InsertableProvider/InsertMethodInfo.cs
@@ -1,10 +1,23 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Reflection;
 using System.Text;
 
 namespace SqlSugar 
 {
     public class InsertMethodInfo
     {
+        internal SqlSugarProvider Context { get; set; }
+        internal MethodInfo MethodInfo { get; set; }
+        internal object objectValue { get; set; }
+
+        public int ExecuteCommand()
+        {
+            if (Context == null) return 0;
+            var inertable=MethodInfo.Invoke(Context, new object[] { objectValue });
+            var result= inertable.GetType().GetMethod("ExecuteCommand").Invoke(inertable,new object[] { });
+            return (int)result;
+        }
     }
 }
