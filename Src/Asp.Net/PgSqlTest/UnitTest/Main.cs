61ac8721d29e18ae371cef1aad0c32944c05f6e5
sunkaixuan <610262374@qq.com>
2023-01-15 14:24:07 +0800

    Add unit test

--- Src/Asp.Net/PgSqlTest/UnitTest/Main.cs
+++ Src/Asp.Net/PgSqlTest/UnitTest/Main.cs
@@ -31,6 +31,7 @@ namespace OrmTest
         }
         public static void Init()
         {
+            ULock.Init();
             UInsert3.Init();
             UnitSubToList.Init();
             UnitByteArray.Init();
