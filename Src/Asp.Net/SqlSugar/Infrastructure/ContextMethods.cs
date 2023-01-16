af354cfaf523e8f0dfa8c5e39c560f31b8740850
sunkaixuan <610262374@qq.com>
2023-01-16 15:36:21 +0800

    Update nuget

--- Src/Asp.Net/SqlSugar/Infrastructure/ContextMethods.cs
+++ Src/Asp.Net/SqlSugar/Infrastructure/ContextMethods.cs
@@ -506,7 +506,9 @@ namespace SqlSugar
 
         private static bool IsArrayItem(Dictionary<string, object> readerValues, PropertyInfo item)
         {
-            return item.PropertyType.IsArray && readerValues.Any(y => y.Key.EqualCase(item.Name)) && readerValues.FirstOrDefault(y => y.Key.EqualCase(item.Name)).Value is string;
+            var isArray= item.PropertyType.IsArray && readerValues.Any(y => y.Key.EqualCase(item.Name)) && readerValues.FirstOrDefault(y => y.Key.EqualCase(item.Name)).Value is string;
+            var isListItem = item.PropertyType.IsIn(typeof(List<string>), typeof(List<int>), typeof(List<short>), typeof(List<long>), typeof(List<object>)) && readerValues.Any(y => y.Key.EqualCase(item.Name)) && readerValues.FirstOrDefault(y => y.Key.EqualCase(item.Name)).Value is string;
+            return isArray || isListItem;
         }
 
         private static bool IsJsonList(Dictionary<string, object> readerValues, PropertyInfo item)
