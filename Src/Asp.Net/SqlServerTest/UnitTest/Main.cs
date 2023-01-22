60c57f8afa47c2b17dda27e93993764df8372e2a
sunkaixuan <610262374@qq.com>
2023-01-22 15:35:25 +0800

    Add unit test

--- Src/Asp.Net/SqlServerTest/UnitTest/Main.cs
+++ Src/Asp.Net/SqlServerTest/UnitTest/Main.cs
@@ -31,6 +31,7 @@ namespace OrmTest
         }
         public static void Init()
         {
+            Unitadfa1231.Init();
             UInsert3.Init();
             USelectTest.Init();
             UnitSubToList.Init();
