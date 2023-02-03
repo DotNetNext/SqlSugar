bf769828116de6072cb8d1a586781d8e74ca2a2c
sunkaixuan <610262374@qq.com>
2023-02-03 16:18:15 +0800

    Add unit test bug

--- Src/Asp.Net/SqliteTest/UnitTest/Main.cs
+++ Src/Asp.Net/SqliteTest/UnitTest/Main.cs
@@ -31,6 +31,7 @@ namespace OrmTest
         }
         public static void Init()
         {
+            UnitFilterdafa.Init();
             UInsert3.Init();
             USaveable.Init();
             UnitSubToList.Init();
