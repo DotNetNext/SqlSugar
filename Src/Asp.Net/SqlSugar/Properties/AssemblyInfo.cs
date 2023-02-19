d1a3d4b16703ebcfdd992e6eacb03b5c96ea2a5f
sunkaixuan <610262374@qq.com>
2023-02-19 16:30:44 +0800

    5.1.3.51

--- Src/Asp.Net/SqlSugar/Properties/AssemblyInfo.cs
+++ Src/Asp.Net/SqlSugar/Properties/AssemblyInfo.cs
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("5.1.3.50")]
-[assembly: AssemblyFileVersion("5.1.3.50")]
+[assembly: AssemblyVersion("5.1.3.51")]
+[assembly: AssemblyFileVersion("5.1.3.51")]
