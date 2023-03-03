3094a2ddff41430978663ce406a84c4092f5ae15
sunkaixuan <610262374@qq.com>
2023-03-03 18:37:29 +0800

    Add unit test

---
+++ Src/Asp.Net/SqlServerTest/UnitTest/UnitTime.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrmTest 
+{
+    public class UnitTime
+    {
+        public static void Init()
+        {
+            var db = NewUnitTest.Db;
+            var xx=db.Queryable<Test>().Where(it => (it.a == DateTime.Now && it.a == DateTime.Now) || (it.a == DateTime.Now && it.a == DateTime.Now)).ToSql();
+            foreach (var item in xx.Value)
+            {
+                Console.WriteLine(item.DbType);
+            }
+        }
+        public class Test 
+        {
+            [SqlSugar.SugarColumn(SqlParameterDbType =System.Data.DbType.Date)]
+            public DateTime a { get; set; }
+        }
+    }
+}
