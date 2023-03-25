23fdc2b6a99fc789a40faed6bfaca98839ae99ad
sunkaixuan <610262374@qq.com>
2023-03-25 15:39:50 +0800

    Add  CommonPropertyConvert

--- Src/Asp.Net/SqlSugar/Entities/DefaultCustom.cs
+++ Src/Asp.Net/SqlSugar/Entities/DefaultCustom.cs
@@ -32,4 +32,22 @@ namespace SqlSugar.DbConvert
             return (T)Enum.Parse(undertype, str);
         }
     }
+
+
+    public class CommonPropertyConvert : ISugarDataConverter
+    {
+        public SugarParameter ParameterConverter<T>(object columnValue, int columnIndex)
+        {
+            var name = "@MyEnmu" + columnIndex;
+            Type undertype = SqlSugar.UtilMethods.GetUnderType(typeof(T));//获取没有nullable的枚举类型
+            return new SugarParameter(name, columnValue, undertype);
+        }
+
+        public T QueryConverter<T>(IDataRecord dr, int i)
+        {
+
+            var value = dr.GetValue(i);
+            return (T)UtilMethods.ChangeType2(value, typeof(T));
+        }
+    }
 }
