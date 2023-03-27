bce8b154b5bf37e80168401c3dd96fadd74e3d22
sunkaixuan <610262374@qq.com>
2023-03-27 18:26:41 +0800

    Add:  Queryable.IncludesAllSecondLayer(exp)

--- Src/Asp.Net/SqlSugar/Interface/IIncludes.cs
+++ Src/Asp.Net/SqlSugar/Interface/IIncludes.cs
@@ -14,8 +14,10 @@ namespace SqlSugar
     public partial interface ISugarQueryable<T>
     {
         NavISugarQueryable<T> AsNavQueryable();
+        ISugarQueryable<T> IncludesByExpression2<TReturn1, TReturn2>(Expression include1, Expression include2);
         ISugarQueryable<T> IncludesByExpression<TReturn1>(Expression include1);
         ISugarQueryable<T> IncludesAllFirstLayer(params string[] ignoreProperyNameList);
+        ISugarQueryable<T> IncludesAllSecondLayer<TReturn1>(Expression<Func<T, TReturn1>> expression,params string[] ignoreProperyNameList);
         ISugarQueryable<T> Includes<TReturn1>(Expression<Func<T, List<TReturn1>>> include1);
         ISugarQueryable<T> Includes<TReturn1>(Expression<Func<T, TReturn1>> include1);
         ISugarQueryable<T> Includes<TReturn1, TReturn2>(Expression<Func<T, List<TReturn1>>> include1, Expression<Func<TReturn1, List<TReturn2>>> include2);
