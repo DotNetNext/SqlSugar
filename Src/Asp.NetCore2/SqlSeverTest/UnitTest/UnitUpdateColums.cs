6f58bf78049e5abb8b530eabd08c9c9b0b29a1d7
sunkaixuan <610262374@qq.com>
2023-04-04 20:34:18 +0800

    Add unit test

---
+++ Src/Asp.NetCore2/SqlSeverTest/UnitTest/UnitUpdateColums.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OrmTest
+{
+    internal class UnitUpdateColums
+    {
+        public static void Init()
+        {
+            var db = NewUnitTest.Db;
+            db.CodeFirst.InitTables<DemoUpdateColums>();
+            db.Updateable(new DemoUpdateColums() { Id = 1 }).UpdateColumns(it => new
+            {
+                it.DBTYPE,
+                it.Name
+            }).ExecuteCommand();
+        }
+
+    }
+    public class DemoUpdateColums 
+    {
+        [SqlSugar.SugarColumn(IsPrimaryKey =true,ColumnName ="MYID")]
+        public int Id { get; set; }
+        [SqlSugar.SugarColumn(ColumnName = "myDBTYPE")]
+        public SqlSugar.DbType? DBTYPE { get; set; }
+        [SqlSugar.SugarColumn(ColumnName = "myname")]
+        public String Name { get; set; }
+    }
+}
