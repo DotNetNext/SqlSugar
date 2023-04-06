061746affe595c79788e29075efb6b52c3ca8b88
sunkaixuan <610262374@qq.com>
2023-04-06 12:27:51 +0800

    Update exp to sql

--- Src/Asp.Net/SqlSugar/ExpressionsToSql/ResolveItems/MemberConstExpressionResolve.cs
+++ Src/Asp.Net/SqlSugar/ExpressionsToSql/ResolveItems/MemberConstExpressionResolve.cs
@@ -35,6 +35,28 @@ namespace SqlSugar
 
         private void Where(ExpressionParameter parameter, bool? isLeft, object value, ExpressionParameter baseParameter, bool isSetTempData)
         {
+            if (parameter.OppsiteExpression != null) 
+            {
+                var exp = ExpressionTool.RemoveConvert(parameter.OppsiteExpression);
+                if (exp is MemberExpression) 
+                {
+                    var member=(exp as MemberExpression);
+                    var memberParent = member.Expression;
+                    if (memberParent != null && this.Context?.SugarContext?.Context != null)
+                    {
+                        var entity = this.Context.SugarContext.Context.EntityMaintenance.GetEntityInfo(memberParent.Type);
+                        var columnInfo = entity.Columns.FirstOrDefault(it => it.PropertyName == member.Member.Name);
+                        if (columnInfo?.SqlParameterDbType is Type)
+                        {
+                            var type = columnInfo.SqlParameterDbType as Type;
+                            var ParameterConverter = type.GetMethod("ParameterConverter").MakeGenericMethod(columnInfo.PropertyInfo.PropertyType);
+                            var obj = Activator.CreateInstance(type);
+                            var p = ParameterConverter.Invoke(obj, new object[] { value, 100+this.ContentIndex }) as SugarParameter;
+                            value = p.Value; 
+                        }
+                    }
+                }
+            }
             if (isSetTempData)
             {
                 baseParameter.CommonTempData = value;
