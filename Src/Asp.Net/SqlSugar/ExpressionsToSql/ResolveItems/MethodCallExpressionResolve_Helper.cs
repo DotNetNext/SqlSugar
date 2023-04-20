0c97101845f3b580da7e6d27c48ac881e37b6029
sunkaixuan <610262374@qq.com>
2023-04-21 06:45:58 +0800

    Update enum to sql

--- Src/Asp.Net/SqlSugar/ExpressionsToSql/ResolveItems/MethodCallExpressionResolve_Helper.cs
+++ Src/Asp.Net/SqlSugar/ExpressionsToSql/ResolveItems/MethodCallExpressionResolve_Helper.cs
@@ -279,7 +279,7 @@ namespace SqlSugar
             }
             else
             {
-                AppendModel(parameter, model, item);
+                AppendModel(parameter, model, item,name);
             }
         }
 
@@ -334,7 +334,7 @@ namespace SqlSugar
                 Check.Exception(true, "The SqlFunc.IIF(arg1,arg2,arg3) , {0} argument  do not support ", item.ToString());
             }
         }
-        private void AppendModel(ExpressionParameter parameter, MethodCallExpressionModel model, Expression item)
+        private void AppendModel(ExpressionParameter parameter, MethodCallExpressionModel model, Expression item,string name)
         {
             parameter.CommonTempData = CommonTempDataType.Result;
             base.Expression = item;
@@ -408,6 +408,10 @@ namespace SqlSugar
                 var parameterName = this.Context.SqlParameterKeyWord + ExpressionConst.MethodConst + this.Context.ParameterIndex;
                 this.Context.ParameterIndex++;
                 methodCallExpressionArgs.MemberName = parameterName;
+                if (name == "ToString"&&UtilMethods.GetUnderType(base.Expression.Type).IsEnum()) 
+                {
+                    value = value?.ToString();
+                }
                 methodCallExpressionArgs.MemberValue = value;
                 this.Context.Parameters.Add(new SugarParameter(parameterName, value));
             }
