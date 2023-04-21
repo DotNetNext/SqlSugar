807cab9dedf942b1158fe0b5983bd46537cb2972
sunkaixuan <610262374@qq.com>
2023-04-21 23:29:17 +0800

    Synchronization code

--- Src/Asp.NetCore2/SqlSugar/ExpressionsToSql/Subquery/Items/SubWhereIF.cs
+++ Src/Asp.NetCore2/SqlSugar/ExpressionsToSql/Subquery/Items/SubWhereIF.cs
@@ -58,7 +58,13 @@ namespace SqlSugar
                 return "WHERE 1=1 ";
             }
             var argExp = exp.Arguments[1];
-            var result = "WHERE " + SubTools.GetMethodValue(Context, argExp, ResolveExpressType.WhereMultiple); ;
+            var copyContext = this.Context;
+            if (this.Context.JoinIndex > 0)
+            {
+                copyContext = this.Context.GetCopyContextWithMapping();
+                copyContext.IsSingle = false;
+            }
+            var result = "WHERE " + SubTools.GetMethodValue(copyContext, argExp, ResolveExpressType.WhereMultiple); ;
             var selfParameterName = Context.GetTranslationColumnName((argExp as LambdaExpression).Parameters.First().Name) + UtilConstants.Dot;
             if (this.Context.JoinIndex == 0)
                 result = result.Replace(selfParameterName, SubTools.GetSubReplace(this.Context));
