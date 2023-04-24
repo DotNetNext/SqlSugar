4310c0328f30a99a44f281b01edf02c565570908
sunkaixuan <610262374@qq.com>
2023-04-24 17:45:58 +0800

    Add demo

--- Src/Asp.Net/PgSqlTest/Demo/Demo2_Updateable.cs
+++ Src/Asp.Net/PgSqlTest/Demo/Demo2_Updateable.cs
@@ -69,9 +69,12 @@ namespace OrmTest
                   .SetColumns(it => it.CreateTime == DateTime.Now)
                 .Where(it => it.Id == 1).ExecuteCommand();
             //
-
-
-
+            var result61 = db.Updateable<Order>()
+               .InnerJoin<Custom>((x, y) => x.CustomId == y.Id)
+               .SetColumns((x, y) => new Order() { Name = y.Name, Price = y.Id })
+               .Where((x, y) => x.Id == 1)
+               .ExecuteCommand();
+             
 
             /*** 3.by Dictionary ***/
             var dt = new Dictionary<string, object>();
