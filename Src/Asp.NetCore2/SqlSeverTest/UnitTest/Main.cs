7146eeceb75eaf0b363bf2f27144aec793abca2a
sunkaixuan <610262374@qq.com>
2023-04-25 11:44:39 +0800

    Add unit test

--- Src/Asp.NetCore2/SqlSeverTest/UnitTest/Main.cs
+++ Src/Asp.NetCore2/SqlSeverTest/UnitTest/Main.cs
@@ -31,6 +31,7 @@ namespace OrmTest
         }
         public static void Init()
         {
+            UinitCustomConvert.Init();
             UnitCustom020.Init();
             UnitSubToList.Init();
             UJsonsdafa.Init();
