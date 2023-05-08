9000e7007336919cd48b126b0a1792378fd1c3c3
sunkaixuan <610262374@qq.com>
2023-05-09 00:01:07 +0800

    Synchronization code

--- Src/Asp.Net/SqlSugar/Realization/Oracle/OracleProvider.cs
+++ Src/Asp.Net/SqlSugar/Realization/Oracle/OracleProvider.cs
@@ -148,15 +148,14 @@ namespace SqlSugar
         }
         public override Action<SqlSugarException> ErrorEvent => it =>
         {
-
-            if (it.Message != null && it.Message.Contains("无效的主机/绑定变量名"))
-            {
-                Check.ExceptionEasy(it.Message, $"错误：{it.Message}，出现这个错的原因： 1.可能是参数名为关键词（例如 @user ）2. SQL错误。");
-            }
             if (base.ErrorEvent != null)
             {
                 base.ErrorEvent(it);
             }
+            if (it.Message != null && it.Message.Contains("无效的主机/绑定变量名"))
+            {
+                Check.ExceptionEasy(it.Message, $"错误：{it.Message}，出现这个错的原因： 1.可能是参数名为关键词（例如 @user ）2. SQL错误。");
+            } 
         };
         public override void SetCommandToAdapter(IDataAdapter dataAdapter, DbCommand command)
         {
