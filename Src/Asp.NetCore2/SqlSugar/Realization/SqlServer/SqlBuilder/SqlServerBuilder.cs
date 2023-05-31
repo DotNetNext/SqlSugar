75c1a5a08b8d18585d98331183db8bc95891169e
sunkaixuan <610262374@qq.com>
2023-05-31 23:28:20 +0800

    Update  SqlServerBuilder

--- Src/Asp.NetCore2/SqlSugar/Realization/SqlServer/SqlBuilder/SqlServerBuilder.cs
+++ Src/Asp.NetCore2/SqlSugar/Realization/SqlServer/SqlBuilder/SqlServerBuilder.cs
@@ -14,7 +14,7 @@ namespace SqlSugar
 
         public override string RemoveParentheses(string sql)
         {
-            if (sql.Contains("ORDER BY")&&!sql.StartsWith("(SELECT TOP 1")) 
+            if (sql.Contains("ORDER BY")&&!sql.StartsWith("(SELECT TOP 1")&&!sql.Contains("(SELECT TOP 1000000")) 
             {
                 sql = $"SELECT * FROM {sql.Replace("(SELECT ", "(SELECT TOP 1000000")} TEMP";
             }
