b17d01b7cc1ba5694dcfddb4b03417b00280b1a2
sunkaixuan <610262374@qq.com>
2023-06-11 12:35:05 +0800

    Add unit test

---
+++ Src/Asp.Net/SqlServerTest/UnitTest/UQueryableByObject.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrmTest 
+{
+    internal class UQueryableByObject
+    {
+        public static void Init()
+        {
+            var db = NewUnitTest.Db;
+            var type = typeof(Order);
+            var data=db.QueryableByObject(type)
+                .Where("id=1").ToList();
+            var data2=db.QueryableByObject(type)
+                .Where("id=@id",new {id=1 }).ToList();
+            var data3 = db.QueryableByObject(type)
+                .OrderBy("id").Where("id=@id", new { id = 1 }).ToList();
+            var data4 = db.QueryableByObject(type)
+                .GroupBy("id").Having("count(name)>0").Select("Id").ToList();
+        }
+    }
+}
