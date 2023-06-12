e21226d9b1ab9d0ed5de14536ea7e79ea0a2fb38
sunkaixuan <610262374@qq.com>
2023-06-12 23:02:47 +0800

    Add unit test

--- Src/Asp.Net/SqlServerTest/UnitTest/UQueryableByObject.cs
+++ Src/Asp.Net/SqlServerTest/UnitTest/UQueryableByObject.cs
@@ -19,7 +19,14 @@ namespace OrmTest
             var data3 = db.QueryableByObject(type)
                 .OrderBy("id").Where("id=@id", new { id = 1 }).ToList();
             var data4 = db.QueryableByObject(type)
+                .AS("order","o")
                 .GroupBy("id").Having("count(name)>0").Select("Id").ToList();
+            var data5 = db.QueryableByObject(type)
+                .AS("order")
+                .GroupBy("id").Having("count(name)>0").Select("Id").ToList();
+            var data6= db.QueryableByObject(type,"o")
+             .AddJoinInfo("order","y","o.id=y.id",SqlSugar.JoinType.Left)
+             .GroupBy("o.id").Having("count(o.name)>0").Select("o.Id").ToList();
         }
     }
 }
