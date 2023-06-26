7f4ac2ddc18a4186c4f77de7f7596911e0d52f42
sunkaixuan <610262374@qq.com>
2023-06-26 18:32:39 +0800

    Add unit test

--- Src/Asp.NetCore2/SqlSeverTest/UnitTest/Main.cs
+++ Src/Asp.NetCore2/SqlSeverTest/UnitTest/Main.cs
@@ -31,6 +31,7 @@ namespace OrmTest
         }
         public static void Init()
         {
+            UpdateNavOneToOne.Init();
             Unitasf1.Init();
             UOneManyMany7.init();
             UOneManyMany6.init();
