4a9316407cc42fe6f54565e09fb28220fdb5c3a9
sunkaixuan <610262374@qq.com>
2023-07-01 23:27:21 +0800

    Synchronization code

--- Src/Asp.Net/SqlSugar/Realization/Dm/CodeFirst/DmCodeFirst.cs
+++ Src/Asp.Net/SqlSugar/Realization/Dm/CodeFirst/DmCodeFirst.cs
@@ -50,6 +50,32 @@ namespace SqlSugar
             }
             return result;
         }
+        protected override void GetDbType(EntityColumnInfo item, Type propertyType, DbColumnInfo result)
+        {
+            if (!string.IsNullOrEmpty(item.DataType))
+            {
+                result.DataType = item.DataType;
+            }
+            else if (propertyType.IsEnum())
+            {
+                result.DataType = this.Context.Ado.DbBind.GetDbTypeName(item.Length > 9 ? UtilConstants.LongType.Name : UtilConstants.IntType.Name);
+            }
+            else
+            {
+                if (propertyType.Name.Equals("Guid", StringComparison.CurrentCultureIgnoreCase))
+                {
+                    result.DataType = this.Context.Ado.DbBind.GetDbTypeName(UtilConstants.StringType.Name);
+                    if (result.Length <= 1)
+                    {
+                        result.Length = 36;
+                    }
+                }
+                else
+                {
+                    result.DataType = this.Context.Ado.DbBind.GetDbTypeName(propertyType.Name);
+                }
+            }
+        }
 
         protected override void ConvertColumns(List<DbColumnInfo> dbColumns)
         {
