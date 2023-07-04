7ff71a9aba734476d83cdd1bd01c242883e33081
sunkaixuan <610262374@qq.com>
2023-07-04 15:19:00 +0800

    Synchronization code

--- Src/Asp.Net/SqlSugar/Infrastructure/ContextMethods.cs
+++ Src/Asp.Net/SqlSugar/Infrastructure/ContextMethods.cs
@@ -45,7 +45,7 @@ namespace SqlSugar
                         var propertyType = propertyTypes[i];
 
                         var propertyInfo = tupleType.GetFields()[i];
-                        propertyInfo.SetValue(tupleInstance, UtilMethods.ChangeType2(propertyValue, propertyType));
+                        propertyInfo.SetValue(tupleInstance, UtilMethods.ChangeType2(propertyValue, propertyInfo.FieldType));
                     }
 
                     // Add the tuple instance to the result list
