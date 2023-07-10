b203a0a1996858c5b424ed1991b25d95cf9efdaa
sunkaixuan <610262374@qq.com>
2023-07-10 20:37:16 +0800

    Synchronization code

--- Src/Asp.Net/SqlSugar/Realization/Dm/DmProvider.cs
+++ Src/Asp.Net/SqlSugar/Realization/Dm/DmProvider.cs
@@ -214,6 +214,16 @@ namespace SqlSugar
 
             return sql;
         }
-
+        public override Action<SqlSugarException> ErrorEvent => it =>
+        {
+            if (base.ErrorEvent != null)
+            {
+                base.ErrorEvent(it);
+            }
+            if (it.Message != null && it.Message.Contains("Detail redacted as it may contain sensitive data."))
+            {
+                Check.ExceptionEasy(it.Message, $"错误：可能是字段太小超出,详细错误：{it.Message} ");
+            }
+        };
     }
 }
