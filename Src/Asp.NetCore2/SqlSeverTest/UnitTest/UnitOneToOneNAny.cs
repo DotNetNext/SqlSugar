50f2a0a46ed8ff0ceef63a7dc0085d73346da9db
sunkaixuan <610262374@qq.com>
2023-07-11 12:48:48 +0800

    Add unit test

--- Src/Asp.NetCore2/SqlSeverTest/UnitTest/UnitOneToOneNAny.cs
+++ Src/Asp.NetCore2/SqlSeverTest/UnitTest/UnitOneToOneNAny.cs
@@ -13,7 +13,15 @@ namespace OrmTest
             db.CodeFirst.InitTables<EmpLicenseLogOff, EmpInformation, EmpDepartmentJob,EmpLicense>();
             db.Queryable<EmpLicenseLogOff>()
                 .Where(it => it.EmpLicense.EmpInformation.EmpDepartmentJobs.Any())
-                .ToList(); 
+                .ToList();
+
+            var sql = db.Queryable<Order>()
+               .Where(b =>
+                       $"{b.Name}^{b.Name}^{b.Name}^{b.Id}^{1}  ".Contains("a")).ToSqlString();
+            if (!sql.Contains("'[Name]^[Name]^[Name]^[Id]^ 1 ' like")) 
+            {
+                throw new Exception("unit error");
+            }
         }
     }
 }
