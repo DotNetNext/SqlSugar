1321c63615d13d19c74001f885f4c8a49651a3dd
sunkaixuan <610262374@qq.com>
2023-07-20 00:23:43 +0800

    Add unit test

--- Src/Asp.NetCore2/SqlSeverTest/UnitTest/Main.cs
+++ Src/Asp.NetCore2/SqlSeverTest/UnitTest/Main.cs
@@ -31,6 +31,7 @@ namespace OrmTest
         }
         public static void Init()
         {
+            AnimalTest.Init();
             UnitOneToOneNAny.Init();
             Unitrasdfa.Init();
             Unitadfasdfa.Init();
