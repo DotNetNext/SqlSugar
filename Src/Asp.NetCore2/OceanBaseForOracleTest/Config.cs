b9535987265b145fe4553f7ea94ab4947722b0b3
guoshun.du <guoshun.du@xquant.com>
2023-07-19 19:51:39 +0800

    *添加OceanBaseForOracleTest项目

---
+++ Src/Asp.NetCore2/OceanBaseForOracleTest/Config.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrmTest
+{
+    /// <summary>
+    /// Setting up the database name does not require you to create the database
+    /// 设置好数据库名不需要你去手动建库
+    /// </summary>
+    public class Config
+    {
+        /// <summary>
+        /// Account have permission to create database
+        /// 用有建库权限的数据库账号
+        /// </summary>
+        public static string ConnectionString = "Driver={OceanBase ODBC 2.0 Driver};Server=172.19.9.9;Port=2883;Database=XIR_TRD;User=XIR_TRD@Xpia2C6G#obtest:1650773680;Password=aaAA11%%;Option=3;";
+        /// <summary>
+        /// Account have permission to create database
+        /// 用有建库权限的数据库账号
+        /// </summary>
+        public static string ConnectionString2 = "Driver={GBase ODBC DRIVER (64-Bit)};Host=localhost;Service=19088;Server=gbase01;Database=testdb;Protocol=onsoctcp;Uid=gbasedbt;Pwd=GBase123;Db_locale=zh_CN.utf8;Client_locale=zh_CN.utf8";
+        /// <summary>
+        /// Account have permission to create database
+        /// 用有建库权限的数据库账号
+        /// </summary>
+        public static string ConnectionString3 = "Driver={GBase ODBC DRIVER (64-Bit)};Host=localhost;Service=19088;Server=gbase01;Database=testdb;Protocol=onsoctcp;Uid=gbasedbt;Pwd=GBase123;Db_locale=zh_CN.utf8;Client_locale=zh_CN.utf8";
+    }
+}
---
+++ Src/Asp.NetCore2/OceanBaseForOracleTest/Program.cs
@@ -0,0 +1,54 @@
+﻿using OrmTest;
+using SqlSugar;
+using static Npgsql.Replication.PgOutput.Messages.RelationMessage;
+
+namespace OceanBaseForOracle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("");
+            Console.WriteLine("#### MasterSlave Start ####");
+            SqlSugarClient db = new SqlSugarClient(new ConnectionConfig()
+            {
+                ConnectionString = Config.ConnectionString,//Master Connection
+                DbType = DbType.OceanBaseForOracle,
+                InitKeyType = InitKeyType.Attribute,
+                IsAutoCloseConnection = true
+            });
+            db.Aop.OnLogExecuted = (s, p) =>
+            {
+                Console.WriteLine(db.Ado.Connection.ConnectionString);
+            };
+            Console.WriteLine("Master:");
+            db.Insertable(new Order() { Id=109,Name = "abc", CustomId = 1, CreateTime = DateTime.Now }).ExecuteCommand();
+            db.Deleteable<Order>().Where(m => m.Id == 109).ExecuteCommand();
+            db.Updateable<Order>().SetColumns(m => new Order
+            {
+                Name = "我是修改"
+            }).Where(m => m.Id == 2).ExecuteCommand();
+            Console.WriteLine("Slave:");
+            //var s = db.Queryable<Order>().First();
+            //var list = db.Queryable<Order>().Select(m => new Order
+            //{
+            //    Id = m.Id,
+            //    CreateTime = m.CreateTime,
+            //    CustomId = m.CustomId,
+            //    Idname = SqlFunc.Subqueryable<Order>().Where(s => s.Id == 2).Select(s => s.Name),
+            //    Name = m.Name,
+            //    Price = m.Price,
+            //}).ToList();
+            //var grouplist = db.Queryable<Order>().OrderByDescending(m=>m.Id).GroupBy(m=>new {m.Id,m.Name}).SelectMergeTable(m => new Order
+            //{
+            //    Id = m.Id,
+            //    Name = m.Name,
+            //    CreateTime= SqlFunc.AggregateMin(m.CreateTime),
+            //    Price= SqlFunc.AggregateSum(m.Price),
+            //}).OrderBy(m=>m.Id).Where(m=>m.Id==1).ToList();
+            //var orderlist = db.Queryable<Order>().OrderBy(m => new { m.Id, m.Name }).ToList();
+            var pageList = db.Queryable<Order>().OrderBy(m=>m.Id).ToOffsetPage(1, 3);
+            Console.WriteLine("#### MasterSlave End ####");
+        }
+    }
+}
