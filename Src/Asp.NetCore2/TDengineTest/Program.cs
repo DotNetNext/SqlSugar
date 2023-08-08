9a5f4fe03d126a34b3c94c9184ddd1c2750a47c8
sunkaixuan <610262374@qq.com>
2023-08-08 16:51:25 +0800

    Update TDengine DEMO

--- Src/Asp.NetCore2/TDengineTest/Program.cs
+++ Src/Asp.NetCore2/TDengineTest/Program.cs
@@ -8,8 +8,12 @@ namespace OrmTest
     {
         static void Main(string[] args)
         {
+            //需要安装：客户端SDK
+            //https://docs.taosdata.com/connector/#%E5%AE%89%E8%A3%85%E5%AE%A2%E6%88%B7%E7%AB%AF%E9%A9%B1%E5%8A%A8
+           
             //orm用例
             ORMTest.Init();
+
             //原生ado用例
             AdoDemo.Init();
          
