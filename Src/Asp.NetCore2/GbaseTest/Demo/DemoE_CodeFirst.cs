6f4e4f1894443ec9984cfea485a94b9e1be30634
sunkaixuan <610262374@qq.com>
2023-08-09 20:28:38 +0800

    Add GBase demo

--- Src/Asp.NetCore2/GbaseTest/Demo/DemoE_CodeFirst.cs
+++ Src/Asp.NetCore2/GbaseTest/Demo/DemoE_CodeFirst.cs
@@ -23,10 +23,26 @@ namespace OrmTest
             db.CodeFirst.InitTables(typeof(CodeFirstTable1));//Create CodeFirstTable1 
             db.Insertable(new CodeFirstTable1() { Name = "a", Text="a" }).ExecuteCommand();
             var list = db.Queryable<CodeFirstTable1>().ToList();
+            db.CodeFirst.InitTables<CodeFirstTable2>();
+            db.DbMaintenance.TruncateTable<CodeFirstTable2>();
+            db.Insertable(new CodeFirstTable2() { IsOk = true, Name = "a", Text = "a" }).ExecuteCommand();
+            db.Insertable(new CodeFirstTable2() { IsOk = false, Name = "a", Text = "a" }).ExecuteCommand();
+            var data= db.Queryable<CodeFirstTable2>().Where(it => it.IsOk==true).First();
+            var data2 = db.Queryable<CodeFirstTable2>().Where(it => it.IsOk == false).First();
+            db.Updateable(data).ExecuteCommand();
             Console.WriteLine("#### CodeFirst end ####");
         }
     }
-
+    public class CodeFirstTable2
+    {
+        [SugarColumn(IsIdentity = true, IsPrimaryKey = true)]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        [SugarColumn(ColumnDataType = "varchar(255)")]//custom
+        public string Text { get; set; }
+        [SugarColumn(IsNullable = true)]
+        public bool IsOk { get; set; }
+    }
     public class CodeFirstTable1
     {
         [SugarColumn(IsIdentity = true, IsPrimaryKey = true)]
