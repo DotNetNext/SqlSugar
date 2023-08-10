65429c9c12f2188777490c29e2cea33d0544d436
sunkaixuan <610262374@qq.com>
2023-08-10 18:02:59 +0800

    Update 达梦

--- Src/Asp.NetCore2/SqlSugar/Realization/Dm/DbBind/DmDbBind.cs
+++ Src/Asp.NetCore2/SqlSugar/Realization/Dm/DbBind/DmDbBind.cs
@@ -136,6 +136,13 @@ namespace SqlSugar
                   new KeyValuePair<string, CSharpDataType>("timestamp with local time zone",CSharpDataType.DateTime),
                   new KeyValuePair<string, CSharpDataType>("timestamp with time zone",CSharpDataType.DateTime),
                   new KeyValuePair<string, CSharpDataType>("timestamp with time zone",CSharpDataType.DateTime),
+
+
+
+                  new KeyValuePair<string, CSharpDataType>("timestamp with local time zone",CSharpDataType.DateTimeOffset),
+                  new KeyValuePair<string, CSharpDataType>("timestamp with time zone",CSharpDataType.DateTimeOffset),
+                  new KeyValuePair<string, CSharpDataType>("timestamp with time zone",CSharpDataType.DateTimeOffset),
+
                   new KeyValuePair<string, CSharpDataType>("time",CSharpDataType.TimeSpan),
 
                   new KeyValuePair<string, CSharpDataType>("float",CSharpDataType.@decimal),
