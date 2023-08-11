7461207551d56213a0fcd447a1ca7a363db6d13b
sunkaixuan <610262374@qq.com>
2023-08-11 14:17:14 +0800

    Update 人大金仓  Index

--- Src/Asp.NetCore2/SqlSugar/Realization/Kdbndp/DbMaintenance/KdbndpDbMaintenance.cs
+++ Src/Asp.NetCore2/SqlSugar/Realization/Kdbndp/DbMaintenance/KdbndpDbMaintenance.cs
@@ -192,7 +192,7 @@ namespace SqlSugar
         {
             get
             {
-                return "  Select count(1) from (SELECT to_regclass('Index_UnitCodeTest1_Id_CreateDate') as c ) t where t.c is not null";
+                return "SELECT count(1) WHERE upper('{0}') IN ( SELECT upper(indexname) FROM sys_indexes ) ";
             }
         }
         protected override string IsAnyProcedureSql => throw new NotImplementedException();
