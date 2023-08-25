878d4389f52f55a63f0bb7c3af27dca975f59663
sunkaixuan <610262374@qq.com>
2023-08-26 03:45:13 +0800

    -

---
+++ Src/Asp.NetCore2/SqlSugar.TDengineCore/Tools/DateTime16.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+
+namespace SqlSugar 
+{
+    public class DateTime16: ISugarDataConverter
+    {
+        public SugarParameter ParameterConverter<T>(object columnValue, int columnIndex)
+        {
+            var name = "@Common" + columnIndex;
+            Type undertype = SqlSugar.UtilMethods.GetUnderType(typeof(T));//获取没有nullable的枚举类型
+            return new SugarParameter(name, columnValue, undertype) { CustomDbType=System.Data.DbType.DateTime2 };
+        }
+
+        public T QueryConverter<T>(IDataRecord dr, int i)
+        {
+
+            var value = dr.GetValue(i);
+            return (T)UtilMethods.ChangeType2(value, typeof(T));
+        }
+    }
+}
