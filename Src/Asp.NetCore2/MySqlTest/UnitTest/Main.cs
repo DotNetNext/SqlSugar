e2aefba6056a8965c77c5aeddf0a986a55358f7b
sunkaixuan <610262374@qq.com>
2023-10-04 20:36:13 +0800

    Add unit  test

--- Src/Asp.NetCore2/MySqlTest/UnitTest/Main.cs
+++ Src/Asp.NetCore2/MySqlTest/UnitTest/Main.cs
@@ -31,6 +31,7 @@ namespace OrmTest
         }
         public static void Init()
         {
+            UnitBulkMerge.Init();
             UnitUpdateNavOneToOneFalse.Init();
             UnitCreateType.Init();
             UnitSubToList.Init();
