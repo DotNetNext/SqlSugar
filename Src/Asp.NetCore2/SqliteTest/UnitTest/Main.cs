c39a7d7cd57e6dfa89bb0269d9dca8d563c53415
sunkaixuan <610262374@qq.com>
2023-10-04 20:49:45 +0800

    Add unit test

--- Src/Asp.NetCore2/SqliteTest/UnitTest/Main.cs
+++ Src/Asp.NetCore2/SqliteTest/UnitTest/Main.cs
@@ -31,6 +31,7 @@ namespace OrmTest
         }
         public static void Init()
         {
+            UnitBulkMerge.Init();
             UnitBizDelete.Init();
             UnitBulkCopyUpdateaasfa.Init();
             UnitSubToList.Init();
