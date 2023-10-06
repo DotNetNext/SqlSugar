d01377d1d4572d72f431420bfb2c987261a55164
sunkaixuan <610262374@qq.com>
2023-10-07 01:13:26 +0800

    Synchronization code

--- Src/Asp.Net/SqlSugar/Abstract/QueryableProvider/QueryMethodInfo.cs
+++ Src/Asp.Net/SqlSugar/Abstract/QueryableProvider/QueryMethodInfo.cs
@@ -151,6 +151,13 @@ namespace SqlSugar
             this.QueryableObj = method.Invoke(QueryableObj, new object[] { models });
             return this;
         }
+        public QueryMethodInfo Select(string expShortName, FormattableString expSelect, Type resultType)
+        {
+            var method = QueryableObj.GetType().GetMyMethod("Select", 3, typeof(string),typeof(FormattableString),typeof(Type));
+            method= method.MakeGenericMethod(resultType);
+            this.QueryableObj = method.Invoke(QueryableObj, new object[] { expShortName, expSelect, resultType });
+            return this;
+        }
         public QueryMethodInfo Select(string selectorSql)
         {
             var method = QueryableObj.GetType().GetMyMethod("Select", 1, typeof(string))
