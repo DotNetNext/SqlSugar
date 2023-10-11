18a25a0e68b7001eb3323ffb7f60643027a7ce00
sunkaixuan <610262374@qq.com>
2023-10-11 17:29:00 +0800

    Update CodeFirst IndexName

--- Src/Asp.NetCore2/SqlSugar/Abstract/CodeFirstProvider/CodeFirstProvider.cs
+++ Src/Asp.NetCore2/SqlSugar/Abstract/CodeFirstProvider/CodeFirstProvider.cs
@@ -344,13 +344,10 @@ namespace SqlSugar
                             include=Regex.Match( item.IndexName,@"\{include\:.+$").Value;
                             item.IndexName = item.IndexName.Replace(include, "");
                         }
-                        if (item.IndexName.Contains("."))
+                        if (item.IndexName.Contains(".")&& item.IndexName.Contains("["))
                         {
                             item.IndexName = item.IndexName.Replace(",","_");
-                            if (item.IndexName.Contains("[")) 
-                            {
-                                item.IndexName = item.IndexName.Replace("[", "").Replace("]", "");
-                            }
+                            item.IndexName = item.IndexName.Replace("[", "").Replace("]", "");
                         }
                     }
                     if (!this.Context.DbMaintenance.IsAnyIndex(item.IndexName))
