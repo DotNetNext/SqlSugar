e673afbab007a72e5d0047609085e2bc94ceb7fd
sunkaixuan <610262374@qq.com>
2023-10-22 23:30:25 +0800

    Add demo

--- Src/Asp.NetCore2/OracleTest/Demo/Demo6_Queue.cs
+++ Src/Asp.NetCore2/OracleTest/Demo/Demo6_Queue.cs
@@ -11,7 +11,18 @@ namespace OrmTest
     {
         public static void Init()
         {
-           //NoSupport
+            var db = NewUnitTest.Db;
+            var insertObjs = new List<Order> {
+                 new Order() { Id = 11, Name = "order11", Price=0 },
+                 new Order() { Id = 12, Name = "order12" , Price=0}
+            };
+            var insertObj = new Order() { Id = 1, Name = "order1", Price = 0 };
+       
+            //blukcopy
+            db.Insertable(insertObjs).AddQueue();
+            db.Insertable(insertObj).AddQueue();
+             
+            db.SaveQueues();
         }
     }
 }
