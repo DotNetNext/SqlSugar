c43cf6296f4fa5e064f62f3b9d9e2b4e4c1fd745
sunkaixuan <610262374@qq.com>
2023-10-22 22:44:21 +0800

    Add demo

--- Src/Asp.NetCore2/OracleTest/Demo/Demo7_Ado.cs
+++ Src/Asp.NetCore2/OracleTest/Demo/Demo7_Ado.cs
@@ -50,6 +50,7 @@ namespace OrmTest
             var list= db.Ado.SqlQuery<Order>("select * from \"ORDER\"");
             var intValue=db.Ado.SqlQuerySingle<int>("select 1 from dual");
             db.Ado.ExecuteCommand("delete \"ORDER\" where id>1000");
+            db.Ado.ExecuteCommand("select 1 from dual;");//Supprt ";"
             //db.Ado.xxx
             Console.WriteLine("#### Ado End ####");
         }
