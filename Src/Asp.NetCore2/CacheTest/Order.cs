b1a758fe61fe04a102550e986dd53b81c705d52a
sunkaixuan <610262374@qq.com>
2023-10-24 18:06:15 +0800

    Add demo

---
+++ Src/Asp.NetCore2/CacheTest/Order.cs
@@ -0,0 +1,23 @@
+﻿using SqlSugar;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+ namespace CacheTest
+
+{
+
+    public class Order
+    {
+        [SugarColumn(IsPrimaryKey = true, IsIdentity = true)]
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        [SugarColumn(IsNullable = true)]
+        public DateTime CreateTime { get; set; }
+        [SugarColumn(IsNullable =true)]
+        public int CustomId { get; set; } 
+    }
+}
