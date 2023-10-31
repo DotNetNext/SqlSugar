ac8bfadac390046f9485df3f934e81a49b0bf430
sunkaixuan <610262374@qq.com>
2023-10-31 22:48:08 +0800

    Synchronization code

--- Src/Asp.NetCore2/SqlSugar/Realization/Oracle/SqlBuilder/OracleQueryBuilder.cs
+++ Src/Asp.NetCore2/SqlSugar/Realization/Oracle/SqlBuilder/OracleQueryBuilder.cs
@@ -87,6 +87,10 @@ namespace SqlSugar
             sql.Replace(UtilConstants.ReplaceKey, isRowNumber ? (isIgnoreOrderBy ? null : rowNumberString) : null);
             if (isIgnoreOrderBy) { this.OrderByValue = oldOrderBy; return sql.ToString(); }
             var result = ToPageSql(sql.ToString(), this.Take, this.Skip);
+            if (this.Take == 1 && this.Skip == 0&&oldOrderBy== "ORDER BY sysdate ") 
+            {
+                result = $" {sql.ToString()} {(this.WhereInfos.Any()?"AND":"WHERE")}   ROWNUM = 1 ";
+            }
             if (ExternalPageIndex > 0)
             {
                 if (externalOrderBy.IsNullOrEmpty())
