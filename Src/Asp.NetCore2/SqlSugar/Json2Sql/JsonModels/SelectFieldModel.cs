d8abcc1f55af9ede912f94733b7304ebecb6b859
sunkaixuan <610262374@qq.com>
2023-11-02 21:58:55 +0800

    Synchronization code

--- Src/Asp.NetCore2/SqlSugar/Json2Sql/JsonModels/SelectFieldModel.cs
+++ Src/Asp.NetCore2/SqlSugar/Json2Sql/JsonModels/SelectFieldModel.cs
@@ -7,7 +7,11 @@ namespace SqlSugar
 {
     public class SelectModel  
     {
-        public object FiledName { get; set; }
+        public object FieldName { get; set; }
+
+        [Obsolete("名字拼错使用FieldName")]
+        public object FiledName { get { return FieldName; }  set { FieldName = value; }  }
+
         public string AsName { get; set; }
 
         public static List<SelectModel> Create(params SelectModel[] SelectModels) 
--- Src/Asp.NetCore2/SqlSugar/Json2Sql/ModelToSql/SelectModelToSql.cs
+++ Src/Asp.NetCore2/SqlSugar/Json2Sql/ModelToSql/SelectModelToSql.cs
@@ -17,7 +17,7 @@ namespace SqlSugar
                 {
                     var orderByModel = item as SelectModel;
                     orderByModel.AsName=GetAsName(orderByModel);
-                    orderByModel.FiledName = GetSqlPart(orderByModel.FiledName, pars).ObjToString();
+                    orderByModel.FieldName = GetSqlPart(orderByModel.FieldName, pars).ObjToString();
                     AppendFiledName(sql, orderByModel);
                 }
                 else
@@ -32,7 +32,7 @@ namespace SqlSugar
         {
             if (orderByModel.AsName.IsNullOrEmpty())
             {
-                orderByModel.AsName = orderByModel.FiledName.ObjToString();
+                orderByModel.AsName = orderByModel.FieldName.ObjToString();
             }
             if (orderByModel.AsName.StartsWith(UtilConstants.ReplaceKey)) 
             {
@@ -48,7 +48,7 @@ namespace SqlSugar
 
         private  void AppendFiledName(StringBuilder sql, SelectModel orderByModel)
         {
-            sql.Append($" {orderByModel.FiledName} AS {orderByModel.AsName} ,");
+            sql.Append($" {orderByModel.FieldName} AS {orderByModel.AsName} ,");
         }
     }
 }
