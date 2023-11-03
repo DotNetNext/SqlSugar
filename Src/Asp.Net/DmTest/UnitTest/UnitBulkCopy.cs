9194f37aff25b3ebbbb1ffb1515a7e8a01abd34b
sunkaixuan <610262374@qq.com>
2023-11-04 00:42:00 +0800

    Add demo

---
+++ Src/Asp.Net/DmTest/UnitTest/UnitBulkCopy.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OrmTest
+{
+    internal class UnitBulkCopy
+    {
+        public static void Init()
+        {
+            var db = NewUnitTest.Db;
+            db.CodeFirst.InitTables<Unitadfa>();
+            db.Insertable(new Unitadfa()
+            {
+                Name = "A",
+                Date = DateTime.Now,
+            }).ExecuteCommand();
+            db.Insertable(new List<Unitadfa>() {
+            new Unitadfa()
+            {
+                Name = "A",
+                Date = DateTime.Now,
+            },
+            new Unitadfa()
+            {
+                Name = "A",
+                Date = DateTime.Now,
+            }}).ExecuteCommand();
+            var list = db.Queryable<Unitadfa>().ToList();
+            db.DbMaintenance.TruncateTable<Unitadfa>();
+            db.Fastest<Unitadfa>().OffIdentity().BulkCopy(list);
+            db.Insertable(new Unitadfa()
+            {
+                Name = "A",
+                Date = DateTime.Now,
+            }).ExecuteCommand();
+            db.DbMaintenance.DropTable<Unitadfa>();
+
+        }
+    }
+    public class Unitadfa
+    {
+        [SqlSugar.SugarColumn(IsIdentity = true, IsPrimaryKey = true)]
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+        [SqlSugar.SugarColumn(ColumnDataType = "Date")]
+        public DateTime Date { get; set; }
+    }
+}
