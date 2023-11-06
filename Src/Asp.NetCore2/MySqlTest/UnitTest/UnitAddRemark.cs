43f46a8c006ab73a6d7d8aed28225cab82950f2d
sunkaixuan <610262374@qq.com>
2023-11-06 13:21:08 +0800

    Update unit test

--- Src/Asp.NetCore2/MySqlTest/UnitTest/UnitAddRemark.cs
+++ Src/Asp.NetCore2/MySqlTest/UnitTest/UnitAddRemark.cs
@@ -12,8 +12,20 @@ namespace  SqlSugar
             var db = NewUnitTest.Db;
             db.CodeFirst.InitTables<UnitAddRemark2>();//添加表
             db.DbMaintenance.AddColumnRemark("id", "UnitAddRemark2", "AA");
+            foreach (var item in db.DbMaintenance.GetColumnInfosByTableName("UnitAddRemark2",false))
+            {
+                Console.WriteLine($" {item.DbColumnName} {item.ColumnDescription} ");
+            }
             db.DbMaintenance.AddColumnRemark("id", "UnitAddRemark2", "");
+            foreach (var item in db.DbMaintenance.GetColumnInfosByTableName("UnitAddRemark2", false))
+            {
+                Console.WriteLine($" {item.DbColumnName} {item.ColumnDescription} ");
+            }
             db.DbMaintenance.AddColumnRemark("id", "UnitAddRemark2", "AA");
+            foreach (var item in db.DbMaintenance.GetColumnInfosByTableName("UnitAddRemark2", false))
+            {
+                Console.WriteLine($" {item.DbColumnName} {item.ColumnDescription} ");
+            }
             db.DbMaintenance.DropTable<UnitAddRemark2>();//删除表
         }
         public class UnitAddRemark2 
