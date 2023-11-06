99c4842e0fabaf70d0d9e14afbf7ec83abf1be77
sunkaixuan <610262374@qq.com>
2023-11-06 21:15:21 +0800

    Update Sqlite demo

--- Src/Asp.Net/SqliteTest/Program.cs
+++ Src/Asp.Net/SqliteTest/Program.cs
@@ -1,41 +1,64 @@
-﻿using System;
+﻿using SqlSugar;
+using System;
 
 namespace OrmTest
 {
-    class Program
+    
+    public class Program
     {
         static void Main(string[] args)
-        {
-            //Demo
-            Demo0_SqlSugarClient.Init();
-            Demo1_Queryable.Init();
-            DemoN_SplitTable.Init();
-            Demo2_Updateable.Init();
-            Demo3_Insertable.Init();
-            Demo4_Deleteable.Init();
-            Demo5_SqlQueryable.Init();
-            Demo6_Queue.Init();
-            Demo7_Ado.Init();
-            Demo8_Saveable.Init();
-            Demo9_EntityMain.Init();
-            DemoA_DbMain.Init();
-            DemoB_Aop.Init();
-            DemoC_GobalFilter.Init();
-            DemoD_DbFirst.Init(); ;
-            DemoE_CodeFirst.Init();
-            DemoF_Utilities.Init();
-            DemoG_SimpleClient.Init();
-            DemoJ_Report.Init();
-            //Unit test
-            //NewUnitTest.Init();
-
-            //Rest Data
-            NewUnitTest.RestData();
-
-            Console.WriteLine("all successfully.");
-            Console.ReadKey();
+        { 
+            _1_CodeFirst.Init();
+            _2_DbFirst.Init();
+            _3_EasyQuery.Init();
+            _4_JoinQuery.Init();
+            _5_PageQuery.Init();
+            _6_NavQuery.Init();
+            _7_GroupQuery.Init();
+            _8_Insert.Init();
+            _9_Update.Init();
+            _a1_Delete.Init();
+            _a2_Sql.Init();
+            _a3_Merge.Init();
         }
+    }
 
- 
+    /// <summary>
+    /// Helper class for database operations
+    /// 数据库操作的辅助类
+    /// </summary>
+    public class DbHelper
+    {
+        /// <summary>
+        /// Database connection string
+        /// 数据库连接字符串
+        /// </summary>
+        public readonly static string Connection = "datasource=sqlite01.db";
+
+        /// <summary>
+        /// Get a new SqlSugarClient instance with specific configurations
+        /// 获取具有特定配置的新 SqlSugarClient 实例
+        /// </summary>
+        /// <returns>SqlSugarClient instance</returns>
+        public static SqlSugarClient GetNewDb()
+        {
+            var db = new SqlSugarClient(new ConnectionConfig()
+            {
+                IsAutoCloseConnection = true,
+                DbType = DbType.Sqlite,
+                ConnectionString = Connection,
+                LanguageType=LanguageType.Default//Set language
+
+            },
+            it => {
+                // Logging SQL statements and parameters before execution
+                // 在执行前记录 SQL 语句和参数
+                it.Aop.OnLogExecuting = (sql, para) =>
+                {
+                    Console.WriteLine(UtilMethods.GetNativeSql(sql, para));
+                };
+            });
+            return db;
+        }
     }
 }
---
+++ Src/Asp.Net/SqliteTest/UserTestCases/CasesMain.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrmTest
+{
+    internal class CasesMain
+    {
+        public static void Init() 
+        {
+            //Demo
+            Demo0_SqlSugarClient.Init();
+            Demo1_Queryable.Init();
+            DemoN_SplitTable.Init();
+            Demo2_Updateable.Init();
+            Demo3_Insertable.Init();
+            Demo4_Deleteable.Init();
+            Demo5_SqlQueryable.Init();
+            Demo6_Queue.Init();
+            Demo7_Ado.Init();
+            Demo8_Saveable.Init();
+            Demo9_EntityMain.Init();
+            DemoA_DbMain.Init();
+            DemoB_Aop.Init();
+            DemoC_GobalFilter.Init();
+            DemoD_DbFirst.Init(); ;
+            DemoE_CodeFirst.Init();
+            DemoF_Utilities.Init();
+            DemoG_SimpleClient.Init();
+            DemoJ_Report.Init();
+            //Unit test
+            NewUnitTest.Init();
+        }
+    }
+}
