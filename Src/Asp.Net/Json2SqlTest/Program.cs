d8c8f89744dc80f49e0d1ebadb7dd08e740d4010
sunkaixuan <610262374@qq.com>
2023-11-14 12:17:57 +0800

    update demo

--- Src/Asp.Net/Json2SqlTest/Program.cs
+++ Src/Asp.Net/Json2SqlTest/Program.cs
@@ -9,18 +9,18 @@ namespace Test
         static void Main(string[] args)
         {
             JsonClient jsonToSqlClient = new JsonClient();
-            //jsonToSqlClient.Context = new SqlSugarClient(new ConnectionConfig()
-            //{
-            //    DbType = DbType.SqlServer,
-            //    IsAutoCloseConnection = true,
-            //    ConnectionString = "server=.;uid=sa;pwd=sasa;database=SQLSUGAR4XTEST"
-            //});
             jsonToSqlClient.Context = new SqlSugarClient(new ConnectionConfig()
             {
-                DbType = DbType.MySql,
+                DbType = DbType.SqlServer,
                 IsAutoCloseConnection = true,
-                ConnectionString = "server=localhost;Database=SqlSugar4xTest;Uid=root;Pwd=haosql"
-            }); ;
+                ConnectionString = "server=.;uid=sa;pwd=sasa;database=SQLSUGAR4XTEST"
+            });
+            //jsonToSqlClient.Context = new SqlSugarClient(new ConnectionConfig()
+            //{
+            //    DbType = DbType.MySql,
+            //    IsAutoCloseConnection = true,
+            //    ConnectionString = "server=localhost;Database=SqlSugar4xTest;Uid=root;Pwd=haosql"
+            //}); ;
             TestHelper.InitDatabase(jsonToSqlClient);
 
             jsonToSqlClient.Context.Aop.OnLogExecuted = (sql, p) =>
