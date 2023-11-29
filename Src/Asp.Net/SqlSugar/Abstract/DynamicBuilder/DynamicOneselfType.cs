88ab4255f8d3ed34d52faa325320376ddc2869b8
sunkaixuan <610262374@qq.com>
2023-11-30 04:37:34 +0800

    Synchronization code

---
+++ Src/Asp.Net/SqlSugar/Abstract/DynamicBuilder/DynamicOneselfType.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SqlSugar 
+{
+    /// <summary>
+    /// Dynamically construct Type, the property is self, with this type
+    /// </summary>
+    public class DynamicOneselfType
+    {
+         
+    }
+    /// <summary>
+    /// Dynamically construct Type, the property is List self , with this type
+    /// </summary>
+    public class DynamicOneselfTypeList
+    {
+
+    }
+}
