339ce8f028cf04a5e13af9a824cd61a0f89deb94
sunkaixuan <610262374@qq.com>
2023-12-15 19:16:55 +0800

    Synchronization code

--- Src/Asp.NetCore2/SqlSugar/Realization/Oracle/DbBind/OracleDbBind.cs
+++ Src/Asp.NetCore2/SqlSugar/Realization/Oracle/DbBind/OracleDbBind.cs
@@ -88,6 +88,7 @@ namespace SqlSugar
                   new KeyValuePair<string, CSharpDataType>("integer",CSharpDataType.@int),
                   new KeyValuePair<string, CSharpDataType>("interval year to month",CSharpDataType.@int),
                   new KeyValuePair<string, CSharpDataType>("interval day to second",CSharpDataType.TimeSpan),
+                  new KeyValuePair<string, CSharpDataType>("intervalds",CSharpDataType.TimeSpan),
 
                   new KeyValuePair<string, CSharpDataType>("number",CSharpDataType.@int),
                   new KeyValuePair<string, CSharpDataType>("number",CSharpDataType.@float),
