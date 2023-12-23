84c146c348c811606ada0c819938cfb747bf9aed
sunkaixuan <610262374@qq.com>
2023-12-23 17:26:01 +0800

    Update  TDengine Demo

--- Src/Asp.NetCore2/TDengineTest/Demo/ORMTest_Partial/Helper.cs
+++ Src/Asp.NetCore2/TDengineTest/Demo/ORMTest_Partial/Helper.cs
@@ -6,8 +6,7 @@ using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
-using SqlSugar;
-using TDengineDriver;
+using SqlSugar; 
 
 namespace TDengineTest
 {
