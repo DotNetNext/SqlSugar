8537cbef739e9431805e5b6acfbccc01e0fd075a
sunkaixuan <610262374@qq.com>
2023-12-30 16:44:15 +0800

    Update sqlite  updateColumn

--- Src/Asp.NetCore2/SqlSugar/Realization/Sqlite/DbMaintenance/SqliteDbMaintenance.cs
+++ Src/Asp.NetCore2/SqlSugar/Realization/Sqlite/DbMaintenance/SqliteDbMaintenance.cs
@@ -258,7 +258,12 @@ namespace SqlSugar
         public override bool UpdateColumn(string tableName, DbColumnInfo column)
         {
             try
-            {
+            { 
+                if (column.IsPrimarykey) 
+                {
+                   Check.ExceptionEasy("Sqlite no support alter column primary key","Sqlite不支持修改主键");
+                }
+
                 // Start a transaction
                 this.Context.Ado.BeginTran();
 
