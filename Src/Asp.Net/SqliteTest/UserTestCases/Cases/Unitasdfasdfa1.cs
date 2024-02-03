f031d391f02e3b29e756a49e8d14f5c8440173de
sunkaixuan <610262374@qq.com>
2024-02-03 12:35:00 +0800

    Add user case test

---
+++ Src/Asp.Net/SqliteTest/UserTestCases/Cases/Unitasdfasdfa1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrmTest
+{
+    internal class Unitasdfasdfa1
+    {
+        public static void Init() 
+        {
+            var db = NewUnitTest.Db;
+            db.CurrentConnectionConfig.MoreSettings = new SqlSugar.ConnMoreSettings()
+            {
+                 IsCorrectErrorSqlParameterName=true
+            };
+            db.CodeFirst.InitTables<UnitTERTAFA>();
+            db.Insertable(new UnitTERTAFA()
+            {
+                Name = "a"
+            }).ExecuteCommand();
+            var dt=db.Queryable<UnitTERTAFA>().Take(2) .ToDataTable();
+            dt.Columns.RemoveAt(0);
+            db.Fastest<DataTable>().AS("UnitTERTAFA").BulkCopy(dt);
+        }
+    }
+    public class UnitTERTAFA 
+    {
+        [SqlSugar.SugarColumn(ColumnName = "id 1",IsPrimaryKey =true,IsIdentity =true)]
+        public int id { get; set; }
+        [SqlSugar.SugarColumn(ColumnName ="Name 1")]
+        public string Name { get; set; }
+        
+    }
+}
