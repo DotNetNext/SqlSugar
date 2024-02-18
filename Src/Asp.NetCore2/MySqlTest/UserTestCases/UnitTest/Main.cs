c2df602e810c9f12aa8c246359ed917a877c5399
sunkaixuan <610262374@qq.com>
2024-02-18 17:25:48 +0800

    Add user test case

--- Src/Asp.NetCore2/MySqlTest/UserTestCases/UnitTest/Main.cs
+++ Src/Asp.NetCore2/MySqlTest/UserTestCases/UnitTest/Main.cs
@@ -31,6 +31,7 @@ namespace OrmTest
         }
         public static void Init()
         {
+            UnitWeek.Init();
             UnitTest.Init();
             Unitdfasfasfa.Init();
             UnitAddRemark.Init();
