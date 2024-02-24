4c7e794c9eb281c2cc73487d3f061bd59ebe74fa
sunkaixuan <610262374@qq.com>
2024-02-24 17:22:00 +0800

    Add user test case

--- Src/Asp.Net/PgSqlTest/UserTestCases/UnitTest/Main.cs
+++ Src/Asp.Net/PgSqlTest/UserTestCases/UnitTest/Main.cs
@@ -31,6 +31,7 @@ namespace OrmTest
         }
         public static void Init()
         {
+            UnitAny.Init();
             Unitadfafafasd.Init();
             UintOneToOneDto.Init();
             Unitadfaafsd.Init();
