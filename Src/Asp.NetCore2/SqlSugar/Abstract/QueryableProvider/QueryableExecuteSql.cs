6e2153125cb91b1754866ea0e0bc49981826d27b
sunkaixuan <610262374@qq.com>
2024-02-27 18:38:10 +0800

    修改递归上限

--- Src/Asp.NetCore2/SqlSugar/Abstract/QueryableProvider/QueryableExecuteSql.cs
+++ Src/Asp.NetCore2/SqlSugar/Abstract/QueryableProvider/QueryableExecuteSql.cs
@@ -321,7 +321,7 @@ namespace SqlSugar
                 int i = 0;
                 while (parentId != null && this.Context.Queryable<T>().AS(tableName).Filter(null, this.QueryBuilder.IsDisabledGobalFilter).ClearFilter(this.QueryBuilder.RemoveFilters).In(parentId).Any())
                 {
-                    Check.Exception(i > 100, ErrorMessage.GetThrowMessage("Dead cycle", "出现死循环或超出循环上限（100），检查最顶层的ParentId是否是null或者0"));
+                    Check.Exception(i > 1000, ErrorMessage.GetThrowMessage("Dead cycle", "出现死循环或超出循环上限（1000），检查最顶层的ParentId是否是null或者0"));
                     var parent = this.Context.Queryable<T>().AS(tableName).Filter(null, this.QueryBuilder.IsDisabledGobalFilter).InSingle(parentId);
                     result.Add(parent);
                     parentId = ParentInfo.PropertyInfo.GetValue(parent, null);
@@ -363,7 +363,7 @@ namespace SqlSugar
                 int i = 0;
                 while (parentId != null && this.Context.Queryable<T>().AS(tableName).WhereIF(parentWhereExpression!=default, parentWhereExpression).Filter(null, this.QueryBuilder.IsDisabledGobalFilter).In(parentId).Any())
                 {
-                    Check.Exception(i > 100, ErrorMessage.GetThrowMessage("Dead cycle", "出现死循环或超出循环上限（100），检查最顶层的ParentId是否是null或者0"));
+                    Check.Exception(i > 1000, ErrorMessage.GetThrowMessage("Dead cycle", "出现死循环或超出循环上限（1000），检查最顶层的ParentId是否是null或者0"));
                     var parent = this.Context.Queryable<T>().AS(tableName).WhereIF(parentWhereExpression!=default, parentWhereExpression).Filter(null, this.QueryBuilder.IsDisabledGobalFilter).InSingle(parentId);
                     result.Add(parent);
                     parentId = ParentInfo.PropertyInfo.GetValue(parent, null);
