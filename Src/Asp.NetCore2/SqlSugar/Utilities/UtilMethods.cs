1ee383a841f809e66a2af29073ff6c415f3c90e7
sunkaixuan <610262374@qq.com>
2024-03-05 23:11:27 +0800

    Update split table

--- Src/Asp.NetCore2/SqlSugar/Utilities/UtilMethods.cs
+++ Src/Asp.NetCore2/SqlSugar/Utilities/UtilMethods.cs
@@ -335,6 +335,12 @@ namespace SqlSugar
                 context.CurrentConnectionConfig.ConfigureExternalServices.SplitTableService
                     = (ISplitTableService)Activator.CreateInstance(splitTableAttribute.CustomSplitTableService);
             }
+            if (splitTableAttribute.CustomSplitTableService != null
+                && splitTableAttribute.CustomSplitTableService == null
+                && !splitTableAttribute.SplitType.IsIn(SplitType._Custom01, SplitType._Custom02, SplitType._Custom03, SplitType._Custom04, SplitType._Custom05, SplitType._Custom06)) 
+            {
+                splitTableAttribute.CustomSplitTableService = null;
+            }
         }
         public static void ConvertParameter(SugarParameter p, ISqlBuilder builder)
         {
