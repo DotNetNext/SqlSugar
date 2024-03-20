134d637fd08705a89ae2c6dae92b81aac802825a
sunkaixuan <610262374@qq.com>
2024-03-21 05:59:12 +0800

    Update demo

--- Src/Asp.NetCore2/QuestDbTest/Demo/DemoL_BulkCopy.cs
+++ Src/Asp.NetCore2/QuestDbTest/Demo/DemoL_BulkCopy.cs
@@ -20,8 +20,19 @@ namespace OrmTest
             });
 
             var list=db.Queryable<Order>().Take(2).ToList();
+            Console.WriteLine(db.Queryable<Order>().Count());
             var i=db.RestApi().BulkCopy(list);
-            var i2 = db.RestApi().ExecuteCommand("select 1");
+            Console.WriteLine(db.Queryable<Order>().Count());
+            Console.WriteLine("--2---");
+            var i2 = db.RestApi().BulkCopy(list.First());
+            Console.WriteLine(db.Queryable<Order>().Count());
+            Console.WriteLine("--1---");
+            var i3 = db.RestApi().BulkCopyAsync(list).GetAwaiter().GetResult();
+            Console.WriteLine(db.Queryable<Order>().Count());
+            Console.WriteLine("--2---");
+            var i4 = db.RestApi().BulkCopyAsync(list.First()).GetAwaiter().GetResult();
+            Console.WriteLine(db.Queryable<Order>().Count());
+            Console.WriteLine("--1---");
         }
     }
 }
