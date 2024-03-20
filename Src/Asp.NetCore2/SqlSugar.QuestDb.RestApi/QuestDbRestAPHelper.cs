e7be0ce07e78c05e3ce3384eb5b2db88228ed383
sunkaixuan <610262374@qq.com>
2024-03-21 02:21:52 +0800

    Add QuestDb rest api

---
+++ Src/Asp.NetCore2/SqlSugar.QuestDb.RestApi/QuestDbRestAPHelper.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Globalization;
+using System.IO;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using Newtonsoft.Json;
+using CsvHelper;
+
+namespace SqlSugar 
+{
+    internal class QuestDbRestAPHelper
+    { 
+
+        /// <summary>
+        /// 逐行读取，包含空行
+        /// </summary>
+        /// <param name="text"></param>
+        /// <returns></returns>
+        public static List<string> SplitByLine(string text)
+        {
+            List<string> lines = new List<string>();
+            byte[] array = Encoding.UTF8.GetBytes(text);
+            using (MemoryStream stream = new MemoryStream(array))
+            {
+                using (var sr = new StreamReader(stream))
+                {
+                    string line = sr.ReadLine();
+                    while (line != null)
+                    {
+                        lines.Add(line);
+                        line = sr.ReadLine();
+                    }
+                }
+            }
+
+            return lines;
+        }
+
+
+    }
+}
---
+++ Src/Asp.NetCore2/SqlSugar.QuestDb.RestApi/QuestDbRestAPI.cs
@@ -0,0 +1,166 @@
+﻿using CsvHelper;
+using Newtonsoft.Json;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace SqlSugar 
+{
+
+    public class QuestDbRestAPI
+    {
+        internal  string url = string.Empty;
+        internal  string authorization = string.Empty;
+        ISqlSugarClient db;
+        public QuestDbRestAPI(ISqlSugarClient db)
+        {
+            this.db = db;
+            string host = "";
+            string username = "";
+            string password = "";
+
+            url = host;
+            if (url.EndsWith("/"))
+                url = url.Remove(url.Length - 1);
+
+            if (!url.ToLower().StartsWith("http"))
+                url = $"http://{url}";
+            //生成TOKEN
+            if (!string.IsNullOrWhiteSpace(username) && !string.IsNullOrWhiteSpace(password))
+            {
+                var base64 = Convert.ToBase64String(Encoding.UTF8.GetBytes($"{username}:{password}"));
+                authorization = $"Basic {base64}";
+            }
+        }
+
+
+        public  async Task<string> ExecuteCommandAsync(string sql)
+        {
+            //HTTP GET 执行SQL
+            var result = string.Empty;
+            var client = new HttpClient();
+            var url = $"{this.url}/exec?query={HttpUtility.UrlEncode(sql)}";
+            if (!string.IsNullOrWhiteSpace(authorization))
+                client.DefaultRequestHeaders.Add("Authorization", authorization);
+            var httpResponseMessage = await client.GetAsync(url);
+            result = await httpResponseMessage.Content.ReadAsStringAsync();
+            return result;
+        }
+
+        public  string ExecuteCommand(string sql)
+        {
+            return ExecuteCommandAsync(sql).GetAwaiter().GetResult();
+        }
+        /// <summary>
+        /// 批量快速插入
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="that"></param>
+        /// <param name="dateFormat">导入时，时间格式 默认:yyyy/M/d H:mm:ss</param>
+        /// <returns></returns>
+        public  async Task<int>  BulkCopyAsync<T>(List<T> insertList,  string dateFormat = "yyyy/M/d H:mm:ss") where T : class
+        {
+
+            if (string.IsNullOrWhiteSpace(url))
+            {
+                throw new Exception("BulkCopy功能需要启用RestAPI，程序启动时执行：RestAPIExtension.UseQuestDbRestAPI(\"localhost:9000\", \"username\", \"password\")");
+            }
+            var result = 0;
+            var fileName = $"{Guid.NewGuid()}.csv";
+            var filePath = Path.Combine(AppContext.BaseDirectory, fileName);
+            try
+            {
+                var client = new HttpClient();
+                var boundary = "---------------" + DateTime.Now.Ticks.ToString("x");
+                var list = new List<Hashtable>();
+                var name = db.EntityMaintenance.GetEntityInfo<T>().DbTableName;  //获取表名
+                db.DbMaintenance.GetColumnInfosByTableName(name).ForEach(d =>
+                {
+                    if (d.DataType == "TIMESTAMP")
+                    {
+                        list.Add(new Hashtable()
+                    {
+                        { "name", d.DbColumnName },
+                        { "type", d.DataType },
+                        { "pattern", dateFormat}
+                    });
+                    }
+                    else
+                    {
+                        list.Add(new Hashtable()
+                    {
+                        { "name", d.DbColumnName },
+                        { "type", d.DataType }
+                    });
+                    }
+                });
+                var schema = JsonConvert.SerializeObject(list);
+                //写入CSV文件
+                using (var writer = new StreamWriter(filePath))
+                using (var csv = new CsvWriter(writer, CultureInfo.CurrentCulture))
+                {
+                    await csv.WriteRecordsAsync(insertList);
+                }
+
+                var httpContent = new MultipartFormDataContent(boundary);
+                if (!string.IsNullOrWhiteSpace(this.authorization))
+                    client.DefaultRequestHeaders.Add("Authorization", this.authorization);
+                httpContent.Add(new StringContent(schema), "schema");
+                httpContent.Add(new ByteArrayContent(File.ReadAllBytes(filePath)), "data");
+                //boundary带双引号 可能导致服务器错误情况
+                httpContent.Headers.Remove("Content-Type");
+                httpContent.Headers.TryAddWithoutValidation("Content-Type",
+                    "multipart/form-data; boundary=" + boundary);
+                var httpResponseMessage =
+                    await client.PostAsync($"{this.url}/imp?name={name}", httpContent);
+                var readAsStringAsync = await httpResponseMessage.Content.ReadAsStringAsync();
+                var splitByLine = QuestDbRestAPHelper.SplitByLine(readAsStringAsync);
+                foreach (var s in splitByLine)
+                {
+                    if (s.Contains("Rows"))
+                    {
+                        var strings = s.Split('|');
+                        if (strings[1].Trim() == "Rows imported")
+                        {
+                            result = Convert.ToInt32(strings[2].Trim());
+                        }
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                try
+                {
+                    File.Delete(filePath);
+                }
+                catch
+                {
+                    // ignored
+                }
+            }
+            return result;
+        }
+
+        /// <summary>
+        /// 批量快速插入
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="that"></param>
+        /// <param name="dateFormat">导入时，时间格式 默认:yyyy/M/d H:mm:ss</param>
+        /// <returns></returns>
+        public  int BulkCopy<T>(List<T> insertList, string dateFormat = "yyyy/M/d H:mm:ss") where T : class
+        {
+             return BulkCopyAsync(insertList, dateFormat).GetAwaiter().GetResult();
+        }
+    }
+}
