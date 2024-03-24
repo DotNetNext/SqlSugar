a276c2e3cd150a8482c75dee4ffc7ba427d6f240
sunkaixuan <610262374@qq.com>
2024-03-24 20:14:05 +0800

    Update Value object

--- Src/Asp.NetCore2/SqlSugar/Abstract/InsertableProvider/InsertableHelper.cs
+++ Src/Asp.NetCore2/SqlSugar/Abstract/InsertableProvider/InsertableHelper.cs
@@ -204,7 +204,15 @@ namespace SqlSugar
             {
                 foreach (var columnInfo in this.EntityInfo.Columns)
                 {
-                    dataEvent(columnInfo.PropertyInfo.GetValue(item, null), new DataFilterModel() { OperationType = DataFilterType.InsertByObject, EntityValue = item, EntityColumnInfo = columnInfo });
+                    if (columnInfo.ForOwnsOnePropertyInfo != null)
+                    {
+                        var data = columnInfo.ForOwnsOnePropertyInfo.GetValue(item, null);
+                        dataEvent(columnInfo.PropertyInfo.GetValue(data, null), new DataFilterModel() { OperationType = DataFilterType.InsertByObject, EntityValue = item, EntityColumnInfo = columnInfo });
+                    }
+                    else
+                    {
+                        dataEvent(columnInfo.PropertyInfo.GetValue(item, null), new DataFilterModel() { OperationType = DataFilterType.InsertByObject, EntityValue = item, EntityColumnInfo = columnInfo });
+                    }
                 }
             }
         }
