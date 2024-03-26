ccd6755b79c3cd98fb6bc5156bb19e12c13c8dc3
sunkaixuan <610262374@qq.com>
2024-03-26 09:02:13 +0800

    Update demo

--- Src/Asp.Net/PgSqlTest/UserTestCases/UnitTest/UnitDataTable.cs
+++ Src/Asp.Net/PgSqlTest/UserTestCases/UnitTest/UnitDataTable.cs
@@ -14,6 +14,7 @@ namespace OrmTest
             db.CodeFirst.InitTables<Unitadfadsfa>();
             var list=new List<Unitadfadsfa>() { new Unitadfadsfa() { Id=Guid.NewGuid(), Name="a" },
             new Unitadfadsfa() { Id=Guid.NewGuid(),Name="a" }};
+            var dt = db.Utilities.ListToDataTable(list);
             db.Storageable(list).WhereColumns(it => it.Id).ExecuteCommand();
         }
     }
