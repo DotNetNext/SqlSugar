f2ace4336c783ccc9d0575f557bea60bef5e4549
sunkaixuan <610262374@qq.com>
2024-03-26 20:30:57 +0800

    Update demo

--- Src/Asp.NetCore2/DorisTest/Program.cs
+++ Src/Asp.NetCore2/DorisTest/Program.cs
@@ -10,11 +10,24 @@ namespace OrmTest
         {
 
             var db = DbHelper.GetNewDb();
-            db.Open();
-            db.Close();
-        }
-    }
+            db.BeginTran();
 
+             var XX=  db.Ado.SqlQuery<DbTableInfo>("Select TABLE_NAME as Name,TABLE_COMMENT as Description from information_schema.tables\r\n                         where  TABLE_SCHEMA=(select database())  AND TABLE_TYPE='BASE TABLE'");
+             db.CommitTran();
+             db.BeginTran();
+
+            var XX2 = db.Ado.SqlQuery<DbTableInfo>("Select TABLE_NAME as Name,TABLE_COMMENT as Description from information_schema.tables\r\n                         where  TABLE_SCHEMA=(select database())  AND TABLE_TYPE='BASE TABLE'");
+            db.CommitTran();
+            db.CodeFirst.InitTables<Student11>();
+        }
+    } 
+    public class Student11
+    {
+        [SqlSugar.SugarColumn(IsPrimaryKey = true )]
+        public int Id { get; set; } // 学生ID (Student ID)
+        public string Name { get; set; } // 学生姓名 (Student Name)
+        public int Age { get; set; } // 学生年龄 (Student Age)
+    }
 
     /// <summary>
     /// Helper class for database operations
@@ -26,7 +39,7 @@ namespace OrmTest
         /// Database connection string
         /// 数据库连接字符串
         /// </summary>
-        public readonly static string Connection = "data source=139.170.74.9;port=9030;database=test;user id=root;password=1q!1q!;pooling=true;charset=utf8;";
+        public readonly static string Connection = "data source=139.170.74.9;port=9030;database=test;user id=root;password=1q!1q!;pooling=true;charset=utf8;Pooling=false";
 
         /// <summary>
         /// Get a new SqlSugarClient instance with specific configurations
