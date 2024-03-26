c4cfdf0a108caeb73fc0c0066560f7b57272df2a
sunkaixuan <610262374@qq.com>
2024-03-26 09:00:34 +0800

    Add user test case

--- Src/Asp.Net/PgSqlTest/UserTestCases/UnitTest/Main.cs
+++ Src/Asp.Net/PgSqlTest/UserTestCases/UnitTest/Main.cs
@@ -31,6 +31,7 @@ namespace OrmTest
         }
         public static void Init()
         {
+            UnitDataTable.Init();
             UnitAny.Init();
             Unitadfafafasd.Init();
             UintOneToOneDto.Init();
---
+++ Src/Asp.Net/PgSqlTest/UserTestCases/UnitTest/UnitDataTable.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrmTest
+{
+    internal class UnitDataTable
+    {
+        public static void Init() 
+        {
+            var db = NewUnitTest.Db;
+            db.CodeFirst.InitTables<Unitadfadsfa>();
+            var list=new List<Unitadfadsfa>() { new Unitadfadsfa() { Id=Guid.NewGuid(), Name="a" },
+            new Unitadfadsfa() { Id=Guid.NewGuid(),Name="a" }};
+            db.Storageable(list).WhereColumns(it => it.Id).ExecuteCommand();
+        }
+    }
+    public class Unitadfadsfa 
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+    }
+}
