dc96069dffc25a6c8d49d4ce9571c8b897b60b0d
sunkaixuan <610262374@qq.com>
2024-03-31 20:03:49 +0800

    Add questdb

--- Src/Asp.NetCore2/QuestDbTest/Demo/DemoL_BulkCopy.cs
+++ Src/Asp.NetCore2/QuestDbTest/Demo/DemoL_BulkCopy.cs
@@ -9,7 +9,7 @@ namespace OrmTest
 {
     internal class DemoL_BulkCopy
     {
-        public static void Init() 
+        public static void Init()
         {
             SqlSugarClient db = new SqlSugarClient(new ConnectionConfig()
             {
@@ -19,9 +19,9 @@ namespace OrmTest
                 IsAutoCloseConnection = true
             });
 
-            var list=db.Queryable<Order>().Take(2).ToList();
+            var list = db.Queryable<Order>().Take(2).ToList();
             Console.WriteLine(db.Queryable<Order>().Count());
-            var i=db.RestApi().BulkCopy(list);
+            var i = db.RestApi().BulkCopy(list);
             Console.WriteLine(db.Queryable<Order>().Count());
             Console.WriteLine("--2---");
             var i2 = db.RestApi().BulkCopy(list.First());
@@ -33,6 +33,44 @@ namespace OrmTest
             var i4 = db.RestApi().BulkCopyAsync(list.First()).GetAwaiter().GetResult();
             Console.WriteLine(db.Queryable<Order>().Count());
             Console.WriteLine("--1---");
+            Demo1(db);
+            Demo2(db);
+        }
+
+        private static void Demo1(SqlSugarClient db)
+        {
+            db.CodeFirst.InitTables<BulkCopyTest>();
+            List<BulkCopyTest> bulkCopyTests = new List<BulkCopyTest>();
+            bulkCopyTests.Add(new BulkCopyTest() { Id = SnowFlakeSingle.Instance.NextId(), DateTime = DateTime.Now });
+            bulkCopyTests.Add(new BulkCopyTest() { Id = SnowFlakeSingle.Instance.NextId(), DateTime = DateTime.Now });
+            var rows = db.RestApi().BulkCopy(bulkCopyTests);
+            var list2 = db.Queryable<BulkCopyTest>().Take(2).ToList();
+        }
+        private static void Demo2(SqlSugarClient db)
+        {
+            db.CodeFirst.InitTables<BulkCopyTest2>();
+            List<BulkCopyTest2> bulkCopyTests = new List<BulkCopyTest2>();
+            bulkCopyTests.Add(new BulkCopyTest2() { Id = SnowFlakeSingle.Instance.NextId(), DateTime = DateTime.Now });
+            bulkCopyTests.Add(new BulkCopyTest2() { Id = SnowFlakeSingle.Instance.NextId(), DateTime = DateTime.Now });
+            var rows = db.RestApi().BulkCopy(bulkCopyTests);
+            var list2 = db.Queryable<BulkCopyTest2>().Take(2).ToList();
+        }
+        public class BulkCopyTest 
+        {
+            [SugarColumn(IsPrimaryKey =true)]
+            public long Id { get; set; }
+            public DateTime DateTime { get; set; }
+            [SugarColumn(IsIgnore =true)]
+            public string aaa { get; }
+        }
+        public class BulkCopyTest2
+        {
+            [SugarColumn(IsPrimaryKey = true)]
+            public long Id { get; set; }
+            [SugarColumn(ColumnName ="dt")]
+            public DateTime DateTime { get; set; }
+            [SugarColumn(IsIgnore = true)]
+            public string aaa { get; }
         }
     }
 }
