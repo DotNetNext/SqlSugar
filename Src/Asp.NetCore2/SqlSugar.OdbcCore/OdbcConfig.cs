ca6b714e703884f29ba7cabaf0dada47f0c7884e
sunkaixuan <610262374@qq.com>
2024-04-01 18:06:09 +0800

    Update Odbc

---
+++ Src/Asp.NetCore2/SqlSugar.OdbcCore/OdbcConfig.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SqlSugar.Odbc
+{
+    public class OdbcConfig
+    {
+        public static string SqlTranslationLeft ="";
+        public static string SqlTranslationRight="";
+    }
+}
