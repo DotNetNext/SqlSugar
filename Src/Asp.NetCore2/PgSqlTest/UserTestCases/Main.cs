891f0af03163e390c87e9a26fca6abe1134993c1
sunkaixuan <610262374@qq.com>
2024-04-12 19:20:57 +0800

    Add user test case

--- Src/Asp.NetCore2/PgSqlTest/UserTestCases/Main.cs
+++ Src/Asp.NetCore2/PgSqlTest/UserTestCases/Main.cs
@@ -31,6 +31,7 @@ namespace OrmTest
         }
         public static void Init()
         {
+            Unitadfaf2s.Init();
             UnitWeek.Init();
             UnitTestOneToOne.Init();
             Unitadfafafasd.Init();
