de44e7224b526450e2592813d93aebf0c1f3bd0b
sunkaixuan <610262374@qq.com>
2024-04-29 11:43:41 +0800

    Add  user test case

--- Src/Asp.NetCore2/MySqlTest/UserTestCases/UnitTest/UJson.cs
+++ Src/Asp.NetCore2/MySqlTest/UserTestCases/UnitTest/UJson.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using SqlSugar;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -25,9 +27,29 @@ namespace OrmTest
             UValidate.Check("order3", list.First().Order.Name, "Json");
 
             var list2 = Db.Queryable<UnitJsonTest>().ToList();
+
+            Db.CodeFirst.InitTables<UnitJsonTest123123>();
+            Db.Insertable(new UnitJsonTest123123() {
+                Order = JObject.Parse(Db.Utilities.SerializeObject(new { x = new { y = 100 } }))
+            }).ExecuteCommand();
+            var list3 = Db.Queryable<UnitJsonTest123123>().Select(it => new {
+                x =  SqlFunc.JsonField(it.Order, "x") 
+            }).ToList();
+            var list31 = Db.Queryable<UnitJsonTest123123>().Select(it => new {
+                x = SqlFunc.JsonField(SqlFunc.JsonField(it.Order, "x" ),"y")
+            }).ToList();
+            var list32 = Db.Queryable<UnitJsonTest123123>().Select(it=>new { 
+              x=SqlFunc.JsonField(it.Order, "x","y")    
+            }).ToList();
         }
     }
-
+    public class UnitJsonTest123123
+    {
+        [SqlSugar.SugarColumn(IsPrimaryKey = true, IsIdentity = true)]
+        public int Id { get; set; }
+        [SqlSugar.SugarColumn(ColumnDataType = "varchar(4000)", IsJson = true)]
+        public JObject Order { get; set; }
+    }
 
     public class UnitJsonTest
     {
