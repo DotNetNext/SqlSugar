81c8bdeebd2bf21ef018588a066ffb6e5e6ef143
sunkaixuan <610262374@qq.com>
2024-04-29 17:23:13 +0800

    Synchronization code

--- Src/Asp.Net/SqlSugar/Realization/PostgreSQL/DbMaintenance/PostgreSQLDbMaintenance.cs
+++ Src/Asp.Net/SqlSugar/Realization/PostgreSQL/DbMaintenance/PostgreSQLDbMaintenance.cs
@@ -309,6 +309,11 @@ WHERE tgrelid = '"+tableName+"'::regclass");
                 string sql = string.Format(AddDefaultValueSql, this.SqlBuilder.GetTranslationColumnName(tableName), this.SqlBuilder.GetTranslationColumnName(columnName), defaultValue.Replace("''","'").TrimEnd('\'').TrimStart('\''));
                 return this.Context.Ado.ExecuteCommand(sql) > 0;
             }
+            else if (defaultValue?.ToLower()?.Contains("now()") == true)
+            {
+                string sql = string.Format(AddDefaultValueSql, this.SqlBuilder.GetTranslationColumnName(tableName), this.SqlBuilder.GetTranslationColumnName(columnName), defaultValue.TrimEnd('\'').TrimStart('\''));
+                return this.Context.Ado.ExecuteCommand(sql) > 0;
+            }
             else
             {
                 return base.AddDefaultValue(this.SqlBuilder.GetTranslationTableName(tableName), this.SqlBuilder.GetTranslationTableName(columnName), defaultValue);
