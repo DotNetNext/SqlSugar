27fbc27af5b30dc9399c03553d6d8f6cb753072c
sunkaixuan <610262374@qq.com>
2024-06-02 12:10:11 +0800

    Synchronization code

--- Src/Asp.Net/SqlSugar/Abstract/SaveableProvider/StorageableMethodInfo.cs
+++ Src/Asp.Net/SqlSugar/Abstract/SaveableProvider/StorageableMethodInfo.cs
@@ -109,6 +109,24 @@ namespace SqlSugar
             var newObj= value.GetType().GetMethod("ExecuteCommand").Invoke(value, new object[] { });
             return (int)newObj;
         }
+        public StorageableCommonMethodInfo IgnoreColumns(params string[] ignoreColumns)
+        {
+            PropertyInfo property = ObjectValue.GetType().GetProperty(type);
+            var value = property.GetValue(ObjectValue);
+            var newObj = value.GetType().GetMyMethod("IgnoreColumns", 1, typeof(string[])).Invoke(value, new object[] { ignoreColumns });
+            StorageableCommonMethodInfo result = new StorageableCommonMethodInfo();
+            result.Value = newObj;
+            return result;
+        } 
+    }
+    public class StorageableCommonMethodInfo
+    {
+        public object Value { get;  set; }
+        public int ExecuteCommand()
+        { 
+            var newObj = Value.GetType().GetMethod("ExecuteCommand").Invoke(Value, new object[] { });
+            return (int)newObj;
+        }
     }
 
     public class StorageableSplitTableMethodInfo
