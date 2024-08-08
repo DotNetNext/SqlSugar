eba700c322455adaf09f75afe3158431749cd52a
shaohua <shaohuatsou@foxmail.com>
2024-08-08 20:06:15 +0800

    更新 ORACLE 验证约束和索引的问题

--- Src/Asp.NetCore2/SqlSugar/Realization/Oracle/DbMaintenance/OracleDbMaintenance.cs
+++ Src/Asp.NetCore2/SqlSugar/Realization/Oracle/DbMaintenance/OracleDbMaintenance.cs
@@ -58,7 +58,7 @@ namespace SqlSugar
         {
             get
             {
-                return "select count(1) from user_ind_columns where upper(index_name)=upper('{0}')";
+                return "SELECT NVL2((SELECT INDEX_NAME FROM ALL_INDEXES WHERE INDEX_NAME=UPPER('{0}') AND OWNER = USER ),1,0)+NVL2((SELECT CONSTRAINT_NAME FROM ALL_CONSTRAINTS WHERE CONSTRAINT_NAME=UPPER('{0}') AND OWNER = USER),2,0) AS ROWCOUNT FROM DUAL";
             }
         }
         protected override string CreateIndexSql
@@ -645,6 +645,21 @@ WHERE table_name = '"+tableName+"'");
                 }
             }
             return true;
+        }
+        public override bool IsAnyIndex(string indexName)
+        {
+            string sql = string.Format(this.IsAnyIndexSql, indexName);
+            return this.Context.Ado.GetInt(sql) == 1;
+        }
+        public override bool IsAnyConstraint(string constraintName)
+        {
+            string sql = string.Format(this.IsAnyIndexSql, constraintName);
+            int res = this.Context.Ado.GetInt(sql);
+            return res == 2 || res == 3;
+        }
+        public override bool DropIndex(string indexName, string tableName)
+        {
+            return DropIndex(indexName);
         }
         #endregion
 
