2e2c55fdd8b4a829003518b9b13b6c828f228850
sunkaixuan <610262374@qq.com>
2024-10-30 12:01:24 +0800

    Update ClickHouse

--- Src/Asp.NetCore2/SqlSugar.ClickHouseCore/ClickHouse/SqlBuilder/ClickHouseFastBuilder.cs
+++ Src/Asp.NetCore2/SqlSugar.ClickHouseCore/ClickHouse/SqlBuilder/ClickHouseFastBuilder.cs
@@ -21,7 +21,9 @@ namespace SqlSugar.ClickHouse
             bulkCopy = new ClickHouseBulkCopy(conn);
             await conn.OpenAsync();
             bulkCopy.BatchSize = 100000;
-            bulkCopy.DestinationTableName = dt.TableName;
+            var pro=bulkCopy.GetType().GetProperty("DestinationTableName");
+            pro.SetValue(bulkCopy,dt.TableName);
+            await bulkCopy.InitAsync();
             try
             {
                 await bulkCopy.WriteToServerAsync(dt, new CancellationToken());
