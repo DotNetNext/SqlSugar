f16f7ca3c65af3dccad856fc8287f89f891c3fa8
sunkaixuan <610262374@qq.com>
2025-02-13 12:14:03 +0800

    SqlSugar.OceanBaseForOracleCore  DataSetAll

--- Src/Asp.NetCore2/SqlSugar.OceanBaseForOracle/DataTableExtensions/OceanBaseForOracleDataAdapter.cs
+++ Src/Asp.NetCore2/SqlSugar.OceanBaseForOracle/DataTableExtensions/OceanBaseForOracleDataAdapter.cs
@@ -104,53 +104,10 @@ namespace SqlSugar.OceanBaseForOracle
         /// <param name="ds"></param>
         public void Fill(DataSet ds)
         {
-            if (ds == null)
+            using (var cmd = new OdbcDataAdapter(this.SelectCommand))
             {
-                ds = new DataSet();
-            }
-            using (var dr = command.ExecuteReader())
-            {
-                do
-                {
-                    var dt = new DataTable();
-                    var columns = dt.Columns;
-                    var rows = dt.Rows;
-                    for (int i = 0; i < dr.FieldCount; i++)
-                    {
-                        string name = dr.GetName(i).Trim();
-                        if (dr.GetFieldType(i).Name == "DateTime")
-                        {
-                            if (!columns.Contains(name))
-                                columns.Add(new DataColumn(name, UtilConstants.DateType));
-                            else
-                            {
-                                columns.Add(new DataColumn(name + i, UtilConstants.DateType));
-                            }
-                        }
-                        else
-                        {
-                            if (!columns.Contains(name))
-                                columns.Add(new DataColumn(name, dr.GetFieldType(i)));
-                            else
-                            {
-                                columns.Add(new DataColumn(name + i, dr.GetFieldType(i)));
-                            }
-                        }
-                    }
-
-                    while (dr.Read())
-                    {
-                        DataRow daRow = dt.NewRow();
-                        for (int i = 0; i < columns.Count; i++)
-                        {
-                            daRow[columns[i].ColumnName] = dr.GetValue(i);
-                        }
-                        dt.Rows.Add(daRow);
-                    }
-                    dt.AcceptChanges();
-                    ds.Tables.Add(dt);
-                } while (dr.NextResult());
-            }
+                cmd.Fill(ds);
+            } 
         }
     }
 
