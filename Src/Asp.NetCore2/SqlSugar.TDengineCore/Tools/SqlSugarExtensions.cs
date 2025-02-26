52989146769c10690d200b92e8dec2af8904a009
sunkaixuan <610262374@qq.com>
2025-02-26 20:21:10 +0800

    Update TDengine

--- Src/Asp.NetCore2/SqlSugar.TDengineCore/Tools/SqlSugarExtensions.cs
+++ Src/Asp.NetCore2/SqlSugar.TDengineCore/Tools/SqlSugarExtensions.cs
@@ -1,13 +1,27 @@
 ﻿using SqlSugar.TDengine;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Reflection;
+using System.Runtime.CompilerServices;
 using System.Text;
 
 namespace SqlSugar 
 {
      public static class SqlSugarExtensions
-    {
+     {
+        public static ISugarQueryable<T> AsTDengineSTable<T>(this ISugarQueryable<T> queryable)   where T:class,new()
+        {
+            var attr=SqlSugar.TDengine.UtilMethods.GetCommonSTableAttribute(queryable.Context,typeof(T).GetCustomAttribute<STableAttribute>());
+            queryable.AS(attr.STableName);
+            return queryable;
+        }
+        public static IDeleteable<T> AsTDengineSTable<T>(this IDeleteable<T> queryable) where T : class, new()
+        {
+            var attr = SqlSugar.TDengine.UtilMethods.GetCommonSTableAttribute(((DeleteableProvider<T>)queryable).Context, typeof(T).GetCustomAttribute<STableAttribute>());
+            queryable.AS(attr.STableName);
+            return queryable;
+        }
         public static void MappingSTableName<T>(this ISqlSugarClient db,string newSTableName)
         {
             STableAttribute sTableAttribute = typeof(T).GetCustomAttribute<STableAttribute>();
