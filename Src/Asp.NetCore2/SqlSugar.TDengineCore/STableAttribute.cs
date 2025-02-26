5962ce982c3d4427bc3b0bc3791bb9428cb2af11
sunkaixuan <610262374@qq.com>
2025-02-26 15:09:56 +0800

    Update Tdengine

--- Src/Asp.NetCore2/SqlSugar.TDengineCore/STableAttribute.cs
+++ Src/Asp.NetCore2/SqlSugar.TDengineCore/STableAttribute.cs
@@ -7,6 +7,10 @@ namespace SqlSugar.TDengine
     public class STableAttribute:Attribute
     {
         public string Tags { get; set; }
+        public string Tag1 { get; set; }
+        public string Tag2 { get; set; }
+        public string Tag3 { get; set; }
+        public string Tag4 { get; set; }
         public string STableName { get; set; }
     }
 }
