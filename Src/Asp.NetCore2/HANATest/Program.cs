d0aea1173a0843891e6eb1758207631676f8769f
sunkaixuan <610262374@qq.com>
2025-03-10 15:33:39 +0800

    Add NANA Test

---
+++ Src/Asp.NetCore2/HANATest/Program.cs
@@ -0,0 +1,16 @@
+﻿
+using SqlSugar;
+
+var db = new SqlSugarClient(new ConnectionConfig()
+{
+    ConnectionString = "DRIVER={HANAQAS64};SERVERNODE=172.16.10.12:32015;UID=WLH_BPM_TASK;PWD=BPM4pass1;DATABASENAME=Q00",
+    DbType = DbType.HANA,
+    IsAutoCloseConnection = true,
+});
+
+db.Open(); 
+db.Close();
+
+var dt = db.Ado.GetDataTable("SELECT 1 as id");
+
+Console.WriteLine("Hello, World!");
