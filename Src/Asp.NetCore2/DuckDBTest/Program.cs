4d4bfbd8ca8d43286cd87d81605aeb09369ec4b0
sunkaixuan <610262374@qq.com>
2025-04-20 18:03:16 +0800

    Update DuckDb

--- Src/Asp.NetCore2/DuckDBTest/Program.cs
+++ Src/Asp.NetCore2/DuckDBTest/Program.cs
@@ -1,5 +1,9 @@
 ﻿using SqlSugar;
 using OrmTest;
 _1_CodeFirst.Init();
+_2_DbFirst.Init();
+_3_EasyQuery.Init();
+_4_JoinQuery.Init();
+_5_PageQuery.Init();
 Console.WriteLine("DEMO执行完成");
 Console.Read();
