9436a7a6093d4e5aad0ee11f4d28ce83b087754d
sunkaixuan <610262374@qq.com>
2025-04-23 17:27:42 +0800

    Update Custom db demo

--- Src/Asp.NetCore2/CustomDbTest/Demo.cs
+++ Src/Asp.NetCore2/CustomDbTest/Demo.cs
@@ -66,7 +66,7 @@ namespace OrmTest
                 UserName="admin",
                 RegistrationDate=DateTime.Now,
 
-            }).ExecuteCommand();
+            }).ExecuteReturnIdentity();
 
             //Query
             //查询
