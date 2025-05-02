9cc2c3ee0dc6637423367e3d3f9c87b70f5ec007
sunkaixuan <610262374@qq.com>
2025-05-02 15:34:15 +0800

    Update mongodb

--- Src/Asp.NetCore2/MongoDb.Ado.data/ExecuteScalarItems/ExecuteScalarHandler.cs
+++ Src/Asp.NetCore2/MongoDb.Ado.data/ExecuteScalarItems/ExecuteScalarHandler.cs
@@ -9,19 +9,16 @@ namespace MongoDb.Ado.data
 {
     public class ExecuteScalarHandler
     {
-        public object Handle(IMongoCollection<BsonDocument> collection, string json)
+        public object Handle(string operation, IMongoCollection<BsonDocument> collection, string json)
         {
-            var filter = string.IsNullOrWhiteSpace(json) ? FilterDefinition<BsonDocument>.Empty : BsonDocument.Parse(json);
-            // 执行查询并限制返回一条记录
-            var document = collection.Find(filter).FirstOrDefault();
-
-            // 如果查询到结果且文档非空，则获取第一个字段的值
-            if (document != null && document.Elements.Any())
+            using (var dbReader = new DbDataReaderFactory().Handle(operation, collection, json))
             {
-                var firstElement = document.Elements.First();  // 获取第一个字段（列）
-                return firstElement.Value;  // 返回该字段的值
-            } 
-            return null; //  
-        } 
+                if (dbReader.Read())
+                {
+                   return dbReader.GetValue(0);
+                } 
+                return null; //  
+            }
+        }
     }
 }
