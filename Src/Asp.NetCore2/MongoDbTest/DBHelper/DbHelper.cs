1f4b5911828a80b595d50332018bae4c4ab44462
sunkaixuan <610262374@qq.com>
2025-05-04 16:46:54 +0800

    Update mongodb

--- Src/Asp.NetCore2/MongoDbTest/DBHelper/DbHelper.cs
+++ Src/Asp.NetCore2/MongoDbTest/DBHelper/DbHelper.cs
@@ -13,7 +13,7 @@ namespace MongoDbTest.DBHelper
     /// </summary>
     public class DbHelper
     {
-        public static string ConnectionString = "mongodb://mongouser:localhost:27018/SqlSugarDb?replicaSet=cmgo-7d07e4w1_0&authSource=admin";
+        public static string ConnectionString = "mongodb://mongouser:Huangxin%40123@localhost:27018/SqlSugarDb?replicaSet=cmgo-7d07e4w1_0&authSource=admin";
         public static string SqlSugarConnectionString = "host=localhost;Port=27018;Database=SqlSugarDb;Username=mongouser;Password=Huangxin@123;replicaSet=cmgo-7d07e4w1_0&authSource=admin";
 
         /// <summary>
