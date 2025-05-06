2a7b7b05e222abf853f46745d2799be8e6550fe2
sunkaixuan <610262374@qq.com>
2025-05-06 20:12:38 +0800

    Update mongodb

--- Src/Asp.NetCore2/MongoDbTest/OrmTest/Models/OrderItem.cs
+++ Src/Asp.NetCore2/MongoDbTest/OrmTest/Models/OrderItem.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SqlSugar.MongoDbCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,10 +7,8 @@ using System.Text;
 namespace MongoDbTest
 {
     [SqlSugar.SugarTable("OrderDetail")]
-    public class OrderItem
-    {
-        [SqlSugar.SugarColumn(IsPrimaryKey =true, IsIdentity =true, ColumnName = "_Id")]
-        public int Id { get; set; }
+    public class OrderItem : MongoDbBase
+    { 
         public int OrderId { get; set; }
         public decimal? Price { get; set; }
         [SqlSugar.SugarColumn(IsNullable = true)]
