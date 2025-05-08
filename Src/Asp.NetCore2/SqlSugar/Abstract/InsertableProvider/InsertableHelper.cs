e59c97a4b67c6a176e6cbd68ba07b260f89c8da9
sunkaixuan <610262374@qq.com>
2025-05-08 15:49:33 +0800

    Optimize the code

--- Src/Asp.NetCore2/SqlSugar/Abstract/InsertableProvider/InsertableHelper.cs
+++ Src/Asp.NetCore2/SqlSugar/Abstract/InsertableProvider/InsertableHelper.cs
@@ -42,7 +42,7 @@ namespace SqlSugar
                 foreach (var item in InsertBuilder.DbColumnInfoList)
                 {
                     var isPk = pks.Any(y => y.Equals(item.DbColumnName, StringComparison.CurrentCultureIgnoreCase)) || item.IsPrimarykey;
-                    if (isPk && item.Value is Guid guid && guid == Guid.Empty)
+                    if (isPk && item.PropertyType == UtilConstants.GuidType && item.Value is Guid guid && guid == Guid.Empty)
                     {
                         if (StaticConfig.CustomGuidFunc != null)
                         {
