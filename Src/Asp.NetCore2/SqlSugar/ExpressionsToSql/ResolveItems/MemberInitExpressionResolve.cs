5efe3fa0395f91eb7ce45eddd1eed0427b0c5ff6
sunkaixuan <610262374@qq.com>
2025-06-07 16:27:39 +0800

    Update exp to sql(DTO)

--- Src/Asp.NetCore2/SqlSugar/ExpressionsToSql/ResolveItems/MemberInitExpressionResolve.cs
+++ Src/Asp.NetCore2/SqlSugar/ExpressionsToSql/ResolveItems/MemberInitExpressionResolve.cs
@@ -356,6 +356,19 @@ namespace SqlSugar
                         item = (item as UnaryExpression).Operand;
                     }
                 }
+                if (item is ParameterExpression)
+                {
+                    var itemType = item.Type;
+                    var ignoreProperty = itemType.GetProperties().FirstOrDefault(it => it.PropertyType == itemType);
+                    if (ignoreProperty != null && ignoreProperty.Name != memberName)
+                    {
+                        if (this.Context.SugarContext.QueryBuilder.SelectNewIgnoreColumns == null)
+                        {
+                            this.Context.SugarContext.QueryBuilder.SelectNewIgnoreColumns = new List<KeyValuePair<string, string>>();
+                        }
+                        this.Context.SugarContext.QueryBuilder.SelectNewIgnoreColumns.Add(new KeyValuePair<string, string>(ignoreProperty.Name, itemType.Name));
+                    }
+                }
                 ResolveNewExpressions(parameter, item, memberName);
             }
         }
