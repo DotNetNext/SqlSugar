c1af1be02bdc54e1132fd5c57cdd554f470e49fe
sunkaixuan <610262374@qq.com>
2025-06-08 12:12:14 +0800

    Update demo

--- Src/Asp.NetCore2/Net7Test/NET7Test/NET7Test/Program.cs
+++ Src/Asp.NetCore2/Net7Test/NET7Test/NET7Test/Program.cs
@@ -72,14 +72,14 @@ static void MyTest()
         ["d2"] = 2
     };
     var statsValue = db.DynamicBuilder().CreateObjectByType(type, stats);
-    db.StorageableByObject(statsValue).ExecuteCommand();
+    db.StorageableByObject(statsValue).ExecuteCommand(); 
 }
 static void ServerTest()
 {
     var sqlugar = new SqlSugarClient(new ConnectionConfig()
     {
         DbType = DbType.SqlServer,
-        ConnectionString = "SERVER=.;uid=sa;pwd=sasa;database=SqlSugar4Text4"
+        ConnectionString = "SERVER=.;uid=sa;pwd=sasa;database=SqlSugar4Text4;Encrypt=True;TrustServerCertificate=True"
     },
     it =>
     {
@@ -181,6 +181,9 @@ static void ServerTest()
         left.CustNum,
         ViewWorker = right
     }).First();
+    sqlugar.CodeFirst.InitTables<Unitadfadfassyss>(); 
+    sqlugar.Storageable(new Unitadfadfassyss() { Code = "a", dateOnly = DateOnly.Parse("2020-1-1"), Name = "a" })
+        .WhereColumns(it=>new { it.Code,it.dateOnly }).ExecuteCommand();
 }
 
 
@@ -290,5 +293,11 @@ public class UnitDatez211afa2222
     [SugarColumn(IsNullable =true)]
     public TimeOnly? timeOnly { get; set; }
     [SugarColumn(IsNullable = true)]
+    public DateOnly? dateOnly { get; set; }
+}
+public class Unitadfadfassyss 
+{
+    public string Name { get; set; }
+    public string Code { get; set; }
     public DateOnly? dateOnly { get; set; }
 }
