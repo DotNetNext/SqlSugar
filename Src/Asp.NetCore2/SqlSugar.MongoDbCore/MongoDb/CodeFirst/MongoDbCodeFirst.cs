4906faa2c398a70add3c0988d62498ec5ec3f66b
sunkaixuan <610262374@qq.com>
2025-06-12 16:39:04 +0800

    Update mongodb

--- Src/Asp.NetCore2/SqlSugar.MongoDbCore/MongoDb/CodeFirst/MongoDbCodeFirst.cs
+++ Src/Asp.NetCore2/SqlSugar.MongoDbCore/MongoDb/CodeFirst/MongoDbCodeFirst.cs
@@ -10,7 +10,10 @@ namespace SqlSugar.MongoDb
     {
         public override void InitTables(Type entityType)
         {
-            Console.WriteLine(ErrorMessage.GetThrowMessage($"Create \"{entityType.Name}\" when inserting", $"会在Insert时创建表： \"{entityType.Name}\" ")); 
+            if (this.Context.CurrentConnectionConfig.LanguageType == LanguageType.English)
+                Console.WriteLine($"[MongoDbCodeFirst]: Create \"{entityType.Name}\" when inserting");
+            else
+                Console.WriteLine($"[MongoDbCodeFirst]: 会在Insert时创建表： \"{entityType.Name}\" ");
         }
     }
 }
