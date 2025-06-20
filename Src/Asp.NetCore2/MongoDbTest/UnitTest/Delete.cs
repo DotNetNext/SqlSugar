120bcb28000df25e2182365bcf0e3a20dbdcdccb
sunkaixuan <610262374@qq.com>
2025-06-20 15:53:16 +0800

    Update mongodb

--- Src/Asp.NetCore2/MongoDbTest/UnitTest/Delete.cs
+++ Src/Asp.NetCore2/MongoDbTest/UnitTest/Delete.cs
@@ -14,6 +14,18 @@ namespace MongoDbTest
             var db = DBHelper.DbHelper.GetNewDb();
             db.CodeFirst.InitTables<Student>();
             db.DbMaintenance.TruncateTable<Student>();
+            var id=db.Insertable(new Student() { Age = 1, Name = "11", SchoolId = "111", CreateDateTime = DateTime.Now.AddDays(1) }).ExecuteReturnPkList<string>();
+            db.Insertable(new List<Student>() {
+            new Student() { Age = 2, Name = "22", SchoolId = "222", CreateDateTime = DateTime.Now.AddDays(-1) },
+            new Student() { Age = 3, Name = "33", SchoolId = "333", CreateDateTime = DateTime.Now.AddDays(1) }
+            }).ExecuteCommandAsync().GetAwaiter().GetResult();
+            var count=db.Queryable<Student>().Count();
+            var rows=db.Deleteable<Student>().Where(it => it.CreateDateTime < DateTime.Now).ExecuteCommand();
+            var count2 = db.Queryable<Student>().Count();
+            if (count2 != 2) Cases.ThrowUnitError();
+            var rows2 = db.Deleteable<Student>().In(id).ExecuteCommand();
+            var count3 = db.Queryable<Student>().Count();
+            if (count3 != 1) Cases.ThrowUnitError();
         }
         [SqlSugar.SugarTable("UnitStudent1ssdds3z1")]
         public class Student : MongoDbBase
