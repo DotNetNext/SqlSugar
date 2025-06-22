7940dbae1182186c98f6bd8ac6b92f9c4b111d9d
sunkaixuan <610262374@qq.com>
2025-06-22 20:37:38 +0800

    Update mongodb

--- Src/Asp.NetCore2/MongoDbTest/UnitTest/Insert.cs
+++ Src/Asp.NetCore2/MongoDbTest/UnitTest/Insert.cs
@@ -28,6 +28,8 @@ namespace MongoDbTest
             db.Insertable(new Student() { Age = 1, Name = null, SchoolId = "111", CreateDateTime = DateTime.Now }).ExecuteCommand();
             var list2=db.Queryable<Student>().Where(it => it.Name == null).ToList();
             if(list2.Count!=1|| list2.First().Name!=null) Cases.ThrowUnitError();
+            var list3= db.Queryable<Student>().Where(it => null==it.Name).ToList();
+            if (list3.Count != 1 || list3.First().Name != null) Cases.ThrowUnitError();
         }
         [SqlSugar.SugarTable("UnitStudent1ddsfhssds3z1")]
         public class Student : MongoDbBase
