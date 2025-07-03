9ee5c68de8cc5b19fd7f91dc36ccea358a59e1fc
sunkaixuan <610262374@qq.com>
2025-07-03 16:57:41 +0800

    Update mongodb

---
+++ Src/Asp.NetCore2/SqlSugar.MongoDbCore/Entities/ObjectIdArray.cs
@@ -0,0 +1,12 @@
+﻿using MongoDB.Bson;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SqlSugar.MongoDb 
+{
+    public class ObjectIdArray
+    {
+        
+    }
+}
