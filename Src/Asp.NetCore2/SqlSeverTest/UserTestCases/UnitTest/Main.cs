b04ca100e2947728ef4bbcad5989a0412e283926
sunkaixuan <610262374@qq.com>
2025-07-31 19:50:33 +0800

    Add demo

--- Src/Asp.NetCore2/SqlSeverTest/UserTestCases/UnitTest/Main.cs
+++ Src/Asp.NetCore2/SqlSeverTest/UserTestCases/UnitTest/Main.cs
@@ -34,6 +34,7 @@ namespace OrmTest
         }
         public static void Init()
         {
+            Unitdslasdgy.Init();
             Unitdafaaaaa.Init();
             Unitdfafaa.Init();
             Unitdasfyasdfa.Init();
