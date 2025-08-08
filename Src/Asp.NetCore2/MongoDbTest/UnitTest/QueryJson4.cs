1d0c58681f17771d6f89e71a379ad4071a456897
sunkaixuan <610262374@qq.com>
2025-08-08 15:59:55 +0800

    Update demo

--- Src/Asp.NetCore2/MongoDbTest/UnitTest/QueryJson4.cs
+++ Src/Asp.NetCore2/MongoDbTest/UnitTest/QueryJson4.cs
@@ -25,10 +25,21 @@ namespace MongoDbTest
                     SchoolId = 1,
                     Book2 = new Book() { SchoolId=1,Book2 = new Book() { SchoolId = 2 } }
                 }
-            }).ExecuteCommand(); 
+            }).ExecuteCommand();
+            db.Insertable(new Student()
+            {
+                Age = 1,
+                Book = new Book()
+                {
+                    SchoolId = 222,
+                    Book2 = new Book() { SchoolId = 222, Book2 = new Book() { SchoolId = 2 } }
+                }
+            }).ExecuteCommand();
             var data1 = db.Queryable<Student>().Where(s=>s.Book.Book2.SchoolId==1).ToList();
             var data2 = db.Queryable<Student>().Where(s => s.Book.Book2.Book2.SchoolId == 2).ToList();
-
+            if (data1.Count() != 1 || data1.Count() != 1) Cases.Init();
+            if (data1.First().Book.Book2.SchoolId != 1) Cases.Init();
+            if (data2.First().Book.Book2.Book2.SchoolId != 2) Cases.Init();
         }
 
         [SqlSugar.SugarTable("UnitStudentd23351")]
