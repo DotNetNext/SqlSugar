0c3cf6ba70cc0881d6e51c0df0680254d81e90d1
sunkaixuan <610262374@qq.com>
2025-11-17 15:47:01 +0800

    Update demo

--- Src/Asp.NetCore2/SqlSeverTest/UserTestCases/UnitTest/Main.cs
+++ Src/Asp.NetCore2/SqlSeverTest/UserTestCases/UnitTest/Main.cs
@@ -150,7 +150,7 @@ namespace OrmTest
             UCustom01.Init();
             UCustom02.Init();
             UCustom03.Init();
-            UnitUtilConvert.Init();
+            //UnitUtilConvert.Init(); pull  unit test is  error
             Bulk();
             Filter();
             Insert();
