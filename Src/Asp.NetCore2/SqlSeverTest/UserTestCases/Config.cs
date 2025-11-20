40474f5133bd54683a9b197633d12a03167ee2d5
codomposer <akileus0902@gmail.com>
2025-11-20 05:04:34 -0500

    fix: update the weak sql server password

--- Src/Asp.NetCore2/SqlSeverTest/UserTestCases/Config.cs
+++ Src/Asp.NetCore2/SqlSeverTest/UserTestCases/Config.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -16,16 +16,16 @@ namespace SqlSeverTest.UserTestCases
         /// Account have permission to create database
         /// 用有建库权限的数据库账号
         /// </summary>
-        public static string ConnectionString = "server=.;uid=sa;pwd=sasa;database=SQLSUGAR4XTEST;Encrypt=True;TrustServerCertificate=True";
+        public static string ConnectionString = "server=.;uid=sa;pwd=SqlSugar123!;database=SQLSUGAR4XTEST;Encrypt=True;TrustServerCertificate=True";
         /// <summary>
         /// Account have permission to create database
         /// 用有建库权限的数据库账号
         /// </summary>
-        public static string ConnectionString2 = "server=.;uid=sa;pwd=sasa;database=SQLSUGAR4XTEST2;Encrypt=True;TrustServerCertificate=True";
+        public static string ConnectionString2 = "server=.;uid=sa;pwd=SqlSugar123!;database=SQLSUGAR4XTEST2;Encrypt=True;TrustServerCertificate=True";
         /// <summary>
         /// Account have permission to create database
         /// 用有建库权限的数据库账号
         /// </summary>
-        public static string ConnectionString3 = "server=.;uid=sa;pwd=sasa;database=SQLSUGAR4XTEST3;Encrypt=True;TrustServerCertificate=True";
+        public static string ConnectionString3 = "server=.;uid=sa;pwd=SqlSugar123!;database=SQLSUGAR4XTEST3;Encrypt=True;TrustServerCertificate=True";
     }
 }
