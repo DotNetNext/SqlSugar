5b23a752120ac977d4fc1508ec5a1df7047c85a5
sunkaixuan <610262374@qq.com>
2025-12-04 16:07:05 +0800

    Add demo

---
+++ Src/Asp.NetCore2/DmOrmTest/UnitTest/UnitBool.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrmTest
+{
+    internal class UnitBool
+    {
+        public int num { get; set; }
+        public static void Init()
+        {
+            var db = NewUnitTest.Db;
+            db.CodeFirst.InitTables<UnitADFA>();
+            var p = new UnitBool();
+            db.Updateable<UnitADFA>()
+                .SetColumns(it => new UnitADFA()
+                {
+                    a = p.num > 1
+                })
+                .Where(it=>it.id==1)
+                .ExecuteCommand();
+        }
+        public class UnitADFA
+        {
+            public int id { get; set; }
+            public bool a { get; set; }
+        }
+    }
+}
